82bded2c2915f7789d0d6c63f4bcc7ec6a2f1ed2
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2015-11-12 12:24:41 +0000

    Aula 4 - Implementando PacienteDAO

---
+++ CocOdontoDev/src/br/com/cocodonto/framework/dao/CreateDaoException.java
@@ -0,0 +1,25 @@
+package br.com.cocodonto.framework.dao;
+
+public class CreateDaoException extends Exception {
+
+	public CreateDaoException() {
+		super();
+	}
+
+	public CreateDaoException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public CreateDaoException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public CreateDaoException(String message) {
+		super(message);
+	}
+
+	public CreateDaoException(Throwable cause) {
+		super(cause);
+	}
+
+}
--- CocOdontoDev/src/br/com/cocodonto/modelo/dao/PacienteDAO.java
+++ CocOdontoDev/src/br/com/cocodonto/modelo/dao/PacienteDAO.java
@@ -1,36 +1,39 @@
 package br.com.cocodonto.modelo.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
+import br.com.cocodonto.framework.dao.CreateDaoException;
 import br.com.cocodonto.framework.dao.DaoHelper;
 import br.com.cocodonto.modelo.entidade.Paciente;
 
 public class PacienteDAO {
-	
+
 	private DaoHelper daoHelper;
-	
+
 	public PacienteDAO() {
 		daoHelper = new DaoHelper();
 	}
-	
-	public void inserir(Paciente paciente){				
+
+	public void inserir(Paciente paciente) throws CreateDaoException {
 		Connection conn = null;
-		Statement stmt = null;
+		PreparedStatement ps = null;
 		try {
 			conn = daoHelper.getConnection();
-			stmt = conn.createStatement();
-			String sql = "insert into paciente(nome, rg, cpf, sexo) "
-					+ "values('"+paciente.getNome()+"', "
-							+ "'"+paciente.getRg()+"', "
-							+ "'"+paciente.getCpf()+"', "
-							+ "'"+paciente.getSexo()+"')";
-			stmt.executeUpdate(sql);
+			int index = 0;
+			String sql = "insert into paciente(nome, rg, cpf, sexo) " + "values(?, ?, ?, ?)";
+			ps = conn.prepareStatement(sql);
+			ps.setString(++index, paciente.getNome());
+			ps.setString(++index, paciente.getCpf());
+			ps.setString(++index, paciente.getRg());
+			ps.setString(++index, paciente.getSexo().toString());
+			ps.executeUpdate();
+
 		} catch (SQLException e) {
-			e.printStackTrace();
-		}finally {
-			daoHelper.releaseAll(conn, stmt);
-		}		
+			throw new CreateDaoException("Não foi possível realizar a Transação", e);
+		} finally {
+			daoHelper.releaseAll(conn, ps);
+		}
 	}
 }
