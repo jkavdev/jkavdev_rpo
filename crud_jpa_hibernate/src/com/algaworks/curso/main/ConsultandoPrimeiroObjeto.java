7c52e3608e1f8424bb2757dc92e715b23f07d36b
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2015-11-15 17:22:42 +0000

    Alterando registro do banco de dados

--- crud_jpa_hibernate/src/com/algaworks/curso/main/ConsultandoPrimeiroObjeto.java
+++ crud_jpa_hibernate/src/com/algaworks/curso/main/ConsultandoPrimeiroObjeto.java
@@ -1,9 +1,34 @@
 package com.algaworks.curso.main;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.algaworks.curso.modelo.Cliente;
+
 public class ConsultandoPrimeiroObjeto {
-	
+
 	public static void main(String[] args) {
-		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("crud_pu");
+		EntityManager manager = factory.createEntityManager();
+
+		Long idCliente = 6L;
+		Cliente clienteEncontrado = manager.find(Cliente.class, idCliente);
+
+		mostraCliente(clienteEncontrado);
+
+	}
+
+	private static void mostraCliente(Cliente clienteEncontrado) {
+		//se o cliente nao existir, nao executa
+		if (clienteEncontrado != null) {
+			System.out.println("Nome : " + clienteEncontrado.getNome());
+			System.out.println("Idade : " + clienteEncontrado.getIdade());
+			System.out.println("Sexo : " + clienteEncontrado.getSexo());
+			System.out.println("Profissão : " + clienteEncontrado.getProfissao());
+		}else{
+			System.out.println("Cliente não Encontrado!");
+		}
 	}
 
 }
