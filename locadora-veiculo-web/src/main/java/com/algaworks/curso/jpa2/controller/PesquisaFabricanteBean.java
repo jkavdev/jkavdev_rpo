e5b5585d9d1bca787c8201e55a148e1cb37998a9
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2015-11-19 15:31:24 +0000

    Criando pesquisa de fabricante

--- locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/controller/PesquisaFabricanteBean.java
+++ locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/controller/PesquisaFabricanteBean.java
@@ -1,22 +1,29 @@
 package com.algaworks.curso.jpa2.controller;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
+import javax.inject.Named;
 
 import com.algaworks.curso.jpa2.dao.FabricanteDAO;
 import com.algaworks.curso.jpa2.modelo.Fabricante;
 import com.algaworks.curso.jpa2.service.NegocioException;
 import com.algaworks.curso.jpa2.util.jsf.FacesUtil;
 
-public class PesquisaFabricante implements Serializable {
+@Named
+@ViewScoped
+public class PesquisaFabricanteBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	@Inject
 	FabricanteDAO fabricanteDAO;
 
-	private List<Fabricante> fabricantes;
+	private List<Fabricante> fabricantes = new ArrayList<>();
 
 	private Fabricante fabricanteSelecionado;
 
@@ -24,21 +31,28 @@ public class PesquisaFabricante implements Serializable {
 		return fabricantes;
 	}
 
-	public void excluir() {
+//	public void excluir() {
 //		try {
 //			fabricanteDAO.excluir(fabricanteSelecionado);
 //			this.fabricantes.remove(fabricanteSelecionado);
 //			FacesUtil.addSuccessMessage("Fabricante "
 //					+ fabricanteSelecionado.getNome()
-//					+ " excluído com Sucesso!");
+//					+ " excluído com sucesso.");
 //		} catch (NegocioException e) {
 //			FacesUtil.addErrorMessage(e.getMessage());
 //		}
-	}
-	
-	@PostConstruct
-	public void init(){
-		fabricantes = fabricanteDAO.buscarTodos();
+//	}
+
+	public Fabricante getFabricanteSelecionado() {
+		return fabricanteSelecionado;
 	}
 
+	public void setFabricanteSelecionado(Fabricante fabricanteSelecionado) {
+		this.fabricanteSelecionado = fabricanteSelecionado;
+	}
+
+	@PostConstruct
+	public void inicializar() {
+		fabricantes = fabricanteDAO.buscarTodos();
+	}
 }
