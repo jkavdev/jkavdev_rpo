5acc9ca0930baeb5bba855ce7883ec635d67a669
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2015-11-23 17:42:50 +0000

    explicação dos escopos View request e none

--- ProjetoJSF/src/com/algaworks/cursojsf2/dominio/Produto.java
+++ ProjetoJSF/src/com/algaworks/cursojsf2/dominio/Produto.java
@@ -1,7 +1,11 @@
 package com.algaworks.cursojsf2.dominio;
 
-public class Produto {
+import java.io.Serializable;
 
+public class Produto implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
 	private String nome;
 	private String fabricante;
 	private String categoria;
