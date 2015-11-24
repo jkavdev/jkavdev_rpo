66d1647b1fb71324260de7c2ab7dcb6654cf7052
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2015-11-24 20:31:29 +0000

    Mudando a implementação da chave primaria do fabricante para table onde sera mais portavel caso seja preciso mudar de banco de dado

--- trunk/algaworks/jpa_hibernate/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/modelo/Fabricante.java
+++ trunk/algaworks/jpa_hibernate/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/modelo/Fabricante.java
@@ -4,15 +4,25 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.TableGenerator;
 
 @Entity
+@TableGenerator(
+		name = "fabricante_generator", 
+		table = "gerador_codigo", 
+		pkColumnName = "entidade", 
+		valueColumnName = "Alocacao", 
+		allocationSize = 5)
 public class Fabricante {
 
+	@Id
+	@GeneratedValue(generator="fabricante_generator", strategy=GenerationType.TABLE)
 	private Long codigo;
 	private String nome;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	//mudando a estrategia de geracao de chaves primarias
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	//iremos usar a table que e mais portavel entre os bancos	
 	public Long getCodigo() {
 		return codigo;
 	}
