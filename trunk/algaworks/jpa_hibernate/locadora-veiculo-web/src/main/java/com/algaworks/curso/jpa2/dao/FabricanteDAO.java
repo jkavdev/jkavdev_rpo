4d7e82c2aa090ade65a5746397275df45386051b
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2015-11-24 18:45:44 +0000

    Implementando Pesquisa de Fabricante

--- trunk/algaworks/jpa_hibernate/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/dao/FabricanteDAO.java
+++ trunk/algaworks/jpa_hibernate/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/dao/FabricanteDAO.java
@@ -1,19 +1,31 @@
 package com.algaworks.curso.jpa2.dao;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
 import com.algaworks.curso.jpa2.modelo.Fabricante;
+import com.algaworks.curso.jpa2.service.NegocioException;
 
-public class FabricanteDAO implements Serializable{
-	
+public class FabricanteDAO implements Serializable {
+
 	@Inject
 	private EntityManager manager;
-	
-	public void salvar(Fabricante fabricante){
+
+	public void salvar(Fabricante fabricante) {
 		manager.persist(fabricante);
+	}
+
+	public void excluir(Fabricante fabricanteSelecionado)
+			throws NegocioException {
+
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Fabricante> buscarTodos() {
+		return manager.createQuery("from Fabricante").getResultList();
 	}
 
 }
