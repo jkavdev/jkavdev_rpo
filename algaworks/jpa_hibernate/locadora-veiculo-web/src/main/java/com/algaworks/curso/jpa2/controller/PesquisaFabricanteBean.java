68f292a19bb5a13947d77bf0add93c7f1fe17eb3
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2015-11-24 18:45:44 +0000

    Implementando Pesquisa de Fabricante

---
+++ algaworks/jpa_hibernate/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/controller/PesquisaFabricanteBean.java
@@ -0,0 +1,56 @@
+package com.algaworks.curso.jpa2.controller;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.algaworks.curso.jpa2.dao.FabricanteDAO;
+import com.algaworks.curso.jpa2.modelo.Fabricante;
+import com.algaworks.curso.jpa2.service.NegocioException;
+import com.algaworks.curso.jpa2.util.jsf.FacesUtil;
+
+@Named
+@ViewScoped
+public class PesquisaFabricanteBean implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private FabricanteDAO fabricanteDAO;
+	private Fabricante fabricanteSelecionado;
+	private List<Fabricante> fabricantes;
+	
+	public List<Fabricante> getFabricantes(){
+		return fabricantes;
+	}
+	
+	public void excluir(){
+		try {
+			fabricanteDAO.excluir(fabricanteSelecionado);
+			this.fabricantes.remove(fabricanteSelecionado);
+			FacesUtil.addSuccessMessage("Fabricante " + fabricanteSelecionado.getNome() + " Excluido!");
+		} catch (NegocioException e) {
+			FacesUtil.addErrorMessage(e.getMessage());
+		}		
+	}
+	
+	@PostConstruct
+	public void init(){
+		fabricantes = fabricanteDAO.buscarTodos();
+	}
+
+	public Fabricante getFabricanteSelecionado() {
+		return fabricanteSelecionado;
+	}
+
+	public void setFabricanteSelecionado(Fabricante fabricanteSelecionado) {
+		this.fabricanteSelecionado = fabricanteSelecionado;
+	}
+	
+	
+
+}
