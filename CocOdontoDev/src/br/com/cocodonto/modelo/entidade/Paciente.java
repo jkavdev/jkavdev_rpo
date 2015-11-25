d6e53f4887955cc1775c1dd9a1bc6aaab99eab1b
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2015-11-25 20:11:06 +0000

    Criando entidades do projeto

---
+++ CocOdontoDev/src/br/com/cocodonto/modelo/entidade/Paciente.java
@@ -0,0 +1,11 @@
+package br.com.cocodonto.modelo.entidade;
+
+public class Paciente {
+	
+	private Long id;
+	private String nome;
+	private String rg;
+	private String cpf;
+	private SexoType sexo;
+
+}
