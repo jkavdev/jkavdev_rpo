8db3a5c5b1329ddc92361f2f6018ed2e1632ccaa
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2015-11-25 11:56:35 +0000

    Implementando o relacionamento do modeloCarro e fabricante aplicando hashCode e equals para as classes modelos

---
+++ trunk/algaworks/jpa_hibernate/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/service/CadastroModeloCarroService.java
@@ -0,0 +1,28 @@
+package com.algaworks.curso.jpa2.service;
+
+import java.io.Serializable;
+
+import javax.inject.Inject;
+
+import com.algaworks.curso.jpa2.dao.ModeloCarroDAO;
+import com.algaworks.curso.jpa2.modelo.ModeloCarro;
+import com.algaworks.curso.jpa2.util.jpa.Transactional;
+
+public class CadastroModeloCarroService implements Serializable {
+
+	@Inject
+	ModeloCarroDAO modeloCarroDAO;
+
+	@Transactional
+	public void salvar(ModeloCarro modeloCarro) throws NegocioException {
+		if (modeloCarro.getDescricao() == null
+				|| modeloCarro.getDescricao().trim().equals("")) {
+			throw new NegocioException("O nome do Modelo é obrigatório!");
+		}
+		if (modeloCarro.getFabricante() == null) {
+			throw new NegocioException("O fabricante é obrigatório!");
+		}
+		this.modeloCarroDAO.salvar(modeloCarro);
+	}
+
+}
