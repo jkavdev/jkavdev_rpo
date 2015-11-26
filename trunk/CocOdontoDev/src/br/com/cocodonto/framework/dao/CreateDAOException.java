53b32fd258abcfd6e64af4069e0b03629f4289a5
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2015-11-26 23:02:21 +0000

    Finalizando aula 4 e criando createdaoexception

---
+++ trunk/CocOdontoDev/src/br/com/cocodonto/framework/dao/CreateDAOException.java
@@ -0,0 +1,21 @@
+package br.com.cocodonto.framework.dao;
+
+public class CreateDAOException extends Exception {
+
+	public CreateDAOException() {
+		super();
+	}
+
+	public CreateDAOException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public CreateDAOException(String message) {
+		super(message);
+	}
+
+	public CreateDAOException(Throwable cause) {
+		super(cause);
+	}
+
+}
