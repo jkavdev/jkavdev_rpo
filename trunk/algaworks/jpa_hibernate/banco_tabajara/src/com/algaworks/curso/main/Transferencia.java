3d312254643b40fbb92e5b7f96d35fd2d4b10623
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2015-11-29 03:02:52 +0000

    Criando projeto tabajara e uma pequena transferencia entre contas para exemplificar o rollback

---
+++ trunk/algaworks/jpa_hibernate/banco_tabajara/src/com/algaworks/curso/main/Transferencia.java
@@ -0,0 +1,77 @@
+package com.algaworks.curso.main;
+
+import java.util.Scanner;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.algaworks.curso.modelo.Conta;
+
+public class Transferencia {
+
+	public static void main(String[] args) {
+
+		EntityManagerFactory factory = Persistence
+				.createEntityManagerFactory("pu_tabajara");
+		EntityManager manager = factory.createEntityManager();
+
+		Scanner entrada = new Scanner(System.in);
+
+		// seta valores das contas 1 e 2
+		Conta conta1 = manager.find(Conta.class, 1L);
+
+		if (conta1 == null) {
+			System.out.println("Digite Saldo conta 1 : ");
+			double saldoInicialConta1 = entrada.nextDouble();
+			conta1 = new Conta();
+			conta1.setSaldo(saldoInicialConta1);
+		}
+
+		Conta conta2 = manager.find(Conta.class, 2L);
+
+		if (conta2 == null) {
+			System.out.println("Digite Saldo conta 2 : ");
+			double saldoInicialConta2 = entrada.nextDouble();
+			conta2 = new Conta();
+			conta2.setSaldo(saldoInicialConta2);
+		}
+
+		// persiste a alteracao das contas no banco
+
+		manager.getTransaction().begin();
+		manager.persist(conta1);
+		manager.persist(conta2);
+		manager.getTransaction().commit();
+
+		// apresanta valores das contas
+
+		System.out.println("Saldo Conta 1 : " + conta1.getSaldo());
+		System.out.println("Saldo Conta 2 : " + conta2.getSaldo());
+
+		// valor para transferencia entre as contas
+		System.out.println("------------------------------------");
+		System.out
+				.println("Digite o valor a retirar na conta 1 e depositar na conta 2");
+		Double valorTransferencia = entrada.nextDouble();
+
+		// realizando transação entre as contas
+		manager.getTransaction().begin();
+		conta1.setSaldo(conta1.getSaldo() - valorTransferencia);
+		conta2.setSaldo(conta2.getSaldo() + valorTransferencia);
+
+		if (conta1.getSaldo() > 0) {
+			manager.getTransaction().commit();
+			System.out.println("Transferencia realizada com Sucesso");
+		} else {
+			manager.getTransaction().rollback();
+			System.out
+					.println("Transferencia não realizada, saldo insuficiente!");
+		}
+		
+		System.out.println("Saldo Conta 1 : " + conta1.getSaldo());
+		System.out.println("Saldo Conta 2 : " + conta2.getSaldo());
+
+	}
+
+}
