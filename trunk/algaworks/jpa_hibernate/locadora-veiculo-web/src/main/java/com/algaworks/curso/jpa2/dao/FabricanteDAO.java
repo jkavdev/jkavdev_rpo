c85ba8e0463dad06e153d2296a5a6319c4d053d2
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2015-11-29 14:02:30 +0000

    aula v4.3 - Implementando consulta e exclusão de Faricante

--- trunk/algaworks/jpa_hibernate/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/dao/FabricanteDAO.java
+++ trunk/algaworks/jpa_hibernate/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/dao/FabricanteDAO.java
@@ -1,21 +1,43 @@
 package com.algaworks.curso.jpa2.dao;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
 import com.algaworks.curso.jpa2.modelo.Fabricante;
+import com.algaworks.curso.jpa2.service.NegocioException;
+import com.algaworks.curso.jpa2.util.jpa.Transactional;
 
-public class FabricanteDAO implements Serializable{
+public class FabricanteDAO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Inject
 	private EntityManager manager;
-	
-	public void salvar(Fabricante fabricante){
+
+	public void salvar(Fabricante fabricante) {
 		manager.persist(fabricante);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Fabricante> buscarTodos() {
+		return manager.createQuery("from Fabricante").getResultList();
+	}
+
+	// para remover-mos o objeto precisaremos gerenciar o objeto de volta para
+	// manager
+	// com a busca dos objetos no metodo buscartodos, uma vez que o entrega para
+	// a pagina
+	// o manager é encerrado
+	// com teremos que fazer que o manager conheca este objeto ja uma vez
+	// gerenciado
+	@Transactional
+	public void excluir(Fabricante fabricanteSelecionado)throws NegocioException {
+		fabricanteSelecionado = manager.find(Fabricante.class,fabricanteSelecionado.getCodigo());
+		manager.remove(fabricanteSelecionado);
+		manager.flush();
 	}
 
 }
