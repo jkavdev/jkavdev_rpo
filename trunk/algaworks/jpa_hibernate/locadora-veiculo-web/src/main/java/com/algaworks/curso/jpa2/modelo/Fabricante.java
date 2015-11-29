bf5725617a8080a1c7e2edb1ec579551ab65834b
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2015-11-29 14:49:38 +0000

    aula v6 - implementando novo metodo de geração de chave primaria na entidade fabricante

--- trunk/algaworks/jpa_hibernate/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/modelo/Fabricante.java
+++ trunk/algaworks/jpa_hibernate/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/modelo/Fabricante.java
@@ -4,12 +4,22 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.TableGenerator;
 
 @Entity
+//implementando novo gerador de chave primaria
+@TableGenerator(
+		name="codigo_generator",	//nome do mode de geração da chave, usado no generatorValue 
+		table="gerador_codigo",		//nome da tabela no banco de dados
+		pkColumnName="entidade",	//nome da entidade
+		valueColumnName="alocacao",	//nome do campo do valor gerado
+		allocationSize=5			//quantidade de registros fornecidos pelo banco
+)
 public class Fabricante {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(generator="codigo_generator", strategy=GenerationType.TABLE)
 	private Long codigo;
 	private String nome;
 
