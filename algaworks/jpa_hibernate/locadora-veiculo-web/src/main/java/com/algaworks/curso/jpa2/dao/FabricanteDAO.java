2216c70fd653432224b0d88851e74b5c95d79d4d
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2015-12-04 04:02:35 +0000

    aula 4.3 - implementando a consulta e exclusao

--- algaworks/jpa_hibernate/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/dao/FabricanteDAO.java
+++ algaworks/jpa_hibernate/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/dao/FabricanteDAO.java
@@ -1,27 +1,44 @@
 package com.algaworks.curso.jpa2.dao;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
 import com.algaworks.curso.jpa2.modelo.Fabricante;
+import com.algaworks.curso.jpa2.service.NegocioException;
+import com.algaworks.curso.jpa2.util.jpa.Transactional;
 
 //classe fabricantedao que fara acesso ao banco de dados
 //temos que implementar serializable para o jsf, guardar o estado do objeto
 
-public class FabricanteDAO implements Serializable{
+public class FabricanteDAO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
-	//estamos fazendo a injecao desta instancia atraves do cdi
+
+	// estamos fazendo a injecao desta instancia atraves do cdi
 	@Inject
 	private EntityManager manager;
-	
-	public void salvar(Fabricante fabricante){
+
+	public void salvar(Fabricante fabricante) {
 		manager.persist(fabricante);
 	}
-	
-	
+
+	//para excluir-mos o objeto, precisamos que o manager o gerencie
+	//pois depois do fim da requisao, o manager e fechado perdendo todas as informacoes anteriores
+	//podemos fazer isso com o find, ele buscara no banco se existe o fabricante a ser excluido
+	//como esse metodo fara uma alteração do banco temos que transformalo em um metodo transacional
+	@Transactional
+	public void excluir(Fabricante fabricanteSelecionado)throws NegocioException {
+		fabricanteSelecionado = manager.find(Fabricante.class, fabricanteSelecionado.getCodigo());
+		manager.remove(fabricanteSelecionado);
+		manager.flush();
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Fabricante> buscarTodos() {
+		return manager.createQuery("from Fabricante").getResultList();
+	}
 
 }
