3d3b56a7deca736f42f688a09add0e41864c827a
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2015-12-04 05:02:22 +0000

    aula 4.6 - mapeando a primary key aula 4.5 - mapeando a primary key

--- algaworks/jpa_hibernate/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/modelo/Fabricante.java
+++ algaworks/jpa_hibernate/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/modelo/Fabricante.java
@@ -1,17 +1,28 @@
 package com.algaworks.curso.jpa2.modelo;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.TableGenerator;
 
 //classe de modelo de fabricante 
-
+//mudando a geração da chave primaria para um metodo mais portavel a mais bancos
+@TableGenerator(
+		name="codigo_generator",			//nome do gerador da chave primaria
+		table="gerador_codigo",					//nome da tabela gerada no banco de dados
+		allocationSize=5,								//total de ids que a aplicacao buscara no banco
+		pkColumnName="entidade",			//nome da entidade para qual foi gerada a chave primaria
+		valueColumnName="alocacao"	//quantas alocacoes de chaves que o banco teve que realizar
+)
 @Entity
 public class Fabricante {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(generator="codigo_generator", strategy=GenerationType.TABLE)
+	@Column(name="codigo_fabricante")
 	private Long codigo;
 	private String nome;
 
