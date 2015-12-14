9fe22dd6b5a6c31ddef01429b9a40a7c3c0c1d4e
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2015-12-14 10:48:43 +0000

    projeto universo

---
+++ trunk/java/universo/src/main/java/model/Aluno.java
@@ -0,0 +1,47 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "aluno")
+public class Aluno {
+
+	private Long id;
+	private String descricao;
+	private Sala sala;
+
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Column(name = "descricao")
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@ManyToOne
+	public Sala getSala() {
+		return sala;
+	}
+
+	public void setSala(Sala sala) {
+		this.sala = sala;
+	}
+
+}
