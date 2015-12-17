5876e27eb79522f95868d988842fdcd1a1fb2656
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2015-12-17 19:04:08 +0000

    usando backingBeans

--- trunk/algaworks/jsf_2/PrimeiroProjetoJSF/src/com/algaworks/cursojsf2/OlaMundoBean.java
+++ trunk/algaworks/jsf_2/PrimeiroProjetoJSF/src/com/algaworks/cursojsf2/OlaMundoBean.java
@@ -1,14 +1,28 @@
 package com.algaworks.cursojsf2;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.component.html.HtmlCommandButton;
+import javax.faces.component.html.HtmlInputText;
 
 @ManagedBean
 public class OlaMundoBean {
 
 	private String nome;
+	private HtmlInputText campo;
+	private HtmlCommandButton botao;
 
 	public void enviar() {
 		this.setNome(this.getNome().toUpperCase());
+	}
+
+	public void testar() {
+		if (this.campo.isDisabled()) {
+			this.campo.setDisabled(false);
+			this.botao.setStyle("");
+		} else {
+			this.campo.setDisabled(true);
+			this.botao.setStyle("background-color: red; color: white;");
+		}
 	}
 
 	public String getNome() {
@@ -17,6 +31,22 @@ public class OlaMundoBean {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public HtmlInputText getCampo() {
+		return campo;
+	}
+
+	public void setCampo(HtmlInputText campo) {
+		this.campo = campo;
+	}
+
+	public HtmlCommandButton getBotao() {
+		return botao;
+	}
+
+	public void setBotao(HtmlCommandButton botao) {
+		this.botao = botao;
 	}
 
 }
