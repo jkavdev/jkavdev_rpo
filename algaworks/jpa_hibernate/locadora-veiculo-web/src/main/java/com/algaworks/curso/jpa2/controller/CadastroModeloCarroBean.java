4abe4110896cde52c15ab2ec8daa0a6a4e3823e4
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2015-12-19 22:21:41 +0000

    v4.8-relacionamento-muitos-para-um-v1

---
+++ algaworks/jpa_hibernate/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/controller/CadastroModeloCarroBean.java
@@ -0,0 +1,68 @@
+package com.algaworks.curso.jpa2.controller;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.algaworks.curso.jpa2.dao.FabricanteDAO;
+import com.algaworks.curso.jpa2.modelo.Fabricante;
+import com.algaworks.curso.jpa2.modelo.ModeloCarro;
+import com.algaworks.curso.jpa2.service.CadastroModeloCarroService;
+import com.algaworks.curso.jpa2.service.NegocioException;
+import com.algaworks.curso.jpa2.util.jsf.FacesUtil;
+
+@Named
+@ViewScoped
+public class CadastroModeloCarroBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private CadastroModeloCarroService cadastroModeloCarroService;
+	@Inject
+	private FabricanteDAO fabricanteDAO;
+	private ModeloCarro modeloCarro;
+	// para trazer a lista de fabricantes cadastrados
+	private List<Fabricante> fabricantes;
+
+	public void salvar() {
+		try {
+			this.cadastroModeloCarroService.salvar(modeloCarro);
+			FacesUtil.addSuccessMessage("Modelo carro " + modeloCarro.getDescricao() + " salvo com sucesso!");
+		} catch (NegocioException e) {
+			FacesUtil.addErrorMessage(e.getMessage());
+		}
+		this.limpar();
+	}
+
+	@PostConstruct
+	private void init() {
+		this.limpar();
+		fabricantes = fabricanteDAO.buscarTodos();
+	}
+
+	public void limpar() {
+		this.modeloCarro = new ModeloCarro();
+	}
+
+	public ModeloCarro getModeloCarro() {
+		return modeloCarro;
+	}
+
+	public void setModeloCarro(ModeloCarro modeloCarro) {
+		this.modeloCarro = modeloCarro;
+	}
+
+	public List<Fabricante> getFabricantes() {
+		return fabricantes;
+	}
+
+	public void setFabricantes(List<Fabricante> fabricantes) {
+		this.fabricantes = fabricantes;
+	}
+
+}
--- algaworks/jpa_hibernate/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/modelo/Fabricante.java
+++ algaworks/jpa_hibernate/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/modelo/Fabricante.java
@@ -39,4 +39,38 @@ public class Fabricante {
 		this.nome = nome;
 	}
 
+	//implementacao do hash e equals
+	//quando o jsf compara um objeto com outro, 
+	//por padrao ele compara sua posicao nao memoria se estes objetos sao iguais
+	//no nosso caso, um objeto e igual se ele tiver a mesma chave primaria
+	//para isso implementamos os metodos de verifacao de igualdade
+	//em cima do codigo
+	//se este tiverem os mesmos codigos, estes serao iguais
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Fabricante other = (Fabricante) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+	
+	
+
 }
