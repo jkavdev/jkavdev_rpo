04fc228b0637312a3d51e3cf7258de98ebedec3b
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2015-12-19 21:33:17 +0000

    v4.6. Mapeando a Primary Key

--- trunk/algaworks/jpa_hibernate/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/modelo/Fabricante.java
+++ trunk/algaworks/jpa_hibernate/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/modelo/Fabricante.java
@@ -4,17 +4,25 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.TableGenerator;
 
 //camada de modelo de dados
 
 @Entity
+@TableGenerator(
+		name = "fabricante_generator",	//nome do gerador 
+		table = "gerador_codigo", 		//nome da tabela no banco
+		allocationSize = 1, 			//quantidade de ids fornecidos pelo banco	
+		pkColumnName = "entidade", 		//nome do campo, do qual sera de qual entidade sera gerada o id
+		valueColumnName = "alocacao")	//quantidade de ids gerados
 public class Fabricante {
 
 	private Long codigo;
 	private String nome;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	 @GeneratedValue(strategy = GenerationType.IDENTITY)
+//	@GeneratedValue(generator = "fabricante_generator", strategy = GenerationType.TABLE)
 	public Long getCodigo() {
 		return codigo;
 	}
