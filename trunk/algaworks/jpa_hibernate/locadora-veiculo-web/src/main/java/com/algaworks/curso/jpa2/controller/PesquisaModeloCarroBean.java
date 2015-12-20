f13f5c8728bc49e92204d5f425559549d9b80483
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2015-12-20 00:28:49 +0000

    v4.10.1-relacionamento-muitos-para-muitos-v1 - implementando edicao, exclusao e listamento de modelo de carro

---
+++ trunk/algaworks/jpa_hibernate/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/controller/PesquisaModeloCarroBean.java
@@ -0,0 +1,58 @@
+package com.algaworks.curso.jpa2.controller;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.algaworks.curso.jpa2.dao.ModeloCarroDAO;
+import com.algaworks.curso.jpa2.modelo.ModeloCarro;
+import com.algaworks.curso.jpa2.service.NegocioException;
+import com.algaworks.curso.jpa2.util.jsf.FacesUtil;
+
+import java.util.List;
+import java.io.Serializable;
+
+@Named
+@ViewScoped
+public class PesquisaModeloCarroBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private ModeloCarroDAO modeloCarroDAO;
+	private List<ModeloCarro> modelosCarro;
+	private ModeloCarro modeloCarroSelecionado;
+
+	@PostConstruct
+	public void init() {
+		modelosCarro = this.modeloCarroDAO.buscarTodos();
+	}
+
+	public void excluir() {
+		try {
+			modeloCarroDAO.excluir(modeloCarroSelecionado);
+			modelosCarro.remove(modeloCarroSelecionado);
+			FacesUtil.addSuccessMessage("Modelo " + modeloCarroSelecionado.getDescricao() + " excluido com sucesso!");
+		} catch (NegocioException e) {
+			FacesUtil.addErrorMessage(e.getMessage());
+		}
+	}
+
+	public List<ModeloCarro> getModelosCarro() {
+		return modelosCarro;
+	}
+
+	public void setModelosCarro(List<ModeloCarro> modelosCarro) {
+		this.modelosCarro = modelosCarro;
+	}
+
+	public ModeloCarro getModeloCarroSelecionado() {
+		return modeloCarroSelecionado;
+	}
+
+	public void setModeloCarroSelecionado(ModeloCarro modeloCarroSelecionado) {
+		this.modeloCarroSelecionado = modeloCarroSelecionado;
+	}
+
+}
