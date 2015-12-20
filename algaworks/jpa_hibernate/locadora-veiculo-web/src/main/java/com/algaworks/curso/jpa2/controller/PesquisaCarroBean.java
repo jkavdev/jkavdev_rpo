a6a6eba1d2134adcaaa5d4efd925df263614c03a
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2015-12-20 05:32:49 +0000

    v4.15-implementando-mais-uma-consulta-v1

---
+++ algaworks/jpa_hibernate/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/controller/PesquisaCarroBean.java
@@ -0,0 +1,58 @@
+package com.algaworks.curso.jpa2.controller;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.algaworks.curso.jpa2.dao.CarroDAO;
+import com.algaworks.curso.jpa2.modelo.Carro;
+import com.algaworks.curso.jpa2.service.NegocioException;
+import com.algaworks.curso.jpa2.util.jsf.FacesUtil;
+
+@Named
+@ViewScoped
+public class PesquisaCarroBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private CarroDAO carroDAO;
+	private Carro carroSelecionado;
+	private List<Carro> carros;
+
+	@PostConstruct
+	public void init() {
+		carros = carroDAO.buscarTodos();
+	}
+
+	public void excluir() {
+		try {
+			carroDAO.excluir(carroSelecionado);
+			carros.remove(carroSelecionado);
+			FacesUtil.addSuccessMessage("Carro " + carroSelecionado.getPlaca() + " excluido com sucesso");
+		} catch (NegocioException e) {
+			FacesUtil.addErrorMessage(e.getMessage());
+		}
+	}
+
+	public Carro getCarroSelecionado() {
+		return carroSelecionado;
+	}
+
+	public void setCarroSelecionado(Carro carroSelecionado) {
+		this.carroSelecionado = carroSelecionado;
+	}
+
+	public List<Carro> getCarros() {
+		return carros;
+	}
+
+	public void setCarros(List<Carro> carros) {
+		this.carros = carros;
+	}
+
+}
