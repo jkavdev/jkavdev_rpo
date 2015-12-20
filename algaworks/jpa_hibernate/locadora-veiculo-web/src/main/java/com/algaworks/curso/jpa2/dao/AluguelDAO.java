84d57b8efc588b0516339e28c30303ebf69ec7c7
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2015-12-20 05:13:12 +0000

    v4.13. Mapeamento Um-Para-Um

--- algaworks/jpa_hibernate/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/dao/AluguelDAO.java
+++ algaworks/jpa_hibernate/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/dao/AluguelDAO.java
@@ -6,6 +6,7 @@ import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
 import com.algaworks.curso.jpa2.modelo.Aluguel;
+import com.algaworks.curso.jpa2.util.jpa.Transactional;
 
 public class AluguelDAO implements Serializable {
 
@@ -13,8 +14,15 @@ public class AluguelDAO implements Serializable {
 
 	@Inject
 	private EntityManager manager;
+	// como salvar a apolice nao fazr parte
+	// da regra de negocio, podemos
+	// salva no dao, onde temos acesso ao banco
+	@Inject
+	private ApoliceSeguroDAO apoliceSeguroDAO;
 
+	@Transactional
 	public void salvar(Aluguel aluguel) {
+		apoliceSeguroDAO.salvar(aluguel.getApoliceSeguro());
 		manager.merge(aluguel);
 	}
 
