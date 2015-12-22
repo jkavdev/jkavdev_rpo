291ac037d01084d36649508f1dd6a9af0264ba43
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2015-12-22 20:24:01 +0000

    criando cadastro de usuario

--- java_para_web/teste/src/main/java/br/com/javaparaweb/teste/web/UsuarioBean.java
+++ java_para_web/teste/src/main/java/br/com/javaparaweb/teste/web/UsuarioBean.java
@@ -1,8 +1,12 @@
 package br.com.javaparaweb.teste.web;
 
 import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 
+@RequestScoped
+@ManagedBean(name="usuarioBean")
 public class UsuarioBean {
 
 	private String nome;
