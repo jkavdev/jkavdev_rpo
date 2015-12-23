e3a311ac99b2f3ae30ed77af7f4475d102643b49
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2015-12-23 02:02:04 +0000

    v7.1-entendendo-a-sessao-e-transacao-no-projeto-v1

--- algaworks/jpa_hibernate/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/util/jpa/EntityManagerProducer.java
+++ algaworks/jpa_hibernate/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/util/jpa/EntityManagerProducer.java
@@ -8,23 +8,30 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+//sera criado apenas um objeto pro aplicacao
 @ApplicationScoped
 public class EntityManagerProducer {
 
 	private EntityManagerFactory factory;
 
+	// quando for criado, a fabrica sera inicializada
 	public EntityManagerProducer() {
 		this.factory = Persistence.createEntityManagerFactory("locadoraVeiculoPU");
 	}
 
+	// informa que sera produzido um manager,
+	// toda vez que entitymanager for injetado com inject
 	@Produces
+	// informa que o escopo sera de requisicao
+	// quando iniciar uma requisicao sera criado um manager
 	@RequestScoped
 	public EntityManager create() {
 		return factory.createEntityManager();
 	}
 
+	//fecha o manager quando a requisicao terminar
 	public void close(@Disposes EntityManager manager) {
 		manager.close();
 	}
-	
+
 }
