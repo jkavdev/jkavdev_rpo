e269f90cd572a688ff22b3f96afe69c8183fe6ef
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2015-12-26 20:26:10 +0000

    Aula 03 - Projeto escola de música - Introdução - YouTube

--- Heitor_Bernadino/escola_musica/src/escola/musica/modelo/CursoBean.java
+++ Heitor_Bernadino/escola_musica/src/escola/musica/modelo/CursoBean.java
@@ -1,11 +1,22 @@
 package escola.musica.modelo;
 
+import java.util.Arrays;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
 @ManagedBean
 public class CursoBean {
 
 	private Curso curso = new Curso();
+	private List<TipoCurso> tipos = Arrays.asList(TipoCurso.values());
+	
+	public void salvar(){
+		FacesContext.getCurrentInstance().addMessage(null, 
+				new FacesMessage("Curso salvo com sucesso"));
+	}
 
 	public Curso getCurso() {
 		return curso;
@@ -13,6 +24,14 @@ public class CursoBean {
 
 	public void setCurso(Curso curso) {
 		this.curso = curso;
+	}
+
+	public List<TipoCurso> getTipos() {
+		return tipos;
+	}
+
+	public void setTipos(List<TipoCurso> tipos) {
+		this.tipos = tipos;
 	}
 
 }
