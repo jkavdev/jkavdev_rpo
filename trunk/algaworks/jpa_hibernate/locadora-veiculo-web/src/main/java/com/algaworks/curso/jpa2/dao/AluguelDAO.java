83deca6a15db0cdc3930ed634f0256548e34836b
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2015-12-27 20:07:25 +0000

    v8.2-consulta-com-criteria-v1

--- trunk/algaworks/jpa_hibernate/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/dao/AluguelDAO.java
+++ trunk/algaworks/jpa_hibernate/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/dao/AluguelDAO.java
@@ -1,11 +1,22 @@
 package com.algaworks.curso.jpa2.dao;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.ParameterExpression;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import com.algaworks.curso.jpa2.modelo.Aluguel;
+import com.algaworks.curso.jpa2.modelo.ModeloCarro;
 import com.algaworks.curso.jpa2.util.jpa.Transactional;
 
 public class AluguelDAO implements Serializable {
@@ -24,6 +35,53 @@ public class AluguelDAO implements Serializable {
 	public void salvar(Aluguel aluguel) {
 		apoliceSeguroDAO.salvar(aluguel.getApoliceSeguro());
 		manager.merge(aluguel);
+	}
+
+	public List<Aluguel> buscarPorDataDeEntregaEModeloCarro(Date dataEntrega, ModeloCarro modelo) {
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
+		CriteriaQuery<Aluguel> createQuery = builder.createQuery(Aluguel.class);
+		Root<Aluguel> a = createQuery.from(Aluguel.class);
+		createQuery.select(a);
+
+		List<Predicate> predicates = new ArrayList<>();
+
+		if (dataEntrega != null) {
+			ParameterExpression<Date> dataEntregaInicial = builder.parameter(Date.class, "dataEntregaInicial");
+			ParameterExpression<Date> dataEntregaFinal = builder.parameter(Date.class, "dataEntregaFinal");
+			predicates.add(builder.between(a.<Date> get("dataEntrega"), dataEntregaInicial, dataEntregaFinal));
+		}
+
+		if (modelo != null) {
+			ParameterExpression<ModeloCarro> modeloExpression = builder.parameter(ModeloCarro.class, "modelo");
+			predicates.add(builder.equal(a.get("carro").get("modelo"), modeloExpression));
+		}
+
+		createQuery.where(predicates.toArray(new Predicate[0]));
+
+		TypedQuery<Aluguel> query = manager.createQuery(createQuery);
+
+		if (dataEntrega != null) {
+			Calendar dataEntregaInicial = Calendar.getInstance();
+			dataEntregaInicial.setTime(dataEntrega);
+			dataEntregaInicial.set(Calendar.HOUR_OF_DAY, 0);
+			dataEntregaInicial.set(Calendar.MINUTE, 0);
+			dataEntregaInicial.set(Calendar.SECOND, 0);
+
+			Calendar dataEntregaFinal = Calendar.getInstance();
+			dataEntregaFinal.setTime(dataEntrega);
+			dataEntregaFinal.set(Calendar.HOUR_OF_DAY, 23);
+			dataEntregaFinal.set(Calendar.MINUTE, 59);
+			dataEntregaFinal.set(Calendar.SECOND, 59);
+
+			query.setParameter("dataEntregaInicial", dataEntregaInicial.getTime());
+			query.setParameter("dataEntregaFinal", dataEntregaFinal.getTime());
+		}
+
+		if (modelo != null) {
+			query.setParameter("modelo", modelo);
+		}
+
+		return query.getResultList();
 	}
 
 }
