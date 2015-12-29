c0ca499389388323a8741dbc3fbfe8cb23698501
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2015-12-29 16:40:04 +0000

    implementando listagem de registros

--- java/financeiro/src/main/java/br/com/javaparaweb/financeiro/web/UsuarioBean.java
+++ java/financeiro/src/main/java/br/com/javaparaweb/financeiro/web/UsuarioBean.java
@@ -1,5 +1,7 @@
 package br.com.javaparaweb.financeiro.web;
 
+import java.util.List;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
@@ -14,8 +16,12 @@ public class UsuarioBean {
 
 	private Usuario usuario = new Usuario();
 	private String confirmaSenha;
+	private List<Usuario> lista;
+	private String destinoSalvar;
 
+	//setando o destino para usuario sucesso
 	public String novo() {
+		this.destinoSalvar = "usuarioSucesso";
 		this.usuario = new Usuario();
 		this.usuario.setAtivo(true);
 		return "/publico/usuario";
@@ -29,7 +35,35 @@ public class UsuarioBean {
 			return null;
 		}
 		new UsuarioRN().salvar(this.usuario);
-		return "usuarioSucesso";
+		return this.destinoSalvar;
+	}
+
+	//excluindo usuario
+	//retorna null pois nao mudara de pagina
+	public String excluir() {
+		new UsuarioRN().excluir(this.usuario);
+		this.lista = null;
+		return null;
+	}
+
+	//altera estado do usuario para ativo ou desativado
+	//depois salva as configuracoes no banco
+	//retorna null pois nao mudara de pagina
+	public String ativar() {
+		if (this.usuario.isAtivo())
+			this.usuario.setAtivo(false);
+		else
+			this.usuario.setAtivo(true);
+
+		new UsuarioRN().salvar(usuario);
+		return null;
+	}
+
+	//seta confimar senha com a senha do usuario
+	//e manda para pagina de cadastro
+	public String editar() {
+		this.confirmaSenha = this.usuario.getSenha();
+		return "/publico/usuario";
 	}
 
 	public Usuario getUsuario() {
@@ -46,6 +80,28 @@ public class UsuarioBean {
 
 	public void setConfirmaSenha(String confirmaSenha) {
 		this.confirmaSenha = confirmaSenha;
+	}
+
+	//busca registro do banco
+	//se a lista de fo nula, busca do banco
+	//ira buscar do banco quando iniciar a pagina ou quando excluir um registro
+	public List<Usuario> getLista() {
+		if (this.lista == null) {
+			this.lista = new UsuarioRN().listar();
+		}
+		return this.lista;
+	}
+
+	public void setLista(List<Usuario> lista) {
+		this.lista = lista;
+	}
+
+	public String getDestinoSalvar() {
+		return destinoSalvar;
+	}
+
+	public void setDestinoSalvar(String destinoSalvar) {
+		this.destinoSalvar = destinoSalvar;
 	}
 
 }
