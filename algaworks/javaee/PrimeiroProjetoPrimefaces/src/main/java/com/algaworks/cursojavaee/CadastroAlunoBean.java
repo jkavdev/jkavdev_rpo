d2e4b2f77c00f5dfe82f1c5beac1e9282b22aae4
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-01-15 01:48:30 +0000

    2.1 - Panel grid input text output label command button trablhando com campos e butoes do jsf e primefaces

---
+++ algaworks/javaee/PrimeiroProjetoPrimefaces/src/main/java/com/algaworks/cursojavaee/CadastroAlunoBean.java
@@ -0,0 +1,38 @@
+package com.algaworks.cursojavaee;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ViewScoped
+@ManagedBean
+public class CadastroAlunoBean implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	private String nome;
+	private String email;
+
+	public void cadastrar() {
+		System.out.println("Nome " + this.nome);
+		System.out.println("Email " + this.email);
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
