31c00de1ca597fbd9435d033b622a0c33bad0abf
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-01-18 13:34:15 +0000

    criação dos daos

---
+++ trunk/Augusto_Branquinho/Projeto_Hibernate_4/wpattern-framework-hibernate4/src/main/java/org/wpttern/frameworks/hibernate4/utils/DAOFactory.java
@@ -0,0 +1,21 @@
+package org.wpttern.frameworks.hibernate4.utils;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public final class DAOFactory {
+
+	public DAOFactory() {
+	}
+
+	private static final String PERSISTENCE_UNIT_NAME = "hibernate_4";
+	private static EntityManagerFactory factory;
+
+	public static EntityManagerFactory entityManagerFactoryInstance() {
+		if (factory == null) {
+			factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+		}
+		return factory;
+	}
+
+}
