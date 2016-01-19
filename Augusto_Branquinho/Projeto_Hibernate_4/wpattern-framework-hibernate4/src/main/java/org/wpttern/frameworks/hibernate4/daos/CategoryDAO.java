dc16320ecde55f27da00caa2c4c6f145c68bae9a
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-01-19 00:03:02 +0000

    desenvolvendo dao e teste unitario

---
+++ Augusto_Branquinho/Projeto_Hibernate_4/wpattern-framework-hibernate4/src/main/java/org/wpttern/frameworks/hibernate4/daos/CategoryDAO.java
@@ -0,0 +1,10 @@
+package org.wpttern.frameworks.hibernate4.daos;
+
+import org.wpttern.frameworks.hibernate4.entites.CategoryEntity;
+import org.wpttern.frameworks.hibernate4.utils.GenericDAO;
+
+public class CategoryDAO extends GenericDAO<CategoryEntity, Long>{
+	
+	
+
+}
---
+++ Augusto_Branquinho/Projeto_Hibernate_4/wpattern-framework-hibernate4/src/main/java/org/wpttern/frameworks/hibernate4/entites/CategoryEntity.java
@@ -0,0 +1,70 @@
+package org.wpttern.frameworks.hibernate4.entites;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.wpttern.frameworks.hibernate4.utils.BaseBean;
+
+@Entity
+@Table(name = "categories")
+public class CategoryEntity extends BaseBean {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String categoryName;
+	private String description;
+	private Byte[] picture;
+
+	public CategoryEntity() {
+	}
+
+	public CategoryEntity(Long id, String categoryName, String description, Byte[] picture) {
+		super();
+		this.id = id;
+		this.categoryName = categoryName;
+		this.description = description;
+		this.picture = picture;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "category_id")
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Column(name = "category_name")
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Byte[] getPicture() {
+		return picture;
+	}
+
+	public void setPicture(Byte[] picture) {
+		this.picture = picture;
+	}
+
+}
