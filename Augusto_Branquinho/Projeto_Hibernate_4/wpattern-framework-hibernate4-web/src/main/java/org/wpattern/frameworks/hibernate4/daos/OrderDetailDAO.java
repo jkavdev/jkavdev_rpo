09c61bf7a1d08c44b75a7e430265d3d448a682d4
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-01-20 12:26:12 +0000

    projeto hibernate 4 web

---
+++ Augusto_Branquinho/Projeto_Hibernate_4/wpattern-framework-hibernate4-web/src/main/java/org/wpattern/frameworks/hibernate4/daos/OrderDetailDAO.java
@@ -0,0 +1,9 @@
+package org.wpattern.frameworks.hibernate4.daos;
+
+import org.wpattern.frameworks.hibernate4.entites.OrderDetailEntity;
+import org.wpattern.frameworks.hibernate4.keys.OrderDetailKey;
+import org.wpattern.frameworks.hibernate4.utils.GenericDAO;
+
+public class OrderDetailDAO extends GenericDAO<OrderDetailEntity, OrderDetailKey> {
+
+}
