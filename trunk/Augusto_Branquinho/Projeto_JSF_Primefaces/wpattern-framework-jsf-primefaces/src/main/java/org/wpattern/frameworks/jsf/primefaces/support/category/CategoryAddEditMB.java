0dc61092b834ceac295bd5c4a4e0abeed697a961
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-02-04 11:08:34 +0000

    ainda com erro

--- trunk/Augusto_Branquinho/Projeto_JSF_Primefaces/wpattern-framework-jsf-primefaces/src/main/java/org/wpattern/frameworks/jsf/primefaces/support/category/CategoryAddEditMB.java
+++ trunk/Augusto_Branquinho/Projeto_JSF_Primefaces/wpattern-framework-jsf-primefaces/src/main/java/org/wpattern/frameworks/jsf/primefaces/support/category/CategoryAddEditMB.java
@@ -1,7 +1,62 @@
 package org.wpattern.frameworks.jsf.primefaces.support.category;
 
+import java.util.ResourceBundle;
+
+import javax.faces.application.Application;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.context.WebApplicationContext;
+import org.wpattern.frameworks.jsf.primefaces.model.CategoryEntity;
+import org.wpattern.frameworks.jsf.primefaces.model.utils.BaseBeans;
+
 //managed bean especializado para adicao e edicao
 
-public class CategoryAddEditMB {
+@Scope(value = WebApplicationContext.SCOPE_REQUEST)
+@Named(value = "categoryAddEditMB")
+public class CategoryAddEditMB extends BaseBeans {
+
+	private static final long serialVersionUID = 1L;
+
+	//contexto do primefaces
+	@Inject
+	private FacesContext context;
+	private CategoryEntity category;
+	//define o tipo de operacao
+	private String title;
+
+	//de acordo com o metodo indicaremos qual titulo sera carregado
+	//label sera o propriedade no arquivo de idioma
+	public void add() {
+		this.title = this.getResourceProperty("labels", "category_add");
+	}
+
+	public void update() {
+		this.title = this.getResourceProperty("labels", "category_update");
+	}
+
+	private String getResourceProperty(String resource, String label) {
+		Application application = this.context.getApplication();
+		ResourceBundle bundle = application.getResourceBundle(this.context, resource);
+		return bundle.getString(label);
+	}
+
+	public CategoryEntity getCategory() {
+		return category;
+	}
+
+	public void setCategory(CategoryEntity category) {
+		this.category = category;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
 }
