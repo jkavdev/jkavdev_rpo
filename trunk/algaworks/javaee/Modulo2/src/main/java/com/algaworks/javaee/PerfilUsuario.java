2923d52674ceb8c045321534c57f9bb84cc553e8
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-03-07 01:21:54 +0000

    2.12 - Menus de selecao dependentes com select one menu e ajax 2.13 - Botao de radio com select one radio

--- trunk/algaworks/javaee/Modulo2/src/main/java/com/algaworks/javaee/PerfilUsuario.java
+++ trunk/algaworks/javaee/Modulo2/src/main/java/com/algaworks/javaee/PerfilUsuario.java
@@ -23,8 +23,36 @@ public class PerfilUsuario implements Serializable {
 	private Date dataNascimento;
 	private String profissao;
 	private Interesse interesse;
+	private String estado;
+	private String cidade;
+	private String sexo;
+	private String estadoCivil;
 	
 	public static final List<Interesse> INTERESSES = new ArrayList<>();
+	private List<String> estados = new ArrayList<>();
+	private List<String> cidades = new ArrayList<>();
+	
+	public PerfilUsuario() {
+		estados.add("MG");
+		estados.add("SP");
+		estados.add("RJ");
+	}
+	
+	public void carregarCidades(){
+		cidades.clear();
+		if("MG".equals(estado)){
+			cidades.add("Uberlandia");
+			cidades.add("Uberaba");
+			cidades.add("Belo Horizonte");
+		}else if("SP".equals(estado)){
+			cidades.add("Sao Paulo");
+			cidades.add("Franca");
+			cidades.add("Ribeirao");
+		}else if("RJ".equals(estado)){
+			cidades.add("Rio de Janeiro");
+			cidades.add("Belo Niteroi");
+		}
+	}
 	
 	//inicializando lista de interesses
 	static{		
@@ -35,7 +63,8 @@ public class PerfilUsuario implements Serializable {
 	}
 
 	public void atualizar() {
-		System.out.println("Interesse: " + this.interesse.getDescricao());
+		System.out.println("Sexo: " + this.sexo);
+		System.out.println("Estado Civil: " + this.estadoCivil);
 
 		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Perfil Atualizado"));
 	}
@@ -116,6 +145,54 @@ public class PerfilUsuario implements Serializable {
 
 	public void setInteresse(Interesse interesse) {
 		this.interesse = interesse;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public List<String> getEstados() {
+		return estados;
+	}
+
+	public void setEstados(List<String> estados) {
+		this.estados = estados;
+	}
+
+	public List<String> getCidades() {
+		return cidades;
+	}
+
+	public void setCidades(List<String> cidades) {
+		this.cidades = cidades;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getEstadoCivil() {
+		return estadoCivil;
+	}
+
+	public void setEstadoCivil(String estadoCivil) {
+		this.estadoCivil = estadoCivil;
 	}
 	
 	
