094e2527f6605d1562c05eb9badc34d91a29fef8
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-03-16 05:45:14 +0000

    5.2 - Prototipando a pesquisa de pedidos

---
+++ algaworks/javaee/PedidoVenda/src/main/java/com/algaworks/pedidovenda/controller/PesquisaPedidosBean.java
@@ -0,0 +1,27 @@
+package com.algaworks.pedidovenda.controller;
+
+import javax.faces.bean.RequestScoped;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+@RequestScoped
+public class PesquisaPedidosBean {
+
+	private List<Integer> pedidosFiltrados;
+
+	public PesquisaPedidosBean() {
+		pedidosFiltrados = new ArrayList<>();
+		for (int i = 0; i < 50; i++) {
+			pedidosFiltrados.add(i);
+		}
+	}
+
+	public List<Integer> getPedidosFiltrados() {
+		return pedidosFiltrados;
+	}
+
+}
