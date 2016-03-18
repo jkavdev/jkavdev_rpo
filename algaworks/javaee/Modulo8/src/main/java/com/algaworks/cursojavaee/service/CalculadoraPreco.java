06ba8c845b17e3f428ee24ec5ec8442d5a096a32
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-03-18 13:17:11 +0000

    8.4 - Escopos de beans.1

--- algaworks/javaee/Modulo8/src/main/java/com/algaworks/cursojavaee/service/CalculadoraPreco.java
+++ algaworks/javaee/Modulo8/src/main/java/com/algaworks/cursojavaee/service/CalculadoraPreco.java
@@ -1,6 +1,23 @@
 package com.algaworks.cursojavaee.service;
 
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Dependent;
+
+//bean padrao do cdi
+
+//deendent significa tambem que este bean e dependente
+//de quem o injeta
+//no caso se produto bean que eh o injeto deste bean
+//for request este bean tambem sera request, mudando seu escopo 
+//@Dependent
+@ApplicationScoped
 public class CalculadoraPreco {
+	
+	@PostConstruct
+	public void init(){
+		System.out.println("init calculadoraPreco");
+	}
 
 	public double CalculaPreco(int quantidade, double precoUnitario) {
 		return quantidade * precoUnitario;
