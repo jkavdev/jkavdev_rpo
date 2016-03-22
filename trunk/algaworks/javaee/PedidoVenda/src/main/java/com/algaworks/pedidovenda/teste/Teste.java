9620a20b1439128a6685400dc9e8063a79a7309d
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-03-22 14:23:26 +0000

    10.7 - Mapeando entidades do cadastro de clientes

---
+++ trunk/algaworks/javaee/PedidoVenda/src/main/java/com/algaworks/pedidovenda/teste/Teste.java
@@ -0,0 +1,42 @@
+package com.algaworks.pedidovenda.teste;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import com.algaworks.pedidovenda.model.Cliente;
+import com.algaworks.pedidovenda.model.Endereco;
+import com.algaworks.pedidovenda.model.TipoPessoa;
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("pedidoPU");
+		EntityManager manager = factory.createEntityManager();
+
+		EntityTransaction transaction = manager.getTransaction();
+
+		transaction.begin();
+
+		Cliente cliente = new Cliente();
+		cliente.setNome("Jhonatan");
+		cliente.setEmail("jhonatan@gmail.com");
+		cliente.setDocumentoReceitaFederal("044.882.891-00");
+		cliente.setTipo(TipoPessoa.FISICA);
+
+		Endereco endereco = new Endereco();
+		endereco.setLogradouro("Qr 12 Casa 15");
+		endereco.setNumero("15");
+		endereco.setCidade("Planaltina de Goiais");
+		endereco.setUf("GO");
+		endereco.setCep("73754012");
+		manager.persist(cliente);
+		endereco.setCliente(cliente);
+
+		transaction.commit();
+
+	}
+
+}
