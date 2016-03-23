b142a146dbb13071eacccd7797164dee0150949e
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-03-23 02:39:23 +0000

    10.11 - Mapeando entidades do pedido de venda

--- trunk/algaworks/javaee/PedidoVenda/src/main/java/com/algaworks/pedidovenda/teste/Teste.java
+++ trunk/algaworks/javaee/PedidoVenda/src/main/java/com/algaworks/pedidovenda/teste/Teste.java
@@ -16,29 +16,29 @@ public class Teste {
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("pedidoPU");
 		EntityManager manager = factory.createEntityManager();
 
-		EntityTransaction transaction = manager.getTransaction();
-
-		transaction.begin();
-
-		Cliente cliente = new Cliente();
-		cliente.setNome("Jhonatan");
-		cliente.setEmail("jhonatan@gmail.com");
-		cliente.setDocumentoReceitaFederal("044.882.891-00");
-		cliente.setTipo(TipoPessoa.FISICA);
-
-		Endereco endereco = new Endereco();
-		endereco.setLogradouro("Qr 12 Casa 15");
-		endereco.setNumero("15");
-		endereco.setCidade("Planaltina de Goiais");
-		endereco.setUf("GO");
-		endereco.setCep("73754012");		
-		endereco.setCliente(cliente);
-		
-		cliente.getEnderecos().add(endereco);
-		
-		manager.persist(cliente);
-
-		transaction.commit();
+//		EntityTransaction transaction = manager.getTransaction();
+//
+//		transaction.begin();
+//
+//		Cliente cliente = new Cliente();
+//		cliente.setNome("Jhonatan");
+//		cliente.setEmail("jhonatan@gmail.com");
+//		cliente.setDocumentoReceitaFederal("044.882.891-00");
+//		cliente.setTipo(TipoPessoa.FISICA);
+//
+//		Endereco endereco = new Endereco();
+//		endereco.setLogradouro("Qr 12 Casa 15");
+//		endereco.setNumero("15");
+//		endereco.setCidade("Planaltina de Goiais");
+//		endereco.setUf("GO");
+//		endereco.setCep("73754012");		
+//		endereco.setCliente(cliente);
+//		
+//		cliente.getEnderecos().add(endereco);
+//		
+//		manager.persist(cliente);
+//
+//		transaction.commit();
 
 	}
 
