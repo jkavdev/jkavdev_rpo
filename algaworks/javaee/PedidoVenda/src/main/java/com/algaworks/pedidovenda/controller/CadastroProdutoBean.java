d163f4eb20e0492eb8926e487d3e69db96fe902e
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-03-23 03:05:40 +0000

    11.2 - Configurando bean validation

--- algaworks/javaee/PedidoVenda/src/main/java/com/algaworks/pedidovenda/controller/CadastroProdutoBean.java
+++ algaworks/javaee/PedidoVenda/src/main/java/com/algaworks/pedidovenda/controller/CadastroProdutoBean.java
@@ -1,14 +1,29 @@
 package com.algaworks.pedidovenda.controller;
 
-import javax.enterprise.context.RequestScoped;
+import java.io.Serializable;
+
+import javax.faces.bean.ViewScoped;
 import javax.inject.Named;
 
+import com.algaworks.pedidovenda.model.Produto;
+
 @Named
-@RequestScoped
-public class CadastroProdutoBean {
+@ViewScoped
+public class CadastroProdutoBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 	
-	public void salvar(){
-		throw new RuntimeException("Teste excecao!]");
+	private Produto produto;
+	
+	public CadastroProdutoBean() {
+		produto = new Produto();
+	}
+
+	public void salvar() {
+	}
+	
+	public Produto getProduto() {
+		return produto;
 	}
 
 }
