4e1998a3fa6a9013770df437ee08319eaa2deea8
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-03-25 17:44:40 +0000

    1 Preparando o ambiente

---
+++ trunk/alura/Controle_Horas_VRaptor_JPA/alura_horas/src/main/java/br/com/alura/vraptor/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package br.com.alura.vraptor.controllers;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Path;
+
+//controller da pagina index
+
+@Controller
+public class IndexController {
+
+	// pagina inicial contexto
+	// caminho para chegar na pagina
+	@Path("/")
+	public void index() {
+	}
+
+}
