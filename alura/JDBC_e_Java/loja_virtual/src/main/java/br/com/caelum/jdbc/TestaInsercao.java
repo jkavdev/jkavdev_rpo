f05e5b25a1ff815fdc561256f39af74285a8e7ec
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-03-26 07:10:27 +0000

    4 - Transações commit e rollback

--- alura/JDBC_e_Java/loja_virtual/src/main/java/br/com/caelum/jdbc/TestaInsercao.java
+++ alura/JDBC_e_Java/loja_virtual/src/main/java/br/com/caelum/jdbc/TestaInsercao.java
@@ -1,42 +1,63 @@
 package br.com.caelum.jdbc;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class TestaInsercao {
 
 	public static void main(String[] args) throws SQLException {
-		Connection connection = Database.getConnection();
 
-		// o mysql nao aceita o as simples inserido deste jeito
-		// tem que ser feito com caractere de scape
-		String nome = "notebook'i5 2013";
-		String descricacao = "notebook i5 3 geracao";
-		int index = 0;
-		// para usar com preparedStatement usamos '?'
-		String sql = "insert into produto (nome, descricao) values (?, ?)";
+		// tentara abrir uma conexao e no final de tudo fechara esta conexao
+		// automaticamente
+		try (Connection connection = Database.getConnection()) {
+			// desabilitando autocommit
+			connection.setAutoCommit(false);
 
-		PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-		//tratando os parametros passados
+			int index = 0;
+			String sql = "insert into produto (nome, descricao) values (?, ?)";
+
+			// criara um statement e no final fechara automaticamente
+			try (PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
+				adiciona("TV LCD", "32 Polegadas", index, sql, statement);
+				adiciona("Bluray", "Full HDMI", index, sql, statement);
+
+				// comitando operacao
+				connection.commit();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				connection.rollback();
+				System.out.println("Rollback Efetuado!");
+			}
+		}
+
+	}
+
+	private static void adiciona(String nome, String descricacao, int index, String sql, PreparedStatement statement)
+			throws SQLException {
+
+		if (nome.equals("blueray")) {
+			throw new IllegalArgumentException("Problema Ocorrido!");
+		}
+
+		// tratando os parametros passados
 		statement.setString(++index, nome);
 		statement.setString(++index, descricacao);
 
 		boolean resultado = statement.execute(sql);
 		System.out.println(resultado);
 
-//		ResultSet resultSet = statement.getGeneratedKeys();
-//
-//		while (resultSet.next()) {
-//			long id = resultSet.getLong("id");
-//			System.out.println(id + " gerado");
-//		}
-
-		connection.close();
-
+		// ResultSet resultSet = statement.getGeneratedKeys();
+		//
+		// while (resultSet.next()) {
+		// long id = resultSet.getLong("id");
+		// System.out.println(id + " gerado");
+		// }
+		//
+		// resultSet.close();
 	}
 
 }
