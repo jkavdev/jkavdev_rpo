e1dcfe39e2b39f6e038f976e5042ff25f1d7cdce
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-03-30 02:26:07 +0000

    Projeto Java completo com Swing - Parte 1 - YouTube

---
+++ Swing/CocOdonto_dev/src/br/com/cocodonto/modelo/entidade/Paciente.java
@@ -0,0 +1,12 @@
+package br.com.cocodonto.modelo.entidade;
+
+public class Paciente {
+	
+	private long id;
+	private String nome;
+	private String rg;
+	private String cpf;
+	private SexoType sexo;
+	private Endereco endereco;
+
+}
