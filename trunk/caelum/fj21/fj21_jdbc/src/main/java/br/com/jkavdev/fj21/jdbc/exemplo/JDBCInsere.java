426b55997555b0478d96bdd40be6bc277136daf5
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-04-05 04:15:11 +0000

    2.8 Javabeans 2.9 Inserindo dados no banco

---
+++ trunk/caelum/fj21/fj21_jdbc/src/main/java/br/com/jkavdev/fj21/jdbc/exemplo/JDBCInsere.java
@@ -0,0 +1,36 @@
+package br.com.jkavdev.fj21.jdbc.exemplo;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Calendar;
+
+import br.com.jkavdev.fj21.jdbc.util.ConnectionFactory;
+
+public class JDBCInsere {
+
+	public static void main(String[] args) throws SQLException {
+
+		Connection connection = new ConnectionFactory().getConnection();
+
+		// cria um preparedStatement com o comando a ser executando
+		String sql = "insert into contato(nome, email, endereco, data_nascimento) values(?, ?, ?, ?)";
+		PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+		// preenche os valores
+		preparedStatement.setString(1, "Caelum");
+		preparedStatement.setString(2, "contato@caelum.com.br");
+		preparedStatement.setString(3, "R. Vargueiro 3185 cj67");
+		preparedStatement.setDate(4, new Date(Calendar.getInstance().getTimeInMillis()));
+
+		// executa
+		preparedStatement.execute();
+		preparedStatement.close();
+
+		System.out.println("Gravado");
+		connection.close();
+
+	}
+
+}
---
+++ trunk/caelum/fj21/fj21_jdbc/src/main/java/br/com/jkavdev/fj21/jdbc/modelo/Contato.java
@@ -0,0 +1,72 @@
+package br.com.jkavdev.fj21.jdbc.modelo;
+
+//javabeans sao classes que possuem o construtor sem argumentos e metodos
+//de acesso do tipo get e set
+
+import java.util.Calendar;
+
+public class Contato {
+
+	private Long id;
+	private String nome;
+	private String email;
+	private String endereco;
+	private Calendar dataNascimento;
+
+	public Contato(String nome, String email, String endereco, Calendar dataNascimento) {
+		this();
+		this.nome = nome;
+		this.email = email;
+		this.endereco = endereco;
+		this.dataNascimento = dataNascimento;
+	}
+
+	public Contato(Long id, String nome, String email, String endereco, Calendar dataNascimento) {
+		this(nome, email, endereco, dataNascimento);
+		this.id = id;
+	}
+
+	public Contato() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public Calendar getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Calendar dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+}
