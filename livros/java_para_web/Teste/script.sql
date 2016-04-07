a2a1dfb88b5f92e01732f06437ebcac73e8f8ce9
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-04-07 07:27:38 +0000

    3.2.3 Operacoes com JDBC
    Listagem 3.2 contato.sql
    Listagem 3.3 Contato.java
    Listagem 3.4 ContatoCrudJDBC.java

--- livros/java_para_web/Teste/script.sql
+++ livros/java_para_web/Teste/script.sql
@@ -1,3 +1,13 @@
 create database db_jpw_agenda;
 use db_jpw_agenda;
 
+create table contato(
+	codigo int not null auto_increment,
+	nome varchar(50) null,
+	telefone varchar(50) null,
+	email varchar(50) null,
+	data_cadastro date null,
+	observacao text,
+	primary key(codigo)
+);
+
