facdd89ec00325f2364865692d85a72930594dbc
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-04-07 06:26:28 +0000

    2.14 Ciclo de vida de uma requisicao JSF

---
+++ trunk/livros/java_para_web/Teste/src/main/java/br/com/jkavdev/teste/web/exemplo/CicloDeVidaJSF.java
@@ -0,0 +1,11 @@
+package br.com.jkavdev.teste.web.exemplo;
+
+public class CicloDeVidaJSF {
+	
+	//Restore View
+		//Caso a pagina nao tenha sido carregada ainda, o jsf fara uma leitura da pagina solicitada
+		//e carregara em memoria todos os componentes
+	
+	
+
+}
