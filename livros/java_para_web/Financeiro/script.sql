d185f270ad45cf7519eda898f578b214c2bd36ed
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-04-08 20:07:25 +0000

    5.2 Preparacao da area administrativa 5.3 Utilizacao da datatable 5.4 Como trabalhar com imagens 5.5 Desenvolvimento da listagem de usuarios 5.5.1 Criacao da pagina de listagem de usuarios 5.5.2 Alteracao da classe UsuarioBean
    Listagem 5.1 principal.xhtml
    Listagem 5.2 UsuarioBean.java
    Listagem 5.3 usuario.xhtml
    Listagem 5.4 principal.xhtml

--- livros/java_para_web/Financeiro/script.sql
+++ livros/java_para_web/Financeiro/script.sql
@@ -1,2 +1,10 @@
 create database db_jpw_financeiro;
 use db_jpw_financeiro;
+
+insert into usuario(nome, login, senha, email, idioma, celular, ativo, dataNascimento) values
+	('Jhonatan', 'jhonatan', '123', 'jhonatan@gmail.com', 'pt_BR', '92546554', 1, '1994/05/10'),
+    ('Ismael', 'ismael', '123', 'ismael@gmail.com', 'en_US', '99346124', 0, '1995/05/10'),
+    ('Eilane', 'eilane', '123', 'eilane@gmail.com', 'pt_BR', '96698554', 0, '1996/05/10'),
+    ('Maria', 'maria', '123', 'maria@gmail.com', 'pt_BR', '99341254', 1, '1994/05/10'),
+    ('Douglas', 'douglas', '123', 'douglas@gmail.com', 'en_US', '99123554', 1, '1999/05/10'),
+    ('Lucas', 'lucas', '123', 'lucas@gmail.com', 'es_ES', '99487554', 1, '2000/05/10');
