0f2cc6ffd244b9f56cc81ba998f14a600c661092
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-04-12 14:24:52 +0000

    6.3 Instalacao do Spring security 6.3.1.2 Criacao do arquivo context.xml 6.3.1.3 Alteracao do arquivo web.xml 6.3.1.4 Alteracao do arquivo hibernate.cfg.xml 6.4 Configuracao do Spring Security 6.4.1 Alteracao do arquivo web.xml 6.4.2 Criacao dos arquivos de configuracao do Spring Security
    Listagem 6.1 pom.xml
    Listagem 6.2 context.xml
    Listagem 6.3 web.xml
    Listagem 6.4 web.xml
    Listagem 6.5 applicationContext.xml
    Listagem 6.6 applicationContext-security.xml

--- trunk/livros/java_para_web/Financeiro/src/main/java/br/com/jkavdev/financeiro/util/DaoFactory.java
+++ trunk/livros/java_para_web/Financeiro/src/main/java/br/com/jkavdev/financeiro/util/DaoFactory.java
@@ -3,7 +3,7 @@ package br.com.jkavdev.financeiro.util;
 import br.com.jkavdev.financeiro.usuario.UsuarioDao;
 import br.com.jkavdev.financeiro.usuario.UsuarioDaoHibernate;
 
-public class DAOFactory {
+public class DaoFactory {
 
 	public static UsuarioDao criarUsuarioDAO() {
 		// cria implementacao do dao
