088368afe818167a19f1da4aa4f0a7fad9047be0
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-04-13 06:17:51 +0000

    Parte 06 - Projeto JSF e Primefaces (Desenvolvendo a Adição de Categorias)

--- youtube/augusto_branquinho/wpattern-frameworks-jsf-primefaces/src/main/java/br/com/jkavdev/frameworks/jsf/primefaces/support/category/CategoryAddEditBean.java
+++ youtube/augusto_branquinho/wpattern-frameworks-jsf-primefaces/src/main/java/br/com/jkavdev/frameworks/jsf/primefaces/support/category/CategoryAddEditBean.java
@@ -1,5 +1,59 @@
 package br.com.jkavdev.frameworks.jsf.primefaces.support.category;
 
-public class CategoryAddEditBean {
+import java.util.ResourceBundle;
+
+import javax.faces.application.Application;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.context.WebApplicationContext;
+
+import br.com.jkavdev.frameworks.jsf.primefaces.model.CategoryEntity;
+import br.com.jkavdev.frameworks.jsf.primefaces.model.utils.BaseBeans;
+
+@Scope(value = WebApplicationContext.SCOPE_REQUEST)
+@Named(value = "categoryAddEditBean")
+public class CategoryAddEditBean extends BaseBeans {
+
+	private static final long serialVersionUID = 1L;
+
+	// contexto do primefaces
+	@Inject
+	private FacesContext context;
+	private CategoryEntity category;
+	private String title;
+
+	public void add() {
+		this.title = this.getResourceProperty("labels", "category_add");
+	}
+
+	public void update() {
+		this.title = this.getResourceProperty("labels", "category_update");
+	}
+
+	private String getResourceProperty(String resource, String label) {
+		Application application = this.context.getApplication();
+		ResourceBundle bundle = application.getResourceBundle(this.context, resource);
+		
+		return bundle.getString(label);
+	}
+
+	public CategoryEntity getCategory() {
+		return category;
+	}
+
+	public void setCategory(CategoryEntity category) {
+		this.category = category;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
 }
