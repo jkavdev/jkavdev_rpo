76096b79ab1bed74ba7c2f3ca9693edde5aaefb2
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-04-18 21:19:54 +0000

    v9.3-exclusao-com-many-to-many-v1

--- trunk/algaworks/jpa_hibernate/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/modelo/ModeloCarro.java
+++ trunk/algaworks/jpa_hibernate/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/modelo/ModeloCarro.java
@@ -9,8 +9,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="modelo_carro")
 public class ModeloCarro {
 
 	private Long codigo;
