adc24d9a3abd558cf1c67d236f6b3cad5f29405b
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-04-18 04:09:59 +0000

    v8.9-subqueries-v1

--- trunk/algaworks/jpa_hibernate/locadora-veiculo-web/src/test/java/com/algaworks/curso/jpa2/criteria/ExemplosCriteria.java
+++ trunk/algaworks/jpa_hibernate/locadora-veiculo-web/src/test/java/com/algaworks/curso/jpa2/criteria/ExemplosCriteria.java
@@ -14,6 +14,7 @@ import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Order;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import javax.persistence.criteria.Subquery;
 
 import org.junit.After;
 import org.junit.Before;
@@ -112,93 +113,116 @@ public class ExemplosCriteria {
 		CriteriaQuery<PrecoCarro> criteriaQuery = builder.createQuery(PrecoCarro.class);
 		
 		Root<Carro> carro = criteriaQuery.from(Carro.class);
-		criteriaQuery.select(builder.construct(PrecoCarro.class, 
-						carro.get("placa"), carro.get("valorDiaria")));
-		
+		criteriaQuery.select(builder.construct(PrecoCarro.class, carro.get("placa"), carro.get("valorDiaria")));
+
 		TypedQuery<PrecoCarro> query = manager.createQuery(criteriaQuery);
 		List<PrecoCarro> resultado = query.getResultList();
-		
+
 		for (PrecoCarro precoCarro : resultado) {
 			System.out.println(precoCarro.getPlaca() + " - " + precoCarro.getValor());
 		}
 	}
-	
+
 	@Test
-	public void exemploFuncao(){
+	public void exemploFuncao() {
 		CriteriaBuilder builder = manager.getCriteriaBuilder();
 		CriteriaQuery<Carro> criteriaQuery = builder.createQuery(Carro.class);
-		
+
 		Root<Carro> carro = criteriaQuery.from(Carro.class);
-		Predicate predicate = builder.equal(builder.upper(carro.<String>get("cor")), "azul".toUpperCase());
-		
+		Predicate predicate = builder.equal(builder.upper(carro.<String> get("cor")), "azul".toUpperCase());
+
 		criteriaQuery.select(carro);
 		criteriaQuery.where(predicate);
-		
+
 		TypedQuery<Carro> query = manager.createQuery(criteriaQuery);
 		List<Carro> carros = query.getResultList();
-		
+
 		for (Carro carro2 : carros) {
 			System.out.println(carro2.getPlaca() + " - " + carro2.getCor());
 		}
-		
+
 	}
-	
+
 	@Test
-	public void exemploOrdenacao(){
+	public void exemploOrdenacao() {
 		CriteriaBuilder builder = manager.getCriteriaBuilder();
 		CriteriaQuery<Carro> criteriaQuery = builder.createQuery(Carro.class);
-		
+
 		Root<Carro> carro = criteriaQuery.from(Carro.class);
 		Order order = builder.desc(carro.get("valorDiaria"));
-		
+
 		criteriaQuery.select(carro);
 		criteriaQuery.orderBy(order);
-		
+
 		TypedQuery<Carro> query = manager.createQuery(criteriaQuery);
 		List<Carro> carros = query.getResultList();
-		
+
 		for (Carro carro2 : carros) {
 			System.out.println(carro2.getPlaca() + " - " + carro2.getValorDiaria());
 		}
 	}
-	
+
 	@Test
-	public void exemploJoinEFecth(){
+	public void exemploJoinEFecth() {
 		CriteriaBuilder criteriaBuilder = manager.getCriteriaBuilder();
 		CriteriaQuery<Carro> criteriaQuery = criteriaBuilder.createQuery(Carro.class);
+
+		Root<Carro> carro = criteriaQuery.from(Carro.class);
+		// informando ao criteria para trazer o modelo, junto com o
+		// carro
+		// Join<Carro, ModeloCarro> modelo = (Join) carro.fetch("modelo");
+		// podemos usar o join, mas ele nao traz o modelo
+		// tendo que o jpa fazer todas as querys para trazelos
+		// podemos usar o join, para trazer carros com uma condicao baseada no
+		// modelo
+		Join<Carro, ModeloCarro> modelo = (Join) carro.join("modelo");
+
+		// trazendo apenas os carros com modelo Fiat Siena
+		criteriaQuery.where(criteriaBuilder.equal(modelo.get("descricao"), "Fiat Siena"));
+
+		TypedQuery<Carro> query = manager.createQuery(criteriaQuery);
+		List<Carro> carros = query.getResultList();
+
+		for (Carro c : carros) {
+			System.out.println(c.getPlaca() + " - " + c.getModelo().getDescricao());
+		}
+	}
+
+	@Test
+	public void mediaDasDiariasDosCarros() {
+		CriteriaBuilder criteriaBuilder = manager.getCriteriaBuilder();
+		CriteriaQuery<Double> criteriaQuery = criteriaBuilder.createQuery(Double.class);
+
+		Root<Carro> carro = criteriaQuery.from(Carro.class);
+		criteriaQuery.select(criteriaBuilder.avg(carro.<Double> get("valorDiaria")));
+
+		TypedQuery<Double> query = manager.createQuery(criteriaQuery);
+		Double total = query.getSingleResult();
+
+		System.out.println("Media Diaria: " + total);
+	}
+	
+	@Test
+	public void carrosComValoresAcimaDaMedia(){
+		CriteriaBuilder criteriaBuilder = manager.getCriteriaBuilder();
+		CriteriaQuery<Carro> criteriaQuery = criteriaBuilder.createQuery(Carro.class);
+		Subquery<Double> subquery = criteriaQuery.subquery(Double.class);
 		
 		Root<Carro> carro = criteriaQuery.from(Carro.class);
-		//informando ao criteria para trazer o modelo, junto com o
-		//carro
-//		Join<Carro, ModeloCarro> modelo = (Join) carro.fetch("modelo");
-		//podemos usar o join, mas ele nao traz o modelo
-		//tendo que o jpa fazer todas as querys para trazelos
-		//podemos usar o join, para trazer carros com uma condicao baseada no modelo
-		Join<Carro, ModeloCarro> modelo = (Join) carro.join("modelo");
+		Root<Carro> carroSub = subquery.from(Carro.class);
 		
-		//trazendo apenas os carros com modelo Fiat Siena
-		criteriaQuery.where(criteriaBuilder.equal(modelo.get("descricao"), "Fiat Siena"));
+		subquery.select(criteriaBuilder.avg(carroSub.<Double>get("valorDiaria")));
+		
+		criteriaQuery.select(carro);
+		criteriaQuery.where(criteriaBuilder.greaterThanOrEqualTo(carro.<Double>get("valorDiaria"), subquery));
 		
 		TypedQuery<Carro> query = manager.createQuery(criteriaQuery);
 		List<Carro> carros = query.getResultList();
 		
 		for (Carro c : carros) {
-			System.out.println(c.getPlaca() + " - " + c.getModelo().getDescricao());
+			System.out.println(c.getPlaca() + " - " + c.getValorDiaria());
 		}
+		
 	}
-	
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
