a406bda2dcd3aa8451bc72e0b457265157b64855
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-04-20 02:52:42 +0000

    v11.5-consultas-nativas-v1

---
+++ algaworks/jpa_hibernate/locadora-veiculo-web/src/test/java/com/algaworks/curso/jpa2/criteria/ExemploConsultaNativa.java
@@ -0,0 +1,50 @@
+package com.algaworks.curso.jpa2.criteria;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.algaworks.curso.jpa2.modelo.Carro;
+
+public class ExemploConsultaNativa {
+
+	private static EntityManagerFactory factory;
+	private EntityManager manager;
+
+	@BeforeClass
+	public static void init() {
+		factory = Persistence.createEntityManagerFactory("locadoraVeiculoPU");
+	}
+
+	@Before
+	public void setUp() {
+		this.manager = factory.createEntityManager();
+	}
+
+	@After
+	public void tearDown() {
+		this.manager.close();
+	}
+
+	@Test
+	public void consultaNativa() {
+
+		Query nativeQuery = manager.createNativeQuery("select * from Carro", Carro.class);
+		List<Carro> carros = nativeQuery.getResultList();
+
+		for (Carro carro : carros) {
+			System.out.println(carro.getPlaca());
+		}
+
+	}
+
+}
