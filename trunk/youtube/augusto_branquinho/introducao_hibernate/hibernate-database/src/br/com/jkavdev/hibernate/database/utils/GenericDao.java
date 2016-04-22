a3662aa6199d7add16ef1cc6a091409debff9a0e
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-04-22 02:20:18 +0000

    Parte 06 - Introdução ao Hibernate 06 (Criação dos DAOs)

---
+++ trunk/youtube/augusto_branquinho/introducao_hibernate/hibernate-database/src/br/com/jkavdev/hibernate/database/utils/GenericDao.java
@@ -0,0 +1,12 @@
+package br.com.jkavdev.hibernate.database.utils;
+
+import java.io.Serializable;
+
+//classe que fornecera metodos basicos para os daos
+//classe que recebe genericos
+//recebe uma classe, entidade do banco
+//e uma chave 
+
+public abstract class GenericDao<T, ID extends Serializable> {
+
+}
---
+++ trunk/youtube/augusto_branquinho/introducao_hibernate/hibernate-database/src/br/com/jkavdev/hibernate/database/utils/IGenericDao.java
@@ -0,0 +1,5 @@
+package br.com.jkavdev.hibernate.database.utils;
+
+public interface IGenericDao {
+
+}
