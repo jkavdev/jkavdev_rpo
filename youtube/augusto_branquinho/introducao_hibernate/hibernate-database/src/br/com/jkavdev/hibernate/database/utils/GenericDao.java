28fb727f3b151d491566c00882ee9c54ce153365
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-04-22 02:49:38 +0000

    Parte 07 - Introdução ao Hibernate 07 (Criação dos DAOs)

--- youtube/augusto_branquinho/introducao_hibernate/hibernate-database/src/br/com/jkavdev/hibernate/database/utils/GenericDao.java
+++ youtube/augusto_branquinho/introducao_hibernate/hibernate-database/src/br/com/jkavdev/hibernate/database/utils/GenericDao.java
@@ -1,12 +1,102 @@
 package br.com.jkavdev.hibernate.database.utils;
 
 import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
+import java.util.List;
 
-//classe que fornecera metodos basicos para os daos
+import org.apache.log4j.Logger;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Criterion;
+
 //classe que recebe genericos
 //recebe uma classe, entidade do banco
 //e uma chave 
 
-public abstract class GenericDao<T, ID extends Serializable> {
+public abstract class GenericDao<T, ID extends Serializable> implements IGenericDao<T, ID> {
+
+	private final Logger logger = Logger.getLogger(this.getClass());
+
+	// classe que representa entidade que sera persistida
+	private final Class<T> persistentClass;
+	private Session session;
+
+	@SuppressWarnings("unchecked")
+	public GenericDao() {
+		// retornando a tipo que extende esta classe
+		this.persistentClass = (Class<T>) ((ParameterizedType) getClass() // indicando
+																			// esta
+																			// classe
+				.getGenericSuperclass()) // indicando a classe que extender esta
+											// classe
+						.getActualTypeArguments()[0]; // pega o parametro que
+														// informados pela
+														// classe, no caso
+														// pegara o primeiro
+														// parametro que indica
+														// o tipo da classe
+	}
+
+	// encontrar um elementro pelo id
+	@Override
+	public T findById(ID id) {
+		return null;
+	}
+
+	// lista todos os registros
+	@Override
+	public List<T> findAll() {
+		return null;
+	}
+
+	// salva um registro
+	@Override
+	public ID save(T entity) {
+		return null;
+	}
+
+	// atualiza registro
+	@Override
+	public void update(T entity) {
+	}
+
+	// deleta registro
+	@Override
+	public void delete(T entity) {
+	}
+
+	public void setSession(Session session) {
+		this.session = session;
+	}
+
+	public Session getSession() {
+		// verifico se a sessao esta nula
+		// caso sim, lanco excecao
+		if (this.session == null) {
+			this.logger.error("Session not founde.");
+
+			throw new IllegalStateException("Session has not been set Dao before usage");
+		}
+		return this.session;
+	}
+
+	private Class<T> getPersistentClass() {
+		return this.persistentClass;
+	}
+
+	// metodo que retornar registro de acordo criterios
+	// podendo ser um, varios ou nenhum criterio
+	@SuppressWarnings({ "unchecked", "unused" })
+	private List<T> findByCriteria(Criterion... criterion) {
+		// no caso usaremos sempre os criterios da classe persistente
+		Criteria criteria = getSession().createCriteria(getPersistentClass());
+
+		// adicionando os criterios passados a minha consulta
+		for (Criterion criterios : criterion) {
+			criteria.add(criterios);
+		}
+
+		return criteria.list();
+	}
 
 }
