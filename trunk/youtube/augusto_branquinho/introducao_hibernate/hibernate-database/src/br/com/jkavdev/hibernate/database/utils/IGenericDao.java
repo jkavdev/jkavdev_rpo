d54938abb6a228731a3433e015bf7331777a7cd1
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-04-22 02:49:38 +0000

    Parte 07 - Introdução ao Hibernate 07 (Criação dos DAOs)

--- trunk/youtube/augusto_branquinho/introducao_hibernate/hibernate-database/src/br/com/jkavdev/hibernate/database/utils/IGenericDao.java
+++ trunk/youtube/augusto_branquinho/introducao_hibernate/hibernate-database/src/br/com/jkavdev/hibernate/database/utils/IGenericDao.java
@@ -1,5 +1,28 @@
 package br.com.jkavdev.hibernate.database.utils;
 
-public interface IGenericDao {
+import java.io.Serializable;
+import java.util.List;
+
+//interface que fornecera metodos basicos para os daos
+//classe que recebe genericos
+//recebe uma classe, entidade do banco
+//e uma chave
+
+public interface IGenericDao<T, ID extends Serializable> {
+
+	// encontrar um elementro pelo id
+	T findById(ID id);
+
+	// lista todos os registros
+	List<T> findAll();
+
+	// salva um registro
+	ID save(T entity);
+
+	// atualiza registro
+	void update(T entity);
+
+	// deleta registro
+	void delete(T entity);
 
 }
