67103cfdfd07f9a7fc94ddff9ad3087b8ce422f5
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-04-24 02:25:23 +0000

    Part 1 - Java EE (J2EE) Tutorial for beginners Part 2 - Java EE (J2EE) Tutorial for beginners Part 3 - Java EE (J2EE) Tutorial for beginners Part 4 - Java EE (J2EE) Tutorial for beginners Part 5 - Java EE (J2EE) Tutorial for beginners

---
+++ youtube/artur_vin/servlets_jsp_project/src/main/java/br/com/jkavdev/servlet/servlets/ServletExample.java
@@ -0,0 +1,24 @@
+package br.com.jkavdev.servlet.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class ServletExample extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter writer = response.getWriter();
+		
+		writer.println("Hello");
+		
+	}
+
+}
