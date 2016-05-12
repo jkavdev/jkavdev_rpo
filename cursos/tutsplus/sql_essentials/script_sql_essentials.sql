e01bb76f66fbde3e0432f9ece40101f1df14accf
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-05-12 04:51:42 +0000

    05-inserting-records

--- cursos/tutsplus/sql_essentials/script_sql_essentials.sql
+++ cursos/tutsplus/sql_essentials/script_sql_essentials.sql
@@ -20,5 +20,18 @@ EXPLAIN investigations;
 DESCRIBE investigations;
 DESC investigations;
 
+--inserindo dados na tabela
+--inserindo apenas um registro, com todos os valores da tabela
+INSERT INTO investigations VALUES ("Finnigan's Fault", "Charlotta McOwen", 4);
+--inserindo registro informando quais campos serao persistidos, obedecendo a ordens dos registros
+INSERT INTO investigations(days_to_solve, title, detective) VALUES (5, "The Missing Tree", "Finch Hosky");
+--inserindo registro informando titulo e detetive
+INSERT INTO investigations(title, detective) VALUES ("A Case of Allergies", "Carson Andrews");
+--inserindo varios registro em apenas um comando
+INSERT INTO investigations(days_to_solve, title, detective) VALUES 
+	(1, "The Night of Crimes", "Kirk James"),
+	(8, "Murderer of Chong", "Bradd Stone"),
+	(4, "Killing New York People", "Michael Douglas");
+
 
 
