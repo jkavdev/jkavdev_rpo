6ddd2ad085eb3fd7d13fdee3b5ccfa42772ab297
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-05-12 07:04:53 +0000

    08-primary-key

---
+++ trunk/cursos/tutsplus/sql_essentials/script_sql_primary_keys.sql
@@ -0,0 +1,26 @@
+--trabalhando com chaves primarias
+--chaves primarias nao podem ser modificadas
+--toda tabela deve ter uma
+--identifica unicamente uma linha
+--nao pode ser nula
+--e atribuida quando inserido algum registro
+CREATE TABLE detectives(
+	id int not null auto_increment
+	name varchar(100),
+	phone_number varchar(10),
+	certificationDate date,
+	CONSTRAINT detectives_pk PRIMARY KEY(id)
+);
+
+EXPLAIN detectives;
+
+--inserindo registro qual quando nao especificado a chave primaria obtera o maior numero entre os ids
+INSERT INTO detectives(name, phone_number, certificationDate) VALUES
+	("Carlotta McOwen", "1234567890", "2002-02-23"),
+	("Finch Hosky", "9876543210", "1995-06-18");
+
+--inserindo registro especificando uma chave primaria
+INSERT INTO detectives VALUES
+	(5, "Luther Ellenor", "3216547982", "1997-01-25");
+
+SELECT * FROM detectives;
