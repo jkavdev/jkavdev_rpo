a3868d1da349e37930fb1a335a790889334a1ea1
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-05-12 23:25:30 +0000

    09-altering-tables

--- trunk/cursos/tutsplus/sql_essentials/script_sql_alter_table.sql
+++ trunk/cursos/tutsplus/sql_essentials/script_sql_alter_table.sql
@@ -3,45 +3,55 @@ ALTER TABLE investigations RENAME cases;
 
 SELECT * FROM cases;
 
---adicionando coluna extra
+--adicionando uma coluna 
 ALTER TABLE cases ADD criminal varchar(100);
 
 explain cases;
 SELECT * FROM cases;
 
---removendo coluna
+--removendo uma coluna
 ALTER TABLE cases DROP criminal;
 
 SELECT * FROM cases;
 
---alterando o nome da coluna
+--alterando o nome de uma coluna
 ALTER TABLE detectives CHANGE certificationDate certification_date date;
 
 EXPLAIN detectives;
-EXPLAIN cases;
 
-
+--adicionando id para a tabela, ocorrera erro pois id nao eh chave primaria
 --erro, auto_increment apenas para chaves primarias
 ALTER TABLE cases ADD id int not null auto_increment
 
---adicionando id
+--para resolver, adicionar o campo id
 ALTER TABLE cases ADD id int not null;
 
 EXPLAIN cases;
 SELECT * FROM cases;
 
---atribuindo o id criado como chave primaria da tabela
+--para que id seja uma chave primaria seu campo deve ser diferente dos demais registros
+--alterando valor do id, pois quando criado iniciou com zero
+INSERT INTO cases(id) VALUES(1, 2, 3, 4);
+
+--adicionando chave primaria de acordo com o id que adicionamos
 ALTER TABLE cases ADD PRIMARY KEY(id);
 
 EXPLAIN cases;
 
---sobrescrevendo o atributo id para auto_increment
+--tornando a chave primaria auto incrementavel
 ALTER TABLE cases CHANGE id id int not null auto_increment;
 
-EXPLAIN cases;
 SELECT * FROM cases;
 
-INSERT INTO cases(title, detective, days_to_solve) values
-	("Another Case", "Luther King", 2);
+INSERT INTO cases(title, detective, days_to_solve) VALUES
+	('Another Case', "Luther King", 2);
 
 SELECT * FROM cases;
+
+
+
+
+
+
+
+
