8fe3aa3bcb66cb1d0310e71b6a447bc483adc6a0
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-05-12 04:50:35 +0000

    04-create-table

--- trunk/cursos/tutsplus/sql_essentials/script_sql_essentials.sql
+++ trunk/cursos/tutsplus/sql_essentials/script_sql_essentials.sql
@@ -7,5 +7,18 @@ USE intrepid_detectives;
 --listando todas as tabelas de um banco
 SHOW TABLES;
 
+--criando uma tabela
+--informando o nome e tipo da coluna
+CREATE TABLE investigations(
+	title varchar(100),
+	detective varchar(30),
+	days_to_solve integer
+);
+
+--mostrando detalhes da tabela criada
+EXPLAIN investigations;
+DESCRIBE investigations;
+DESC investigations;
+
 
 
