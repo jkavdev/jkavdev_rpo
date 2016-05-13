19c3c30fae1774153b488e61fb97375ded17eebb
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-05-13 11:40:12 +0000

    15-subqueries-joins

--- trunk/cursos/tutsplus/sql_essentials/script_sql_subqueries_joins.sql
+++ trunk/cursos/tutsplus/sql_essentials/script_sql_subqueries_joins.sql
@@ -1,29 +1,130 @@
-select * from cases where detective_id = 
-	(select id from detectives where first_name = "Calotta") --1;
+--retornando os casos da detetive calotta
+SELECT 
+	* FROM cases 
+	WHERE detective_id = 
+		(SELECT id FROM detectives WHERE first_name = "Calotta") --1;
+--retornando o id da calotta	
+SELECT id 
+	FROM detectives 
+	WHERE first_name = "Calotta" --= 1
+
+--retornando os casos com telefone do detetive comecando com...
+SELECT title 
+	FROM cases 
+	WHERE detective_id 
+		IN(SELECT id FROM detectives WHERE phone_number LIKE '325%');
+
+--retornando os registros dos detetives no qual tiveram um caso com Jim	
+SELECT first_name, last_name 
+	FROM detectives, cases 
+	WHERE detectives.id = detective_id 
+	AND criminal_id 
+		IN(SELECT id FROM criminals WHERE first_name = "Jim");
 	
-select id from detectives where first_name = "Calotta" --= 1
+SELECT * 
+	FROM cases;
 
-select title from cases where detective_id 
-	in(select id from detectives where phone_number like '325%');
+--inserindo registro copiando dados do registro id = 3
+INSERT INTO cases 
+	SELECT 10, detective_id, criminal_id, 'Stale Gun', start_date, hours_to_solve 
+		FROM cases
+	WHERE id = 3;
+
+SELECT * 
+	FROM cases;
+
+SELECT first_name, last_name 
+	FROM detectives;
+
+SELECT first_name, last_name 
+	FROM criminals;
+
+--retornado a uniaos dos registro de detetives e criminosos
+SELECT first_name, last_name 
+	FROM detectives 
+	UNION 
+	SELECT first_name, last_name 
+		FROM criminals;
+
+--retornado a uniaos dos registro de detetives e criminosos, e ordenando pelo primeiro nome
+SELECT first_name, last_name 
+	FROM detectives 
+	UNION 
+	SELECT first_name, last_name 
+		FROM criminals 
+	ORDER BY first_name;
+
+--retornado a uniaos dos registro de detetives e criminosos, e ordenando pelo sobrenome
+SELECT first_name, last_name 
+	FROM detectives 
+	UNION 
+	SELECT first_name, last_name 
+		FROM criminals 
+	ORDER BY last_name DESC;
+
+--nao sera retornado, pois total de colunas devem corresponder entre si
+SELECT * FROM detectives 
+	UNION 
+SELECT * FROM criminals;
+
+
+--retornara registros repetidos, 
+--como as tabelas sao diferentes, o sql fara uma busca de um registro eum tabela para cada registro da outra tabela
+SELECT * 
+	FROM cases, detectives;
+
+SELECT * 
+	FROM cases 
+	CROSS JOIN detectives;
+
+SELECT * 
+	FROM cases 
+	INNER JOIN detectives;
+
+SELECT first_name 
+	FROM cases 
+	CROSS JOIN detectives;
+
+--colunas iguais nas tabelas, erro
+SELECT id 
+	FROM cases 
+	CROSS JOIN detectives;
+
+SELECT cases.id 
+	FROM cases 
+	CROSS JOIN detectives;
+
+--retornando detetives e criminosos em comum
+SELECT detectives.last_name, criminals.last_name 
+	FROM detectives 
+	CROSS JOIN criminals 
+	CROSS JOIN cases 
+		WHERE cases.criminal_id = criminals.id AND cases.detective_id = detectives.id;
 	
-select first_name, last_name from detectives, cases where detectives.id = detective_id and
-	criminal_id in(select id from criminals where first_name = "Jim");
+SELECT first_name, last_name, hours_to_solve 
+	FROM criminals
+	CROSS JOIN cases 
+		WHERE criminals.id = criminal_id AND hours_to_solve > 90;
 	
-select * from cases;
+SELECT first_name, last_name, hours_to_solve 
+	FROM criminals
+	CROSS JOIN cases 
+		WHERE criminals.id = criminal_id AND hours_to_solve > 90 
+	ORDER BY hours_to_solve;	
 
-insert into cases select 10, detective_id, criminal_id, 'Stale Gun', start_date, hours_to_solve from cases
-	where id = 3;
+SELECT first_name, last_name, hours_to_solve 
+	FROM criminals
+	INNER JOIN cases 
+		ON criminals.id = criminal_id AND hours_to_solve > 90;
+	
+SELECT first_name, last_name, hours_to_solve 
+	FROM criminals
+	INNER JOIN cases 
+		ON criminals.id = criminal_id;
 
-select * from cases;
-
-select first_name, last_name from detectives;
-select first_name, last_name from criminals;
-select first_name, last_name from detectives union select first_name, last_name from criminals;
-select first_name, last_name from detectives union select first_name, last_name from criminals order by first_name;
-select first_name, last_name from detectives union select first_name, last_name from criminals order by first_name desc;
-select * from detectives union select * from criminals;
-select * from detectives, cases;
-select * from detectives cross join cases;
-select * from detectives inner join cases;
-select first_name from detectives cross join cases;
-select id from detectives cross join cases;
+SELECT first_name, last_name, sum(hours_to_solve) 
+	FROM criminals
+	INNER JOIN cases 
+		ON criminals.id = criminal_id 
+		WHERE hours_to_solve > 90
+	GROUP BY first_name;
