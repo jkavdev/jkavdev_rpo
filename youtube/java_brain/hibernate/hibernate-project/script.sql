32e5727291c2c976d08cb06ede618b2ff15e07cd
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-05-17 03:57:23 +0000

    Hibernate Tutorial 03 Part 3- Saving Objects using Hibernate

--- youtube/java_brain/hibernate/hibernate-project/script.sql
+++ youtube/java_brain/hibernate/hibernate-project/script.sql
@@ -3,3 +3,16 @@ CREATE DATABASE db_java_brains_hibernate
   WITH ENCODING='UTF8'
        OWNER=postgres
        CONNECTION LIMIT=-1;
+
+--tabela usuario criada       
+CREATE TABLE public.userdetails
+(
+  userid integer NOT NULL,
+  username character varying(255),
+  CONSTRAINT userdetails_pkey PRIMARY KEY (userid)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.userdetails
+  OWNER TO postgres;
