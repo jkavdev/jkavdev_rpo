c2cc2119bbb3d35c683f91c9fb2c9df0a71a8626
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-05-17 04:33:15 +0000

    Hibernate Tutorial 04 - hbm2ddl Configuration and Name Annotations

--- trunk/youtube/java_brain/hibernate/hibernate-project/src/main/java/br/com/jkavdev/hibernate/dto/UserDetails.java
+++ trunk/youtube/java_brain/hibernate/hibernate-project/src/main/java/br/com/jkavdev/hibernate/dto/UserDetails.java
@@ -1,14 +1,31 @@
 package br.com.jkavdev.hibernate.dto;
 
+import java.util.Date;
+
+import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
-@Entity(name = "user_details")
+//aqui estamos atribuindo o nome para a
+//entidade e para a tabela no banco
+//entidade - como o java reconhece a tabela
+//@Entity(name = "user_details")
+@Entity
+
+// atribui nome apenas para a tabela no banco
+@Table(name = "user_details")
 public class UserDetails {
 
 	private int userId;
 	private String userName;
+	private Date joinedDate;
+	private String address;
+	private String description;
 
 	@Id
 	@Column(name = "user_id")
@@ -20,14 +37,43 @@ public class UserDetails {
 		this.userId = userId;
 	}
 
-	//apesar de informarmos o nome, o hibernate busca o valor do get
+	// apesar de informarmos o nome, o hibernate busca o valor do get
 	@Column(name = "user_name")
+	//anotacao padrao que indica que este atributo tem que ser persistido
+	@Basic
 	public String getUserName() {
 		return userName + " valor do getter";
 	}
 
 	public void setUserName(String userName) {
 		this.userName = userName;
+	}
+
+	@Temporal(TemporalType.DATE)
+	public Date getJoinedDate() {
+		return joinedDate;
+	}
+
+	public void setJoinedDate(Date joinedDate) {
+		this.joinedDate = joinedDate;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	//para objetos grandes
+	@Lob
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 }
