3baa1c4404e03d512a493b6cc1615c628a441da5
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-05-23 13:50:38 +0000

    Configuração do CDI

--- projects/simulados/simulados-online/src/main/java/br/com/simulado/dao/InstituicaoDao.java
+++ projects/simulados/simulados-online/src/main/java/br/com/simulado/dao/InstituicaoDao.java
@@ -1,12 +1,19 @@
 package br.com.simulado.dao;
 
+import java.io.Serializable;
+
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
 import br.com.simulado.modelo.Instituicao;
 
-public class InstituicaoDao {
+public class InstituicaoDao implements Serializable {
 
-	EntityManager manager = JpaUtil.createEntityManager();
+	private static final long serialVersionUID = 1L;
+
+	// EntityManager manager = JpaUtil.createEntityManager();
+	@Inject
+	private EntityManager manager;
 
 	public void salvar(Instituicao instituicao) {
 		this.manager.getTransaction().begin();
