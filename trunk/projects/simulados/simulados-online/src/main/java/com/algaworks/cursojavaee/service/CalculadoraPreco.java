d8e8125e9497590ece9e30bcd1ba7b72a5c044df
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-05-24 12:57:38 +0000

    excluindo projeto

--- trunk/projects/simulados/simulados-online/src/main/java/com/algaworks/cursojavaee/service/CalculadoraPreco.java
+++
@@ -1,9 +0,0 @@
-package com.algaworks.cursojavaee.service;
-
-public class CalculadoraPreco {
-
-	public double calcularPreco(int quantidade, double precoUnitario) {
-		return quantidade * precoUnitario;
-	}
-	
-}
