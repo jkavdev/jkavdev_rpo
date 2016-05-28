d69e592a80c6f525cced080566dd7db057c1212d
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-05-28 22:00:35 +0000

    Refatoração do projeto simulados-online

--- projects/simulados/simulados-online-project/src/main/java/br/com/simulados/controller/InstituicaoBean.java
+++ projects/simulados/simulados-online-project/src/main/java/br/com/simulados/controller/InstituicaoBean.java
@@ -2,42 +2,52 @@ package br.com.simulados.controller;
 
 import java.io.Serializable;
 
-import javax.enterprise.context.RequestScoped;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import br.com.simulados.dao.InstituicaoDao;
 import br.com.simulados.modelo.Instituicao;
+import br.com.simulados.service.InstituicaoService;
+import br.com.simulados.service.NegocioException;
+import br.com.simulados.util.jsf.FacesUtil;
 
 @Named
-@RequestScoped
+@ViewScoped
 public class InstituicaoBean implements Serializable {
 
-//	private static final long serialVersionUID = 1L;
-//
-//	private Instituicao instituicao = new Instituicao();
-//	@Inject
-//	private InstituicaoDao instituicaoDao;
-//
-//	public Instituicao getInstituicao() {
-//		return instituicao;
-//	}
-//
-//	public void setInstituicao(Instituicao instituicao) {
-//		this.instituicao = instituicao;
-//	}
-//
-//	public void salvar() {
-//		if (instituicaoDao != null) {
-//			if (!instituicaoDao.existe(instituicao)) {
-//				instituicaoDao.salvar(instituicao);
-//			} else {
-//				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN,
-//						"ALERTA! ", "Instituição já cadastrada no sistema!"));
-//			}
-//		}
-//	}
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private InstituicaoService instituicaoService;
+	private Instituicao instituicao;
+
+	@PostConstruct
+	public void init() {
+		//limpando formulario quando iniciar a tela
+		this.limpar();
+	}
+
+	private void limpar() {
+		//limpar dados do formulario
+		instituicao = new Instituicao();
+	}
+
+	public void salvar() {
+		try {
+			instituicaoService.salvar(instituicao);
+			FacesUtil.addSuccessMessage("Todos os campos devem ser preenchidos!");
+		} catch (NegocioException e) {
+			FacesUtil.addErrorMessage(e.getMessage());
+		}
+	}
+
+	public Instituicao getInstituicao() {
+		return instituicao;
+	}
+
+	public void setInstituicao(Instituicao instituicao) {
+		this.instituicao = instituicao;
+	}
 
 }
