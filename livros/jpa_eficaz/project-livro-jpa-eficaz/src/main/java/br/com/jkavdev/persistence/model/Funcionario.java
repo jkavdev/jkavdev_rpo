f4531106351677f3a2c015c903cad6aca1feae07
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-05-29 04:10:24 +0000

    Criação do projeto do livro jpa-eficaz Definição do persistence.xml

---
+++ livros/jpa_eficaz/project-livro-jpa-eficaz/src/main/java/br/com/jkavdev/persistence/model/Funcionario.java
@@ -0,0 +1,9 @@
+package br.com.jkavdev.persistence.model;
+
+public class Funcionario {
+
+	public Funcionario() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ livros/jpa_eficaz/project-livro-jpa-eficaz/src/main/java/br/com/jkavdev/persistence/model/Veiculo.java
@@ -0,0 +1,9 @@
+package br.com.jkavdev.persistence.model;
+
+public class Veiculo {
+
+	public Veiculo() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
