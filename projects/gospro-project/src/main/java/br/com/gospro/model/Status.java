bad92518bea6e257f3c58507bdcbeead0b80b377
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-06-07 06:02:53 +0000

    Adicionando as entidades de endereco

---
+++ projects/gospro-project/src/main/java/br/com/gospro/model/Status.java
@@ -0,0 +1,36 @@
+package br.com.gospro.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Status implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long codigo;
+	private String descricao;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
