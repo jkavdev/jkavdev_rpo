0ca4ce312ba3732e9d0957e51401e513217ee352
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-06-16 13:01:10 +0000

    Introdução ao Hibernate (Integração da Interface com os Serviços) - Parte 13 - Criação da tela de listagem de categorias

---
+++ trunk/samples/hibernate-project/hibernate-application/src/main/java/br/com/jkavdev/hibernate/application/gui/CategoryGridPanel.java
@@ -0,0 +1,44 @@
+package br.com.jkavdev.hibernate.application.gui;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.DefaultTableModel;
+
+import br.com.jkavdev.hibernate.utils.database.beans.CategoryBean;
+import net.miginfocom.swing.MigLayout;
+
+public class CategoryGridPanel extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+	private JTable tableCategoryModel;
+
+	private final DefaultTableModel tableModel;
+
+	/**
+	 * Create the panel.
+	 */
+	public CategoryGridPanel() {
+
+		String[] columnNames = new String[] { "ID", "Name", "Description" };
+
+		this.tableModel = new DefaultTableModel(columnNames, 0);
+
+		setLayout(new MigLayout("", "[grow]", "[grow]"));
+
+		tableCategoryModel = new JTable(this.tableModel);
+		tableCategoryModel.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		tableCategoryModel.setCellSelectionEnabled(true);
+		add(tableCategoryModel, "cell 0 0 2 2,grow");
+		add(this.tableCategoryModel.getTableHeader(), BorderLayout.NORTH);
+
+	}
+
+	public void addCategoryRow(CategoryBean category) {
+		this.tableModel.addRow(
+				new String[] { String.valueOf(category.getId()), category.getName(), category.getDescription() });
+	}
+
+}
