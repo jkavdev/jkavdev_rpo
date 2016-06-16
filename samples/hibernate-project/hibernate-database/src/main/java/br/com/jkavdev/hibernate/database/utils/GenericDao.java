21ddde207672666d616e776fa672e1744c277569
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-06-16 02:47:47 +0000

    Introdução ao Hibernate (Criação dos DAOs) - Parte 07 - criação dos metodos de processamento de dados do DaoGenerico

--- samples/hibernate-project/hibernate-database/src/main/java/br/com/jkavdev/hibernate/database/utils/GenericDao.java
+++ samples/hibernate-project/hibernate-database/src/main/java/br/com/jkavdev/hibernate/database/utils/GenericDao.java
@@ -1,7 +1,80 @@
 package br.com.jkavdev.hibernate.database.utils;
 
 import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
+import java.util.List;
 
-public abstract class GenericDao<T, PK extends Serializable> {
+import javax.persistence.EntityManager;
+
+import org.apache.log4j.Logger;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Criterion;
+
+public abstract class GenericDao<T, ID extends Serializable> implements IGenericDao<T, ID> {
+
+	private final Logger logger = Logger.getLogger(this.getClass());
+	private final Class<T> persistentClass;
+	private EntityManager manager;
+
+	@SuppressWarnings("unchecked")
+	public GenericDao() {
+		this.persistentClass = (Class<T>) ((ParameterizedType) getClass()
+				.getGenericSuperclass())
+				.getActualTypeArguments()[0];
+	}
+
+	@Override
+	public T findById(ID id) {
+		return null;
+	}
+
+	@Override
+	public List<T> findAll() {
+		return null;
+	}
+
+	@Override
+	public ID save(T entity) {
+		return null;
+	}
+
+	@Override
+	public void update(T entity) {
+
+	}
+
+	@Override
+	public void delete(T entity) {
+	}
+
+	public EntityManager getManager() {
+		return manager;
+	}
+
+	public void setManager(EntityManager manager) {
+		if (this.manager == null) {
+			this.logger.error("EntityManager not Founded.");
+
+			throw new IllegalStateException("EntitManager has not been set DAO before usage");
+		}
+		this.manager = manager;
+	}
+
+	private Class<T> getPersistentClass() {
+		return this.persistentClass;
+	}
+
+	@SuppressWarnings({ "unused", "unchecked" })
+	private List<T> findByCriteria(Criterion... criterions) {
+		Session session = (Session) getManager().getDelegate();
+		Criteria createCriteria = session.createCriteria(getPersistentClass());
+
+		for (Criterion criterion : criterions) {
+			createCriteria.add(criterion);
+		}
+
+		return createCriteria.list();
+	}
 
 }
