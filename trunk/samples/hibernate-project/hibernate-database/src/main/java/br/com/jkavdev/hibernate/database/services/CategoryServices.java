3614940e61090ea84441028433dfe7e1251d2682
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-06-16 04:28:51 +0000

    Introdução ao Hibernate (Criação dos Serviços) - Parte 10 - criação dos daos repectivos nos servicos e criacao do parserDatabase que transformara um entidade em bean e vice-versa.

--- trunk/samples/hibernate-project/hibernate-database/src/main/java/br/com/jkavdev/hibernate/database/services/CategoryServices.java
+++ trunk/samples/hibernate-project/hibernate-database/src/main/java/br/com/jkavdev/hibernate/database/services/CategoryServices.java
@@ -2,10 +2,18 @@ package br.com.jkavdev.hibernate.database.services;
 
 import java.util.List;
 
+import br.com.jkavdev.hibernate.database.interfaces.ICategoryDao;
+import br.com.jkavdev.hibernate.database.interfaces.IDaoFactory;
 import br.com.jkavdev.hibernate.utils.database.beans.CategoryBean;
 import br.com.jkavdev.hibernate.utils.database.interfaces.ICategoryServices;
 
 public class CategoryServices implements ICategoryServices{
+	
+	private final ICategoryDao categoryDao;
+	
+	public CategoryServices(IDaoFactory daoFactory) {
+		this.categoryDao = daoFactory.getCategoryDao();
+	}
 
 	@Override
 	public CategoryBean findById(Long id) {
--- trunk/samples/hibernate-project/hibernate-database/src/main/java/br/com/jkavdev/hibernate/database/utils/DaoFactory.java
+++ trunk/samples/hibernate-project/hibernate-database/src/main/java/br/com/jkavdev/hibernate/database/utils/DaoFactory.java
@@ -42,7 +42,7 @@ public abstract class DaoFactory implements IDaoFactory {
 	}
 
 	private EntityManager getCurrentEntityManager() {
-		return null;
+		return JpaUtil.getEntityManager();
 	}
 
 }
