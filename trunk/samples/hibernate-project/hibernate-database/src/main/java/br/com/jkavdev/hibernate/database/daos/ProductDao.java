920b5c400f185b37c9727c4b14bea60791bc7826
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-06-16 03:09:11 +0000

    Introdução ao Hibernate (Criação dos DAOs) - Parte 08 - criação dos daos e finalizacao da implementacao do daoGenerico

--- trunk/samples/hibernate-project/hibernate-database/src/main/java/br/com/jkavdev/hibernate/database/daos/ProductDao.java
+++ trunk/samples/hibernate-project/hibernate-database/src/main/java/br/com/jkavdev/hibernate/database/daos/ProductDao.java
@@ -1,5 +1,9 @@
 package br.com.jkavdev.hibernate.database.daos;
 
-public class ProductDao {
+import br.com.jkavdev.hibernate.database.entities.ProductEntity;
+import br.com.jkavdev.hibernate.database.interfaces.IProductDao;
+import br.com.jkavdev.hibernate.database.utils.GenericDao;
+
+public class ProductDao extends GenericDao<ProductEntity, Long> implements IProductDao {
 
 }
---
+++ trunk/samples/hibernate-project/hibernate-database/src/main/java/br/com/jkavdev/hibernate/database/interfaces/ICategoryDao.java
@@ -0,0 +1,7 @@
+package br.com.jkavdev.hibernate.database.interfaces;
+
+import br.com.jkavdev.hibernate.database.entities.CategoryEntity;
+
+public interface ICategoryDao extends IGenericDao<CategoryEntity, Long>{
+
+}
