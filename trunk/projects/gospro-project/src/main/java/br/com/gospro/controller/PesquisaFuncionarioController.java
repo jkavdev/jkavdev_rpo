3e1dedf25012057058d37a63157956a5199a96da
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-06-17 13:00:05 +0000

    implementando pesquisa de funcionario

--- trunk/projects/gospro-project/src/main/java/br/com/gospro/controller/PesquisaFuncionarioController.java
+++ trunk/projects/gospro-project/src/main/java/br/com/gospro/controller/PesquisaFuncionarioController.java
@@ -3,26 +3,40 @@ package br.com.gospro.controller;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import br.com.gospro.model.Funcionario;
 import br.com.gospro.service.FuncionarioService;
+import br.com.gospro.service.NegocioException;
+import br.com.gospro.util.jsf.FacesUtil;
 
 @Named
 @ViewScoped
 public class PesquisaFuncionarioController implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Inject
 	private FuncionarioService funcionarioService;
 	private List<Funcionario> funcionarios;
 	private Funcionario funcionarioSelecionado;
 
+	@PostConstruct
 	public void init() {
 		funcionarios = funcionarioService.buscarTodos();
+	}
+
+	public void excluir() {
+		try {
+			funcionarioService.excluir(funcionarioSelecionado);
+			funcionarios.remove(funcionarioSelecionado);
+			FacesUtil.addSuccessMessage("Funcionário " + funcionarioSelecionado.getNome() + " excluído com sucesso!");
+		} catch (NegocioException e) {
+			FacesUtil.addErrorMessage(e.getMessage());
+		}
 	}
 
 	public List<Funcionario> getFuncionarios() {
