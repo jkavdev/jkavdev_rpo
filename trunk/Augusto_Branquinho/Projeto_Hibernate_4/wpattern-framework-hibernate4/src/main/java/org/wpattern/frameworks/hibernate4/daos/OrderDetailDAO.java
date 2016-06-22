4bd78282af9edc30bd28fb5d7d7e65fad789fdee
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-06-22 14:32:25 +0000

    Deletando Projeto_Hibernate_4

--- trunk/Augusto_Branquinho/Projeto_Hibernate_4/wpattern-framework-hibernate4/src/main/java/org/wpattern/frameworks/hibernate4/daos/OrderDetailDAO.java
+++
@@ -1,9 +0,0 @@
-package org.wpattern.frameworks.hibernate4.daos;
-
-import org.wpattern.frameworks.hibernate4.entites.OrderDetailEntity;
-import org.wpattern.frameworks.hibernate4.keys.OrderDetailKey;
-import org.wpattern.frameworks.hibernate4.utils.GenericDAO;
-
-public class OrderDetailDAO extends GenericDAO<OrderDetailEntity, OrderDetailKey> {
-
-}
--- trunk/Augusto_Branquinho/Projeto_Hibernate_4/wpattern-framework-hibernate4/src/main/java/org/wpattern/frameworks/hibernate4/daos/ProductDAO.java
+++
@@ -1,8 +0,0 @@
-package org.wpattern.frameworks.hibernate4.daos;
-
-import org.wpattern.frameworks.hibernate4.entites.ProductEntity;
-import org.wpattern.frameworks.hibernate4.utils.GenericDAO;
-
-public class ProductDAO extends GenericDAO<ProductEntity, Long>{
-
-}
