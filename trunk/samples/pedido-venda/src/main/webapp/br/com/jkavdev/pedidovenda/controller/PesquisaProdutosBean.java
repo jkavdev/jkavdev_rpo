0d85fe449c2446647e9977dc9f6d1d2b6a68b2a2
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-06-22 00:32:01 +0000

    5.1 - Prototipando a pesquisa de produtos - Criando a tela de pesquisa de Produtos e incluindo itens na memoria

---
+++ trunk/samples/pedido-venda/src/main/webapp/br/com/jkavdev/pedidovenda/controller/PesquisaProdutosBean.java
@@ -0,0 +1,30 @@
+package br.com.jkavdev.pedidovenda.controller;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class PesquisaProdutosBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private List<Integer> produtosFiltrados;
+
+	public PesquisaProdutosBean() {
+		this.produtosFiltrados = new ArrayList<>();
+		for (int i = 0; i < 50; i++) {
+			this.produtosFiltrados.add(i);
+			System.out.println(i);
+		}
+	}
+
+	public List<Integer> getProdutosFiltrados() {
+		return this.produtosFiltrados;
+	}
+
+}
