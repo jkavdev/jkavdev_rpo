decce9ba3566b282105412ae9a8493079c06ad22
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-06-22 14:32:25 +0000

    Deletando Projeto_Hibernate_4

--- Augusto_Branquinho/Projeto_Hibernate_4/wpattern-framework-hibernate4-web/src/main/java/org/wpattern/frameworks/hibernate4/daos/OrderDAO.java
+++
@@ -1,8 +0,0 @@
-package org.wpattern.frameworks.hibernate4.daos;
-
-import org.wpattern.frameworks.hibernate4.entites.OrderEntity;
-import org.wpattern.frameworks.hibernate4.utils.GenericDAO;
-
-public class OrderDAO extends GenericDAO<OrderEntity, Long>{
-
-}
