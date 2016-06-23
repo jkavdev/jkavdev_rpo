9177070d4f7be46082b61a893f3296dc38275abf
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-06-23 03:51:33 +0000

    8.4 - Escopos de beans - trabalhando com os escopos do cdi

---
+++ trunk/samples/primefaces-samples/src/main/java/br/com/jkavdev/samples/controller/PrecoProdutoBean.java
@@ -0,0 +1,36 @@
+package br.com.jkavdev.samples.controller;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.com.jkavdev.samples.service.CalculadoraPreco;
+
+@Named
+// escopo padrao, será instanciado toda vez que chamado
+// @Dependent
+// escopo de requisicao http
+// @RequestScoped
+@SessionScoped
+public class PrecoProdutoBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private CalculadoraPreco calculadoraPreco;
+
+	@PostConstruct
+	public void init() {
+		System.out.println("Init PrecoProdutoBean");
+	}
+
+	public double getPreco() {
+		//exebindo objeto proxy que o cdi cria
+		System.out.println(this.calculadoraPreco.getClass());
+		return this.calculadoraPreco.calcularPreco(12, 45.32);
+	}
+
+}
