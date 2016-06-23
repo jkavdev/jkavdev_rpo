bc5aef1c521d46f5e1b3ba9fc49132de9f016b29
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-06-23 03:58:19 +0000

    8.5 - Pontos de injecao - conhecendo os pontos de injeçao

--- trunk/samples/primefaces-samples/src/main/java/br/com/jkavdev/samples/controller/PrecoProdutoBean.java
+++ trunk/samples/primefaces-samples/src/main/java/br/com/jkavdev/samples/controller/PrecoProdutoBean.java
@@ -19,8 +19,17 @@ public class PrecoProdutoBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	@Inject
+	// @Inject
 	private CalculadoraPreco calculadoraPreco;
+
+	public PrecoProdutoBean() {
+	}
+
+	@Inject
+	public PrecoProdutoBean(CalculadoraPreco calculadoraPreco) {
+		System.out.println("Construtor: " + calculadoraPreco.getClass());
+		this.calculadoraPreco = calculadoraPreco;
+	}
 
 	@PostConstruct
 	public void init() {
@@ -28,9 +37,15 @@ public class PrecoProdutoBean implements Serializable {
 	}
 
 	public double getPreco() {
-		//exebindo objeto proxy que o cdi cria
+		// exebindo objeto proxy que o cdi cria
 		System.out.println(this.calculadoraPreco.getClass());
 		return this.calculadoraPreco.calcularPreco(12, 45.32);
+	}
+
+	// @Inject
+	public void setCalculadoraPreco(CalculadoraPreco calculadoraPreco) {
+		System.out.println("setCalculadora: " + calculadoraPreco.getClass());
+		this.calculadoraPreco = calculadoraPreco;
 	}
 
 }
