a325c712746c651ba9b116a9b2c8328e423f60d6
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-06-23 01:06:57 +0000

    6.4 - Dialogo de confirmacao com confirm dialog - Criando exclusao de produtos com o p:confirmDialog

--- trunk/samples/primefaces-samples/src/main/java/br/com/jkavdev/samples/controller/ProdutosBean.java
+++ trunk/samples/primefaces-samples/src/main/java/br/com/jkavdev/samples/controller/ProdutosBean.java
@@ -15,10 +15,24 @@ public class ProdutosBean implements Serializable {
 
 	private List<String> produtos = new ArrayList<>();
 	private String nomeProduto;
+	private String produtoSelecionado;
+
+	public ProdutosBean() {
+
+		this.produtos.add("Feijão");
+		this.produtos.add("Arroz");
+		this.produtos.add("Carne");
+		this.produtos.add("Açucar");
+
+	}
 
 	public void incluirProduto() {
 		this.produtos.add(this.nomeProduto);
 		this.nomeProduto = null;
+	}
+
+	public void excluirProduto() {
+		this.produtos.remove(this.produtoSelecionado);
 	}
 
 	public String getNomeProduto() {
@@ -31,6 +45,14 @@ public class ProdutosBean implements Serializable {
 
 	public List<String> getProdutos() {
 		return this.produtos;
+	}
+
+	public String getProdutoSelecionado() {
+		return this.produtoSelecionado;
+	}
+
+	public void setProdutoSelecionado(String produtoSelecionado) {
+		this.produtoSelecionado = produtoSelecionado;
 	}
 
 }
