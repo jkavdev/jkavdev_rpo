40160c7ce1b79bebee430abf029fc4f13ab00842
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-06-23 12:46:03 +0000

    10.6 - Criando entidades do pedido de venda

---
+++ trunk/samples/pedido-venda/src/main/java/br/com/jkavdev/pedidovenda/model/EnderecoEntrega.java
@@ -0,0 +1,64 @@
+package br.com.jkavdev.pedidovenda.model;
+
+import java.io.Serializable;
+
+public class EnderecoEntrega implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String logradouro;
+	private String numero;
+	private String complemento;
+	private String cidade;
+	private String uf;
+	private String cep;
+
+	public String getLogradouro() {
+		return this.logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getNumero() {
+		return this.numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return this.complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getCidade() {
+		return this.cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getUf() {
+		return this.uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public String getCep() {
+		return this.cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+}
---
+++ trunk/samples/pedido-venda/src/main/java/br/com/jkavdev/pedidovenda/model/Usuario.java
@@ -0,0 +1,9 @@
+package br.com.jkavdev.pedidovenda.model;
+
+public class Usuario {
+
+	public Usuario() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
