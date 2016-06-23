4c70636a33142882eda29b3d9e211246b9e03c8e
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-06-23 06:08:13 +0000

    9.2 - Tratando outras excecoes - Criando negocioException e adicionando tratamento de erro de negocioException

--- trunk/samples/pedido-venda/src/main/java/br/com/jkavdev/pedidovenda/controller/CadastroPedidoBean.java
+++ trunk/samples/pedido-venda/src/main/java/br/com/jkavdev/pedidovenda/controller/CadastroPedidoBean.java
@@ -6,6 +6,8 @@ import java.util.List;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 
+import br.com.jkavdev.pedidovenda.service.NegocioException;
+
 @Named
 @RequestScoped
 public class CadastroPedidoBean {
@@ -15,6 +17,10 @@ public class CadastroPedidoBean {
 	public CadastroPedidoBean() {
 		this.itens = new ArrayList<>();
 		this.itens.add(1);
+	}
+
+	public void salvar() {
+		throw new NegocioException("Pedido não pode ser salvo!");
 	}
 
 	public List<Integer> getItens() {
