5c6275e5a32aa518650f8f6187f5206ff6d6a9ec
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-06-24 02:59:57 +0000

    10.11 - Mapeando entidades do pedido de venda

--- trunk/samples/pedido-venda/src/test/java/br/com/jkavdev/pedidovenda/test/InsertTest.java
+++ trunk/samples/pedido-venda/src/test/java/br/com/jkavdev/pedidovenda/test/InsertTest.java
@@ -45,10 +45,8 @@ public class InsertTest {
 	@Test
 	public void insertCliente() {
 
-		Cliente jhonatan = criaCliente("Jhonatan", "jhonatan@gmail.com",
-				TipoPessoa.FISICA, "04488289100");
-		Endereco itapua = criaEndereco("73754012", "Planaltina", "Casa",
-				"QR 12 CASA 15", "15", "GO");
+		Cliente jhonatan = criaCliente("Jhonatan", "jhonatan@gmail.com", TipoPessoa.FISICA, "04488289100");
+		Endereco itapua = criaEndereco("73754012", "Planaltina", "Casa", "QR 12 CASA 15", "15", "GO");
 
 		jhonatan.adicionaEndereco(itapua);
 
@@ -57,33 +55,69 @@ public class InsertTest {
 		this.manager.persist(jhonatan);
 
 		commit();
+	}
 
+	@Test
+	public void insertGrupo() {
+		Grupo administrador = criaGrupo("Administrador", "Administrador de Rede");
+		Grupo gerente = criaGrupo("Gerente", "Gerente do Sistema");
+		Grupo usuario = criaGrupo("Usuario", "Usuario normal");
+
+		beginTransaction();
+		this.manager.persist(usuario);
+		this.manager.persist(gerente);
+		this.manager.persist(administrador);
+		commit();
+	}
+
+	@Test
+	public void insertUsuario() {
+		List<Grupo> grupos = this.manager.createQuery("from Grupo", Grupo.class).getResultList();
+
+		Usuario ian = criaUsuario("ian@gmail.com", "ian", "123456");
+		Usuario felipe = criaUsuario("felipe@gmail.com", "felipe", "212");
+		Usuario jhonatan = criaUsuario("jhonatan@gmail.com", "jhonatan", "12344");
+
+		beginTransaction();
+
+		for (Grupo grupo : grupos) {
+			if (grupo.getNome().equals("Administrador")) {
+				ian.getGrupos().add(grupo);
+			} else if (grupo.getNome().equals("Gerente")) {
+				ian.getGrupos().add(grupo);
+				felipe.getGrupos().add(grupo);
+			} else if (grupo.getNome().equals("Usuario")) {
+				jhonatan.getGrupos().add(grupo);
+			}
+
+			System.out.println(grupo.getDescricao());
+		}
+
+		this.manager.persist(jhonatan);
+		this.manager.persist(ian);
+		this.manager.persist(felipe);
+
+		commit();
 	}
 
 	@Test
 	public void insertProduto() {
 
-		List<Categoria> categorias = this.manager.createQuery("from Categoria",
-				Categoria.class).getResultList();
+		List<Categoria> categorias = this.manager.createQuery("from Categoria", Categoria.class).getResultList();
 
 		beginTransaction();
 
 		for (Categoria categoria : categorias) {
 			if (categoria.getDescricao().equals("Monitores")) {
-				Produto monitor = criaProduto(categoria, "Monitor", 100,
-						"LL1254", BigDecimal.valueOf(256));
+				Produto monitor = criaProduto(categoria, "Monitor", 100, "LL1254", BigDecimal.valueOf(256));
 				this.manager.persist(monitor);
 			} else if (categoria.getDescricao().equals("Computadores")) {
-				Produto computador = criaProduto(categoria, "Computador", 100,
-						"LL1264", BigDecimal.valueOf(1256));
+				Produto computador = criaProduto(categoria, "Computador", 100, "LL1264", BigDecimal.valueOf(1256));
 				this.manager.persist(computador);
 			} else if (categoria.getDescricao().equals("Acessórios")) {
-				Produto teclado = criaProduto(categoria, "Teclado", 100,
-						"LL1274", BigDecimal.valueOf(26));
-				Produto fonte = criaProduto(categoria, "Fonte", 100, "LL1284",
-						BigDecimal.valueOf(26));
-				Produto mouse = criaProduto(categoria, "Mouse", 100, "LL1294",
-						BigDecimal.valueOf(56));
+				Produto teclado = criaProduto(categoria, "Teclado", 100, "LL1274", BigDecimal.valueOf(26));
+				Produto fonte = criaProduto(categoria, "Fonte", 100, "LL1284", BigDecimal.valueOf(26));
+				Produto mouse = criaProduto(categoria, "Mouse", 100, "LL1294", BigDecimal.valueOf(56));
 				this.manager.persist(teclado);
 				this.manager.persist(fonte);
 				this.manager.persist(mouse);
@@ -108,57 +142,10 @@ public class InsertTest {
 		ItemPedido item1 = criaItemPedido(produto, 2, BigDecimal.valueOf(150), pedido);
 
 		pedido.adicionaItem(item1);
-		
+
 		beginTransaction();
 		this.manager.persist(pedido);
 		commit();
-	}
-
-	@Test
-	public void insertGrupo() {
-		Grupo administrador = criaGrupo("Administrador",
-				"Administrador de Rede");
-		Grupo gerente = criaGrupo("Gerente", "Gerente do Sistema");
-		Grupo usuario = criaGrupo("Usuario", "Usuario normal");
-
-		beginTransaction();
-		this.manager.persist(usuario);
-		this.manager.persist(gerente);
-		this.manager.persist(administrador);
-		commit();
-	}
-
-	@Test
-	public void insertUsuario() {
-		List<Grupo> grupos = this.manager
-				.createQuery("from Grupo", Grupo.class).getResultList();
-
-		Usuario ian = criaUsuario("ian@gmail.com", "ian", "123456");
-		Usuario felipe = criaUsuario("felipe@gmail.com", "felipe", "212");
-		Usuario jhonatan = criaUsuario("jhonatan@gmail.com", "jhonatan",
-				"12344");
-
-		beginTransaction();
-
-		for (Grupo grupo : grupos) {
-			if (grupo.getNome().equals("Administrador")) {
-				ian.getGrupos().add(grupo);
-			} else if (grupo.getNome().equals("Gerente")) {
-				ian.getGrupos().add(grupo);
-				felipe.getGrupos().add(grupo);
-			} else if (grupo.getNome().equals("Usuario")) {
-				jhonatan.getGrupos().add(grupo);
-			}
-
-			System.out.println(grupo.getDescricao());
-		}
-
-		this.manager.persist(jhonatan);
-		this.manager.persist(ian);
-		this.manager.persist(felipe);
-
-		commit();
-
 	}
 
 	public void beginTransaction() {
@@ -187,8 +174,7 @@ public class InsertTest {
 
 	}
 
-	public Produto criaProduto(Categoria categoria, String nome,
-			Integer quantidadeEstoque, String sku, BigDecimal valorUnitario) {
+	public Produto criaProduto(Categoria categoria, String nome, Integer quantidadeEstoque, String sku, BigDecimal valorUnitario) {
 		Produto produto = new Produto();
 		produto.setCategoria(categoria);
 		produto.setNome(nome);
@@ -199,8 +185,7 @@ public class InsertTest {
 		return produto;
 	}
 
-	public ItemPedido criaItemPedido(Produto produto, Integer quantidade,
-			BigDecimal valorUnitario, Pedido pedido) {
+	public ItemPedido criaItemPedido(Produto produto, Integer quantidade, BigDecimal valorUnitario, Pedido pedido) {
 		ItemPedido itemPedido = new ItemPedido();
 		itemPedido.setProduto(produto);
 		itemPedido.setQuantidade(quantidade);
@@ -229,8 +214,7 @@ public class InsertTest {
 		return pedido;
 	}
 
-	public EnderecoEntrega criaEnderecoEntrega(String cep, String cidade,
-			String complemento, String logradouro, String numero, String uf) {
+	public EnderecoEntrega criaEnderecoEntrega(String cep, String cidade, String complemento, String logradouro, String numero, String uf) {
 		EnderecoEntrega endereco = new EnderecoEntrega();
 		endereco.setCep(cep);
 		endereco.setCidade(cidade);
@@ -242,8 +226,7 @@ public class InsertTest {
 		return endereco;
 	}
 
-	public Endereco criaEndereco(String cep, String cidade, String complemento,
-			String logradouro, String numero, String uf) {
+	public Endereco criaEndereco(String cep, String cidade, String complemento, String logradouro, String numero, String uf) {
 		Endereco endereco = new Endereco();
 		endereco.setCep(cep);
 		endereco.setCidade(cidade);
@@ -255,8 +238,7 @@ public class InsertTest {
 		return endereco;
 	}
 
-	private Cliente criaCliente(String nome, String email,
-			TipoPessoa tipoPessoa, String documentoReceitaFederal) {
+	private Cliente criaCliente(String nome, String email, TipoPessoa tipoPessoa, String documentoReceitaFederal) {
 		Cliente cliente = new Cliente();
 		cliente.setNome(nome);
 		cliente.setEmail(email);
