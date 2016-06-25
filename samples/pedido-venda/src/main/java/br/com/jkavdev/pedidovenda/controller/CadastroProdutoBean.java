5a729693e3147037be066aeb83fb46ae59788d48
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-06-25 18:41:36 +0000

    12.1 - Criando um produtor cdi para injetar entity manager

--- samples/pedido-venda/src/main/java/br/com/jkavdev/pedidovenda/controller/CadastroProdutoBean.java
+++ samples/pedido-venda/src/main/java/br/com/jkavdev/pedidovenda/controller/CadastroProdutoBean.java
@@ -5,10 +5,9 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 import br.com.jkavdev.pedidovenda.model.Categoria;
 import br.com.jkavdev.pedidovenda.model.Produto;
@@ -19,22 +18,18 @@ public class CadastroProdutoBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Inject
+	private EntityManager manager;
 	private Produto produto;
 	private List<Categoria> categoriasRaizes;
 
 	public CadastroProdutoBean() {
 		this.produto = new Produto();
 	}
-	
-	@PostConstruct
-	public void init(){
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("pedidoPU");
-		EntityManager manager = factory.createEntityManager();
 
+	@PostConstruct
+	public void init() {
 		categoriasRaizes = manager.createQuery("from Categoria", Categoria.class).getResultList();
-		
-		manager.close();
-		factory.close();
 	}
 
 	public void salvar() {
