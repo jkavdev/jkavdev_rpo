d8ce286254de01f658accbee45ebf28796218885
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-06-25 18:41:36 +0000

    12.1 - Criando um produtor cdi para injetar entity manager

---
+++ trunk/samples/pedido-venda/src/main/java/br/com/jkavdev/pedidovenda/util/jpa/EntityManagerProducer.java
@@ -0,0 +1,30 @@
+package br.com.jkavdev.pedidovenda.util.jpa;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Disposes;
+import javax.enterprise.inject.Produces;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+@ApplicationScoped
+public class EntityManagerProducer {
+
+	EntityManagerFactory factory;
+
+	public EntityManagerProducer() {
+		factory = Persistence.createEntityManagerFactory("pedidoPU");
+	}
+
+	@Produces
+	@RequestScoped
+	public EntityManager createEntityManager() {
+		return factory.createEntityManager();
+	}
+
+	public void closeEntityManager(@Disposes EntityManager manager) {
+		manager.close();
+	}
+
+}
