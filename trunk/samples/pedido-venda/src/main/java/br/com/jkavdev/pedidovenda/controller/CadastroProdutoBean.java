04425d3dbe7552d80f71d6d501822ced9a466669
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-06-25 18:34:35 +0000

    12.1 - Criando um produtor cdi para injetar entity manager

--- trunk/samples/pedido-venda/src/main/java/br/com/jkavdev/pedidovenda/controller/CadastroProdutoBean.java
+++ trunk/samples/pedido-venda/src/main/java/br/com/jkavdev/pedidovenda/controller/CadastroProdutoBean.java
@@ -1,10 +1,16 @@
 package br.com.jkavdev.pedidovenda.controller;
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
+import br.com.jkavdev.pedidovenda.model.Categoria;
 import br.com.jkavdev.pedidovenda.model.Produto;
 
 @Named
@@ -14,9 +20,21 @@ public class CadastroProdutoBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Produto produto;
+	private List<Categoria> categoriasRaizes;
 
 	public CadastroProdutoBean() {
 		this.produto = new Produto();
+	}
+	
+	@PostConstruct
+	public void init(){
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("pedidoPU");
+		EntityManager manager = factory.createEntityManager();
+
+		categoriasRaizes = manager.createQuery("from Categoria", Categoria.class).getResultList();
+		
+		manager.close();
+		factory.close();
 	}
 
 	public void salvar() {
@@ -29,6 +47,10 @@ public class CadastroProdutoBean implements Serializable {
 
 	public void setProduto(Produto produto) {
 		this.produto = produto;
+	}
+
+	public List<Categoria> getCategoriasRaizes() {
+		return categoriasRaizes;
 	}
 
 }
