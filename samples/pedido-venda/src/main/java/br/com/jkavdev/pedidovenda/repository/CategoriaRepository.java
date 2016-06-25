0e1a18305cc0d832e87c95e25869119ededca6b9
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-06-25 18:51:10 +0000

    12.2 - Implementando o repositorio de categorias

---
+++ samples/pedido-venda/src/main/java/br/com/jkavdev/pedidovenda/repository/CategoriaRepository.java
@@ -0,0 +1,22 @@
+package br.com.jkavdev.pedidovenda.repository;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import br.com.jkavdev.pedidovenda.model.Categoria;
+
+public class CategoriaRepository implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private EntityManager manager;
+
+	public List<Categoria> raizes() {
+		return manager.createQuery("from Categoria", Categoria.class).getResultList();
+	}
+
+}
