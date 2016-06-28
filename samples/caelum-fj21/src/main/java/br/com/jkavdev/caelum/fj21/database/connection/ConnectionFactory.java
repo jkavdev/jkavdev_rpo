f2f556c7490d73f243ead1f04f1db6c8762204ed
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-06-28 15:30:58 +0000

    Criação do ConnectionFactory e realização do teste de conexão

---
+++ samples/caelum-fj21/src/main/java/br/com/jkavdev/caelum/fj21/database/connection/ConnectionFactory.java
@@ -0,0 +1,22 @@
+package br.com.jkavdev.caelum.fj21.database.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+	private String password = "123456";
+	private String user = "jkavdev";
+	private String url = "jdbc:mysql://localhost:3306/db_caelum_fj21";
+
+	public Connection getConnection() throws NegocioException {
+
+		try {
+			return DriverManager.getConnection(url, user, password);
+		} catch (SQLException e) {
+			throw new NegocioException(e);
+		}
+	}
+
+}
---
+++ samples/caelum-fj21/src/test/java/br/com/jkavdev/caelum/fj21/database/connection/ConnectionFactoryTest.java
@@ -0,0 +1,28 @@
+package br.com.jkavdev.caelum.fj21.database.connection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+public class ConnectionFactoryTest {
+
+	@Test
+	public void testConnection() {
+		try {
+			Connection connection = new ConnectionFactory().getConnection();
+
+			System.out.println("Conexao aberta");
+
+			connection.close();
+			
+		} catch (NegocioException e) {
+			System.out.println("Erro ao abrir conecão:");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			System.out.println("Erro ao fechar conecão:");
+			e.printStackTrace();
+		}
+	}
+
+}
