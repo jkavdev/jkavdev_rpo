a0a7fcfcb6d1583eaa0c0a27c5507aba717db027
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-06-28 03:33:04 +0000

    12.8 - Implementando a pesquisa de produtos

--- trunk/samples/pedido-venda/src/main/java/br/com/jkavdev/pedidovenda/controller/PesquisaProdutosBean.java
+++ trunk/samples/pedido-venda/src/main/java/br/com/jkavdev/pedidovenda/controller/PesquisaProdutosBean.java
@@ -1,30 +1,41 @@
 package br.com.jkavdev.pedidovenda.controller;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.enterprise.context.RequestScoped;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
+import br.com.jkavdev.pedidovenda.model.Produto;
+import br.com.jkavdev.pedidovenda.repository.ProdutoRepository;
+import br.com.jkavdev.pedidovenda.repository.filter.ProdutoFilter;
+
 @Named
-@RequestScoped
+@ViewScoped
 public class PesquisaProdutosBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	private List<Integer> produtosFiltrados;
+	@Inject
+	private ProdutoRepository produtoRepository;
+	private List<Produto> produtosFiltrados;
+	private ProdutoFilter filtro;
 
 	public PesquisaProdutosBean() {
-		this.produtosFiltrados = new ArrayList<>();
-		for (int i = 0; i < 50; i++) {
-			this.produtosFiltrados.add(i);
-			System.out.println(i);
-		}
+		this.filtro = new ProdutoFilter();
 	}
 
-	public List<Integer> getProdutosFiltrados() {
+	public void pesquisar() {
+		this.produtosFiltrados = this.produtoRepository.filtrados(filtro);
+	}
+
+	public List<Produto> getProdutosFiltrados() {
 		return this.produtosFiltrados;
+	}
+
+	public ProdutoFilter getFiltro() {
+		return filtro;
 	}
 
 }
--- trunk/samples/pedido-venda/src/main/java/br/com/jkavdev/pedidovenda/repository/ProdutoRepository.java
+++ trunk/samples/pedido-venda/src/main/java/br/com/jkavdev/pedidovenda/repository/ProdutoRepository.java
@@ -1,12 +1,21 @@
 package br.com.jkavdev.pedidovenda.repository;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 
+import org.apache.commons.lang3.StringUtils;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+
 import br.com.jkavdev.pedidovenda.model.Produto;
+import br.com.jkavdev.pedidovenda.repository.filter.ProdutoFilter;
 
 public class ProdutoRepository implements Serializable {
 
@@ -30,6 +39,23 @@ public class ProdutoRepository implements Serializable {
 			return null;
 		}
 
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Produto> filtrados(ProdutoFilter filtro) {
+		Session session = this.manager.unwrap(Session.class);
+//		Session session = (Session) this.manager.getDelegate();
+		Criteria criteria = session.createCriteria(Produto.class);
+
+		if (StringUtils.isNotBlank(filtro.getSku())) {
+			criteria.add(Restrictions.eq("sku", filtro.getSku()));
+		}
+
+		if (StringUtils.isNotBlank(filtro.getNome())) {
+			criteria.add(Restrictions.ilike("nome", filtro.getNome(), MatchMode.ANYWHERE));
+		}
+
+		return criteria.addOrder(Order.asc("nome")).list();
 	}
 
 }
