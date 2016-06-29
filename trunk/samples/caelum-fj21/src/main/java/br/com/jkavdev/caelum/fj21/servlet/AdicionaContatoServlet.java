61c06bf20576bc5a42636caa3d27fb407c4bbe64
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-06-29 19:12:54 +0000

    Utilizando o getDispatcher

--- trunk/samples/caelum-fj21/src/main/java/br/com/jkavdev/caelum/fj21/servlet/AdicionaContatoServlet.java
+++ trunk/samples/caelum-fj21/src/main/java/br/com/jkavdev/caelum/fj21/servlet/AdicionaContatoServlet.java
@@ -1,7 +1,6 @@
 package br.com.jkavdev.caelum.fj21.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.text.ParseException;
 import java.util.Calendar;
 
@@ -16,40 +15,33 @@ import br.com.jkavdev.caelum.fj21.model.Contato;
 import br.com.jkavdev.caelum.fj21.util.data.DataUtil;
 
 @WebServlet("/contato/AdicionaContato")
-public class AdicionaContatoServlet extends HttpServlet{
+public class AdicionaContatoServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-		PrintWriter out = resp.getWriter();
-		
+
 		String nome = req.getParameter("nome");
 		String email = req.getParameter("email");
 		String endereco = req.getParameter("endereco");
 		String dataNascimentoTexto = req.getParameter("dataNascimento");
 		Calendar dataNascimento = Calendar.getInstance();
-		
+
 		try {
 			dataNascimento = DataUtil.formataDataCalendar(dataNascimentoTexto);
 		} catch (ParseException e) {
 			System.out.println("Erro ao formatar data: " + e);
 		}
-		
+
 		Contato contato = createContato(nome, email, endereco, dataNascimento);
-		
+
 		ContatoDao contatoDao = new ContatoDao();
-		
+
 		if (contato.getNome() != null) {
 			contatoDao.insert(contato);
 			
-			// imprime o nome do contato que foi adicionado
-			out.println("<html>");
-			out.println("<body>");
-			out.println("Contato " + contato.getNome() + " adicionado com sucesso!");
-			out.println("</body>");
-			out.println("</html>");
+			req.getRequestDispatcher("/contato/contato-adicionado.jsp").forward(req, resp);
 		}
 	}
 
