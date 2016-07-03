7fbfbaac2ddfe4c3f393e92270939712fcbe0776
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-07-03 20:49:50 +0000

    Adicionando tela de cadastro de imoveis

---
+++ trunk/samples/imobiliaria/src/main/java/br/com/jkavdev/algaworks/spring/model/Imovel.java
@@ -0,0 +1,111 @@
+package br.com.jkavdev.algaworks.spring.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "imoveis")
+public class Imovel {
+
+	private Long codigo;
+	private String descricao;
+	private BigDecimal valor;
+	private String bairro;
+	private TipoImovel tipo;
+	private BigDecimal area;
+	private Ambiente ambiente = new Ambiente();
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	@Enumerated(EnumType.STRING)
+	public TipoImovel getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoImovel tipo) {
+		this.tipo = tipo;
+	}
+
+	public BigDecimal getArea() {
+		return area;
+	}
+
+	public void setArea(BigDecimal area) {
+		this.area = area;
+	}
+
+	@Embedded
+	public Ambiente getAmbiente() {
+		return ambiente;
+	}
+
+	public void setAmbiente(Ambiente ambiente) {
+		this.ambiente = ambiente;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Imovel other = (Imovel) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+
+}
