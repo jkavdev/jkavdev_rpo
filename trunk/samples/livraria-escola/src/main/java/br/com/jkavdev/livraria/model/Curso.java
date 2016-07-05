eccc36138474d529300fe59eadf8f33845b93aab
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-07-05 18:48:42 +0000

    Criação do relacionamento entre curso e livros indicados

--- trunk/samples/livraria-escola/src/main/java/br/com/jkavdev/livraria/model/Curso.java
+++ trunk/samples/livraria-escola/src/main/java/br/com/jkavdev/livraria/model/Curso.java
@@ -1,7 +1,14 @@
 package br.com.jkavdev.livraria.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import br.com.jkavdev.livraria.utils.jpa.BaseEntity;
@@ -14,6 +21,17 @@ public class Curso extends BaseEntity {
 
 	private String nomeCurso;
 	private String descricao;
+	private List<Livro> livros;
+
+	public Curso(String nomeCurso, String descricao) {
+		this();
+		this.nomeCurso = nomeCurso;
+		this.descricao = descricao;
+	}
+
+	public Curso() {
+		super();
+	}
 
 	@Column(name = "course_name", length = 100, nullable = false)
 	public String getNomeCurso() {
@@ -31,6 +49,26 @@ public class Curso extends BaseEntity {
 
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
+	}
+
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(
+			name = "livros_indicados", 
+			joinColumns = @JoinColumn(name = "livro_id", nullable = false), 
+			inverseJoinColumns = @JoinColumn(name = "curso_id", nullable = false))
+	public List<Livro> getLivros() {
+		return livros;
+	}
+
+	public void setLivros(List<Livro> livros) {
+		this.livros = livros;
+	}
+
+	public void adicionaLivroIndicado(Livro indicado) {
+		if(livros == null) {
+			this.livros = new ArrayList<>();
+		}
+		this.livros.add(indicado);
 	}
 
 }
---
+++ trunk/samples/livraria-escola/src/test/java/br/com/jkavdev/livraria/test/InsertTestCurso.java
@@ -0,0 +1,80 @@
+package br.com.jkavdev.livraria.test;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.jkavdev.livraria.model.Curso;
+import br.com.jkavdev.livraria.model.Livro;
+
+public class InsertTestCurso {
+
+	private EntityManagerFactory factory;
+	private EntityManager manager;
+
+	@Before
+	public void init() {
+
+		factory = Persistence.createEntityManagerFactory("livrariaPU");
+		manager = factory.createEntityManager();
+
+	}
+
+	@Test
+	public void insertCursoSemLivros() {
+
+		beginTransaction();
+
+		Curso arquitetura = criaCurso("Arquitetura", "Arquitetura de Interior");
+		Curso engenhariaMecanica = criaCurso("Engenharia Mecanica", "Engenharia Mecanica de Carros");
+
+		this.manager.persist(engenhariaMecanica);
+		this.manager.persist(arquitetura);
+
+		commit();
+
+	}
+
+	@Test
+	public void insertCursoComLivro() {
+
+		beginTransaction();
+
+		Curso analise = criaCurso("Analise de Desenvolvimento", "Desenvolvimento de Sistemas");
+		Curso direita = criaCurso("Direito", "Direito Penal");
+
+		List<Livro> livros = manager.createQuery("from Livro", Livro.class) .getResultList();
+
+		for (Livro livro : livros) {
+			if (livro.getId() == 1) {
+				analise.adicionaLivroIndicado(livro);
+			} else if (livro.getId() == 2) {
+				direita.adicionaLivroIndicado(livro);
+			}
+		}
+
+		this.manager.persist(analise);
+		this.manager.persist(direita);
+
+		commit();
+
+	}
+
+	public Curso criaCurso(String nomeCurso, String descricao) {
+		return new Curso(nomeCurso, descricao);
+	}
+
+	public void commit() {
+		manager.getTransaction().commit();
+	}
+
+	public void beginTransaction() {
+		manager.getTransaction().begin();
+	}
+
+}
