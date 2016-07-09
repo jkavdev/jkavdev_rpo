002521f077e54f8ea5f6d116561bf639c318324f
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-07-09 04:46:40 +0000

    Condiguração do Spring será no xml, por enquanto

--- trunk/samples/livraria-escola/src/main/java/br/com/jkavdev/livraria/config/AppSecInitializer.java
+++
@@ -1,7 +0,0 @@
-package br.com.jkavdev.livraria.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class AppSecInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
