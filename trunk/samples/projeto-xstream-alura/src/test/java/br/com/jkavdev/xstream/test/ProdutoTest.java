3188880e860fcf0733a1c0351b530b7c8e55d693
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-07-12 05:48:04 +0000

    exemplos de alias

--- trunk/samples/projeto-xstream-alura/src/test/java/br/com/jkavdev/xstream/test/ProdutoTest.java
+++ trunk/samples/projeto-xstream-alura/src/test/java/br/com/jkavdev/xstream/test/ProdutoTest.java
@@ -17,14 +17,18 @@ public class ProdutoTest {
 				"<produto>\n"
 					+ "  <nome>Geladeira</nome>\n"
 					+ "  <preco>1000.0</preco>\n"
-					+ "  <descricao>Geladeira duas portas</descricao>\n"
+					+ "  <descrição>Geladeira duas portas</descrição>\n"
 				+ "</produto>";
 		
 		Produto geladeira = new Produto("Geladeira", 1000.0, "Geladeira duas portas");
 		
 		XStream xstream = new XStream();
 		
+		//criando apelido para a tag produto
 		xstream.alias("produto", Produto.class);
+		
+		//criando um apelido para um campo
+		xstream.aliasField("descrição", Produto.class, "descricao");
 		
 		String xmlGerado = xstream.toXML(geladeira);
 		
