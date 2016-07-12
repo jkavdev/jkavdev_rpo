9fdcb7a48ae731d94a7111d7657747b18cfd13b9
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-07-12 05:53:58 +0000

    Atribuindo valores

--- samples/projeto-xstream-alura/src/main/java/br/com/jkavdev/xstream/model/Produto.java
+++ samples/projeto-xstream-alura/src/main/java/br/com/jkavdev/xstream/model/Produto.java
@@ -2,37 +2,15 @@ package br.com.jkavdev.xstream.model;
 
 public class Produto {
 
+	private int codigo;
 	private String nome;
 	private double preco;
 	private String descricao;
 
-	public Produto(String nome, double preco, String descricao) {
+	public Produto(int codigo, String nome, double preco, String descricao) {
+		this.codigo = codigo;
 		this.nome = nome;
 		this.preco = preco;
-		this.descricao = descricao;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public double getPreco() {
-		return preco;
-	}
-
-	public void setPreco(double preco) {
-		this.preco = preco;
-	}
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
 
