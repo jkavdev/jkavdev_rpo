359eb633490a0400530d2fe11e759ddd92bb95ec
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-07-12 13:20:51 +0000

    Criação do proverdor de usuarios e tela do login

---
+++ trunk/samples/livraria-escola/src/main/java/br/com/jkavdev/livraria/security/UsuarioSistema.java
@@ -0,0 +1,28 @@
+package br.com.jkavdev.livraria.security;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import br.com.jkavdev.livraria.model.Usuario;
+
+public class UsuarioSistema extends User {
+
+	private static final long serialVersionUID = 1L;
+
+	private Usuario usuario;
+
+	public UsuarioSistema(Usuario usuario, Collection<? extends GrantedAuthority> authorities) {
+
+		super(usuario.getEmail(), usuario.getSenha(), authorities);
+
+		this.usuario = usuario;
+
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+}
