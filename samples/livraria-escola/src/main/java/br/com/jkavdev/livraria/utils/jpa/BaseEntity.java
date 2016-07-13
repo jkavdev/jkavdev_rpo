ccf3fe00aab4966fd7f413539c454dea90b7a3ba
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-07-13 12:49:18 +0000

    Modificando as configuração

--- samples/livraria-escola/src/main/java/br/com/jkavdev/livraria/utils/jpa/BaseEntity.java
+++ samples/livraria-escola/src/main/java/br/com/jkavdev/livraria/utils/jpa/BaseEntity.java
@@ -23,15 +23,38 @@ public class BaseEntity implements Serializable {
 		this.id = id;
 	}
 
-	// @Override
-	// public String toString() {
-	// return ToStringBuilder.reflectionToString(this,
-	// ToStringStyle.MULTI_LINE_STYLE);
-	// }
-	//
-	// @Override
-	// public boolean equals(Object obj) {
-	// return EqualsBuilder.reflectionEquals(this, obj);
-	// }
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("BaseEntity [id=").append(id).append("]");
+		
+		return builder.toString();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BaseEntity other = (BaseEntity) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 
 }
