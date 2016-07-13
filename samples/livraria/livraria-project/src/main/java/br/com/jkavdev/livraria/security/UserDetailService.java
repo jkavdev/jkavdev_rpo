7905165e8dd134eee402b476555d08ff62805fbe
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-07-13 21:23:11 +0000

    Adicionando restrição ao acesso das páginas

---
+++ samples/livraria/livraria-project/src/main/java/br/com/jkavdev/livraria/security/UserDetailService.java
@@ -0,0 +1,48 @@
+package br.com.jkavdev.livraria.security;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import br.com.jkavdev.livraria.model.Grupo;
+import br.com.jkavdev.livraria.model.Usuario;
+import br.com.jkavdev.livraria.repository.IUsuarioRepository;
+
+@Service
+public class UserDetailService implements UserDetailsService {
+
+	@Autowired
+	IUsuarioRepository usuarioRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+		Usuario usuario = usuarioRepository.porEmail(email);
+		UsuarioSistema user = null;
+
+		if (usuario != null) {
+			user = new UsuarioSistema(usuario, getGrupos(usuario));
+		}
+
+		return user;
+	}
+
+	private Collection<? extends GrantedAuthority> getGrupos(Usuario usuario) {
+		
+		List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+
+		for (Grupo grupo : usuario.getGrupos()) {
+			authorities.add(new SimpleGrantedAuthority(grupo.getNome().toUpperCase()));
+		}
+
+		return authorities;
+	}
+}
--- samples/livraria/livraria-project/src/main/java/br/com/jkavdev/livraria/utils/jpa/BaseEntity.java
+++ samples/livraria/livraria-project/src/main/java/br/com/jkavdev/livraria/utils/jpa/BaseEntity.java
@@ -1,17 +1,21 @@
 package br.com.jkavdev.livraria.utils.jpa;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.Version;
 
 @MappedSuperclass
 public class BaseEntity implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+
 	private Long id;
+	private int version;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,15 +27,36 @@ public class BaseEntity implements Serializable {
 		this.id = id;
 	}
 
-	// @Override
-	// public String toString() {
-	// return ToStringBuilder.reflectionToString(this,
-	// ToStringStyle.MULTI_LINE_STYLE);
-	// }
-	//
-	// @Override
-	// public boolean equals(Object obj) {
-	// return EqualsBuilder.reflectionEquals(this, obj);
-	// }
+	@Version
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (this.id == null) {
+			return false;
+		}
+
+		if (obj instanceof BaseEntity && obj.getClass().equals(getClass())) {
+			return this.id.equals(((BaseEntity) obj).id);
+		}
+
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 5;
+		hash = 43 * hash + Objects.hashCode(this.id);
+		return hash;
+	}
 
 }
