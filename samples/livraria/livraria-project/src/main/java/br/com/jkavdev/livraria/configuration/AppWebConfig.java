2ee05aa71bc77d58a5b033773cdd7c3af4e333dc
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-07-13 18:02:23 +0000

    Criação do projeto Livraria com Spring e Thymeleaf

---
+++ samples/livraria/livraria-project/src/main/java/br/com/jkavdev/livraria/configuration/AppWebConfig.java
@@ -0,0 +1,62 @@
+package br.com.jkavdev.livraria.configuration;
+
+import java.util.Locale;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
+
+import br.com.jkavdev.livraria.controller.IndexController;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackageClasses = { IndexController.class })
+public class AppWebConfig extends WebMvcConfigurerAdapter {
+
+	@Bean
+	public TemplateResolver webTemplateResolver() {
+		TemplateResolver templateResolver = new ServletContextTemplateResolver();
+		templateResolver.setPrefix("/WEB-INF/views/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode("HTML5");
+		templateResolver.setCharacterEncoding("UTF-8");
+		templateResolver.setCacheable(false);
+
+		return templateResolver;
+	}
+
+	@Bean
+	public TemplateEngine templateEngine(TemplateResolver templateResolver) {
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver);
+		templateEngine.addDialect(new LayoutDialect());
+
+		return templateEngine;
+	}
+
+	@Bean
+	public ViewResolver viewResolver(SpringTemplateEngine templateEngine) {
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+		viewResolver.setTemplateEngine(templateEngine);
+		viewResolver.setCharacterEncoding("UTF-8");
+
+		return viewResolver;
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		return new FixedLocaleResolver(new Locale("pt", "BR"));
+	}
+}
