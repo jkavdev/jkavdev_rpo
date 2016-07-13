6a5187891bbe5ce0e8e814e632e38571dc69bc4d
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-07-13 04:21:08 +0000

    Primeiro projeto com spring-boot, apresentando spring-mvc e thymeleaf e spring-security

---
+++ samples/spring-boot/spring-thymeleaf-project/src/main/java/br/com/jkavdev/spring/SpringThymeleafProjectApplication.java
@@ -0,0 +1,12 @@
+package br.com.jkavdev.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymeleafProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringThymeleafProjectApplication.class, args);
+	}
+}
