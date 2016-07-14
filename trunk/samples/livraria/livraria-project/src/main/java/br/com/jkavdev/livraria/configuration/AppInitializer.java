fa84fe525eb6e939ab11f88de0c0ddc3bfadf3c3
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-07-14 17:54:49 +0000

    Configurando o Jpa

--- trunk/samples/livraria/livraria-project/src/main/java/br/com/jkavdev/livraria/configuration/AppInitializer.java
+++ trunk/samples/livraria/livraria-project/src/main/java/br/com/jkavdev/livraria/configuration/AppInitializer.java
@@ -2,12 +2,12 @@ package br.com.jkavdev.livraria.configuration;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class AppInitializer 
-	extends AbstractAnnotationConfigDispatcherServletInitializer {
+public class AppInitializer extends
+		AbstractAnnotationConfigDispatcherServletInitializer {
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		return new Class<?>[] { AppWebConfig.class, AppSecConfig.class };
+		return new Class<?>[] { AppWebConfig.class, AppJPAConfig.class, AppSecConfig.class };
 	}
 
 	@Override
