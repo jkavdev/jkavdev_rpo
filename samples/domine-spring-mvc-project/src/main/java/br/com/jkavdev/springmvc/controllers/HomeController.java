635319e4760fd4066aafc47bc6b6bcf36a165dbe
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-07-18 00:58:22 +0000

    Adicionando cadastro de produtos

--- samples/domine-spring-mvc-project/src/main/java/br/com/jkavdev/springmvc/controllers/HomeController.java
+++ samples/domine-spring-mvc-project/src/main/java/br/com/jkavdev/springmvc/controllers/HomeController.java
@@ -1,15 +1,18 @@
 package br.com.jkavdev.springmvc.controllers;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
+	private static final Logger logger = Logger.getLogger(HomeController.class);
+
 	@RequestMapping("/home")
 	public String index() {
 
-		System.out.println("Carregando produtos");
+		logger.info("Carregando produtos");
 
 		return "hello-world";
 
