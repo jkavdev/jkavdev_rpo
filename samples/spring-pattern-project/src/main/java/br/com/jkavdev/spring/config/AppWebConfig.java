a309d77bae1c67c55cc412f1871fb7db03f1d123
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-07-20 13:03:51 +0000

    Commit inicial do projeto spring

--- samples/spring-pattern-project/src/main/java/br/com/jkavdev/spring/config/AppWebConfig.java
+++ samples/spring-pattern-project/src/main/java/br/com/jkavdev/spring/config/AppWebConfig.java
@@ -1,76 +1,61 @@
 package br.com.jkavdev.spring.config;
 
-import java.util.Locale;
-
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
-import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
-import org.thymeleaf.templateresolver.TemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 import br.com.jkavdev.spring.controllers.HomeController;
-import nz.net.ultraq.thymeleaf.LayoutDialect;
-
-//classe de configuracao da aplicacao
-//que habilitara o spring web mvc
-//fara tambem a configuracao dos controllers da aplicacao
 
 @Configuration
 @EnableWebMvc
-// informando que todas as classes onde está
-// imoveisController sera controllers
 @ComponentScan(basePackageClasses = { HomeController.class })
-public class AppWebConfig extends WebMvcConfigurerAdapter {
+public class AppWebConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
 
-	// beans que serão usados no contexto do spring mvc
-	@Bean
-	public TemplateResolver webTemplateResolver() {
-		TemplateResolver templateResolver = new ServletContextTemplateResolver();
-		// onde sera salvo minhas paginas
-		// e quais serao a extensao das paginas
-		templateResolver.setPrefix("/WEB-INF/views/");
-		templateResolver.setSuffix(".html");
-		// informando que sera usado o html 5
-		// e o tipo de utf-8 como encoding
-		templateResolver.setTemplateMode("HTML5");
-		templateResolver.setCharacterEncoding("UTF-8");
-		templateResolver.setCacheable(false);
+	private ApplicationContext applicationContext;
 
-		return templateResolver;
-	}
-
-	// configurando o thymeleaf
-	@Bean
-	public TemplateEngine templateEngine(TemplateResolver templateResolver) {
-		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-		templateEngine.setTemplateResolver(templateResolver);
-		templateEngine.addDialect(new LayoutDialect());
-
-		return templateEngine;
+	public void setApplicationContext(ApplicationContext applicationContext) {
+		this.applicationContext = applicationContext;
 	}
 
 	@Bean
-	public ViewResolver viewResolver(SpringTemplateEngine templateEngine) {
-		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-		viewResolver.setTemplateEngine(templateEngine);
-		viewResolver.setCharacterEncoding("UTF-8");
-
-		return viewResolver;
+	public ViewResolver viewResolver() {
+		
+		ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+		resolver.setTemplateEngine(templateEngine());
+		resolver.setCharacterEncoding("UTF-8");
+		
+		return resolver;
 	}
-	// configurando o thymeleaf
 
-	// definindo idioma da aplicacao
 	@Bean
-	public LocaleResolver localeResolver() {
-		return new FixedLocaleResolver(new Locale("pt", "BR"));
+	public TemplateEngine templateEngine() {
+		
+		SpringTemplateEngine engine = new SpringTemplateEngine();
+		engine.setEnableSpringELCompiler(true);
+		engine.setTemplateResolver(templateResolver());
+		
+		return engine;
+	}
+
+	private ITemplateResolver templateResolver() {
+		
+		SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+		resolver.setApplicationContext(applicationContext);
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setTemplateMode(TemplateMode.HTML);
+		
+		return resolver;
 	}
 
 }
