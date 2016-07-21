d4a395f085d531e6f18e8cbb7c099f1b595baad8
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-07-21 05:29:20 +0000

    Spring Mvc with jsp Hello World

--- samples/spring-pattern-project/src/main/java/br/com/jkavdev/spring/controllers/HomeController.java
+++ samples/spring-pattern-project/src/main/java/br/com/jkavdev/spring/controllers/HomeController.java
@@ -2,14 +2,17 @@ package br.com.jkavdev.spring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping(value = "/")
-	public String home(){
-		
-		return "home.html";
+	@RequestMapping({ "/", "/welcome" })
+	public ModelAndView home() {
+
+		String message = "Spring MVC and JSP";
+
+		return new ModelAndView("welcome", "message", message);
 	}
 
 }
