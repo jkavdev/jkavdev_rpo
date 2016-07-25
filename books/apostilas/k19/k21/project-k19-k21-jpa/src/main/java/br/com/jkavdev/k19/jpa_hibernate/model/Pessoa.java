03c53ac3e6c32d98e7c5a553e1f586c44ff9174f
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-07-25 21:25:24 +0000

    Mais sobre a alteração dos atributos de uma entidade

--- books/apostilas/k19/k21/project-k19-k21-jpa/src/main/java/br/com/jkavdev/k19/jpa_hibernate/model/Pessoa.java
+++ books/apostilas/k19/k21/project-k19-k21-jpa/src/main/java/br/com/jkavdev/k19/jpa_hibernate/model/Pessoa.java
@@ -1,12 +1,18 @@
 package br.com.jkavdev.k19.jpa_hibernate.model;
 
 import java.math.BigDecimal;
+import java.util.Calendar;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 
 @Entity
 // alterando o nome da tabela
@@ -16,9 +22,12 @@ public class Pessoa {
 	private Long id;
 	private String nome;
 	private BigDecimal altura;
+	private byte[] avatar;
+	private Calendar dataNascimento;
+	private int idade;
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	// alterando o nome da coluna
 	@Column(name = "pessoa_id")
 	public Long getId() {
@@ -47,6 +56,37 @@ public class Pessoa {
 
 	public void setAltura(BigDecimal altura) {
 		this.altura = altura;
+	}
+
+	// informando que este campo pode ser maior que o comum
+	@Lob
+	public byte[] getAvatar() {
+		return avatar;
+	}
+
+	public void setAvatar(byte[] avatar) {
+		this.avatar = avatar;
+	}
+
+	@Column(name = "data_nascimento")
+	// informando que apenas será persistido a data
+	@Temporal(TemporalType.DATE)
+	public Calendar getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Calendar dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	// informando que este campo não será persistido
+	@Transient
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
 	}
 
 }
