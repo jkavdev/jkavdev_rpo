49b239190104c27554be55fe45dda253ad8ff963
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-07-25 01:44:13 +0000

    4.12-projeto-do-curso-conversor-personalizado-v1 - Conversor para pessoa

--- trunk/course_video_tutorials/algaworks/curso_desenvolvimento_web_com_jsf2/financeiro-jsf/src/main/java/br/com/jkavdev/algaworks/jsf/controllers/CadastroLancamentoBean.java
+++ trunk/course_video_tutorials/algaworks/curso_desenvolvimento_web_com_jsf2/financeiro-jsf/src/main/java/br/com/jkavdev/algaworks/jsf/controllers/CadastroLancamentoBean.java
@@ -1,26 +1,39 @@
 package br.com.jkavdev.algaworks.jsf.controllers;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 
 import br.com.jkavdev.algaworks.jsf.model.Lancamento;
+import br.com.jkavdev.algaworks.jsf.model.Pessoa;
 import br.com.jkavdev.algaworks.jsf.model.TipoLancamento;
+import br.com.jkavdev.algaworks.jsf.services.GestaoPessoas;
 
 @ManagedBean
 @ViewScoped
 public class CadastroLancamentoBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private Lancamento lancamento = new Lancamento();
+	private List<Pessoa> pessoas = new ArrayList<>();
+
+	@PostConstruct
+	public void init() {
+		GestaoPessoas gestaoPessoas = new GestaoPessoas();
+		pessoas = gestaoPessoas.listarTodas();
+	}
 
 	public void cadastrar() {
 		System.out.println("Tipo: " + this.lancamento.getTipo());
 		System.out.println("Descrição: " + this.lancamento.getDescricao());
+		System.out.println("Pessoa: " + this.lancamento.getPessoa().getNome());
 		System.out.println("Valor: " + this.lancamento.getValor());
 		System.out.println("Data vencimento: " + this.lancamento.getDataVencimento());
 		System.out.println("Conta paga: " + this.lancamento.isPago());
@@ -38,6 +51,10 @@ public class CadastroLancamentoBean implements Serializable {
 
 	public Lancamento getLancamento() {
 		return lancamento;
+	}
+
+	public List<Pessoa> getPessoas() {
+		return pessoas;
 	}
 
 }
