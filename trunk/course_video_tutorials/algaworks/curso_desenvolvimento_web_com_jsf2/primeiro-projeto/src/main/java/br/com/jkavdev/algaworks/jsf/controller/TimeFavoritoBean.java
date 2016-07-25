94c9d7986968b31f0476098691ba73cdc6b70087
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-07-25 13:07:37 +0000

    3.3-menus-caixas-de-listagem-e-itens-de-selecao-v1

---
+++ trunk/course_video_tutorials/algaworks/curso_desenvolvimento_web_com_jsf2/primeiro-projeto/src/main/java/br/com/jkavdev/algaworks/jsf/controller/TimeFavoritoBean.java
@@ -0,0 +1,22 @@
+package br.com.jkavdev.algaworks.jsf.controller;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class TimeFavoritoBean {
+
+	private String time;
+	
+	public void escolher(){
+		System.out.println("Time selecionado: " + this.getTime());
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+}
