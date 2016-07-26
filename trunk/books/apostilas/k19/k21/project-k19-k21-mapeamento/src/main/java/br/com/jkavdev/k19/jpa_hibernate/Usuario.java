70cc1c1749b28e19494000b3c1ee87e994d4368e
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-07-26 06:07:19 +0000

    Implementando persistencia de usuario

---
+++ trunk/books/apostilas/k19/k21/project-k19-k21-mapeamento/src/main/java/br/com/jkavdev/k19/jpa_hibernate/Usuario.java
@@ -0,0 +1,59 @@
+package br.com.jkavdev.k19.jpa_hibernate;
+
+import java.util.Calendar;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Usuario {
+
+	private Long id;
+	private String email;
+	private Calendar dataDeCadastro;
+	private byte[] foto;
+
+	@Id
+	@GeneratedValue
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Column(unique = true)
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Column(name = "data_cadastro")
+	@Temporal(TemporalType.DATE)
+	public Calendar getDataDeCadastro() {
+		return dataDeCadastro;
+	}
+
+	public void setDataDeCadastro(Calendar dataDeCadastro) {
+		this.dataDeCadastro = dataDeCadastro;
+	}
+
+	@Lob
+	public byte[] getFoto() {
+		return foto;
+	}
+
+	public void setFoto(byte[] foto) {
+		this.foto = foto;
+	}
+
+}
