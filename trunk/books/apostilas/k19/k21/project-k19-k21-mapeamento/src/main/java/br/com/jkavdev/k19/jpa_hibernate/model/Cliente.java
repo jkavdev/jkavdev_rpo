01ba29de8622657a745573a0c6c27946103d5468
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-07-26 20:07:28 +0000

    Implementando relacionamento ManyToOne

---
+++ trunk/books/apostilas/k19/k21/project-k19-k21-mapeamento/src/main/java/br/com/jkavdev/k19/jpa_hibernate/model/Cliente.java
@@ -0,0 +1,20 @@
+package br.com.jkavdev.k19.jpa_hibernate.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Cliente extends BaseEntity {
+
+	private static final long serialVersionUID = 1L;
+
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
