283afe3c81ff952f71e16659348431e581ec1f9f
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-07-27 18:50:43 +0000

    Adicionando Dao Jpa

---
+++ books/apostilas/baeldung/persistence-with-spring/persistence-with-spring/src/main/java/br/com/jkavdev/spring/persistence/dao/AbstractJpaDao.java
@@ -0,0 +1,46 @@
+package br.com.jkavdev.spring.persistence.dao;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+public class AbstractJpaDao<T extends Serializable> {
+
+	private Class<T> entity;
+
+	@PersistenceContext
+	private EntityManager manager;
+
+	public void setEntity(final Class<T> entity) {
+		this.entity = entity;
+	}
+
+	public T findOne(final Long id) {
+		return manager.find(entity, id);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<T> findAll() {
+		return manager.createQuery("from " + entity.getName()).getResultList();
+	}
+
+	public void salve(final T entity) {
+		manager.persist(entity);
+	}
+
+	public T update(final T entity) {
+		return manager.merge(entity);
+	}
+
+	public void delete(final T entity) {
+		manager.remove(entity);
+	}
+
+	public void deleteById(final Long id) {
+		final T entity = findOne(id);
+		delete(entity);
+	}
+
+}
