fc8dfc46f5b1d0b6fef266da9af5579ca623a5cc
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-07-28 04:13:37 +0000

    5.7-exclusao-de-lancamentos-do-banco-de-dados-v1

--- trunk/course_video_tutorials/algaworks/curso_desenvolvimento_web_com_jsf2/financeiro-jsf/src/main/java/br/com/jkavdev/algaworks/jsf/controllers/ConsultaLancamentoBean.java
+++ trunk/course_video_tutorials/algaworks/curso_desenvolvimento_web_com_jsf2/financeiro-jsf/src/main/java/br/com/jkavdev/algaworks/jsf/controllers/ConsultaLancamentoBean.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 
 import org.hibernate.Session;
@@ -16,21 +17,48 @@ import br.com.jkavdev.algaworks.jsf.util.jpa.HibernateUtil;
 public class ConsultaLancamentoBean {
 
 	private List<Lancamento> lancamentos = new ArrayList<>();
+	private Lancamento lancamentoSelecionado;
 
 	@SuppressWarnings("unchecked")
 	@PostConstruct
 	public void init() {
 		Session session = HibernateUtil.getSession();
-		
-		this.lancamentos = session.createCriteria(Lancamento.class)
-				.addOrder(Order.desc("dataVencimento"))
-				.list();
-		
+
+		this.lancamentos = session.createCriteria(Lancamento.class).addOrder(Order.desc("dataVencimento")).list();
+
 		session.close();
+	}
+	
+	public void excluir(){
+		if(this.lancamentoSelecionado.isPago()){
+			FacesUtil.adicionarMensagem(FacesMessage.SEVERITY_ERROR, "Lançamento já foi pago e não pode ser excluído");	
+		} else { 		
+			Session session = HibernateUtil.getSession();
+			
+			session.getTransaction().begin();
+	
+			session.delete(this.lancamentoSelecionado);
+			
+			session.getTransaction().commit();
+	
+			session.close();
+			
+			this.init();
+			
+			FacesUtil.adicionarMensagem(FacesMessage.SEVERITY_INFO, "Lançamento excluído com sucesso!");
+		}
 	}
 
 	public List<Lancamento> getLancamentos() {
 		return lancamentos;
+	}
+
+	public Lancamento getLancamentoSelecionado() {
+		return lancamentoSelecionado;
+	}
+
+	public void setLancamentoSelecionado(Lancamento lancamentoSelecionado) {
+		this.lancamentoSelecionado = lancamentoSelecionado;
 	}
 
 }
