120ea6b4fd1df4556f668ccf1b299b604098bbdb
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-07-31 15:55:44 +0000

    v1.7-relacionamento-entre-tabelas-v1

--- trunk/course_video_tutorials/algaworks/curso_jpa_e_hibernate/script.sql
+++ trunk/course_video_tutorials/algaworks/curso_jpa_e_hibernate/script.sql
@@ -27,3 +27,21 @@ insert into cliente (nome, idade, sexo, profissao) values
 	('Fernando Alves', 20, 'M', 'Professor'),
 	('Márcio Souza', 22, 'M', 'Cozinheiro'),
 	('Lucas Alves', 20, 'M', 'Marceneiro');
+
+select * from cliente;	
+
+insert into conta_corrente (numero, saldo, codigo_cliente) values
+	(123456, 1000.00, 1),
+	(123457, 10336.00, 2),
+	(123458, 120.00, 3),
+	(123459, 1110.00, 4),
+	(123460, 1560.00, 5);
+
+select * from conta_corrente;
+
+--selecionando todos os cliente com conta_corrente
+--do maior para o menor saldo na conta
+select cli.nome, cc.saldo
+	from cliente cli, conta_corrente cc
+	where cli.codigo = cc.codigo_cliente
+	order by cc.saldo desc;
