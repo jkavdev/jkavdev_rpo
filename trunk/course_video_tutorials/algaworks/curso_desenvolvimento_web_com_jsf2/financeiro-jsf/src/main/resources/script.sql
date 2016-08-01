444bffc2ff589fab083a7f6749ccbd4e7603a47e
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-08-01 06:02:40 +0000

    6.9-seguranca-da-aplicacao-com-jaas-v1

--- trunk/course_video_tutorials/algaworks/curso_desenvolvimento_web_com_jsf2/financeiro-jsf/src/main/resources/script.sql
+++ trunk/course_video_tutorials/algaworks/curso_desenvolvimento_web_com_jsf2/financeiro-jsf/src/main/resources/script.sql
@@ -7,3 +7,26 @@ insert into pessoas values
 	(3, 'Marcos Melhem'),
 	(4, 'Vinícius Gaúcho'),
 	(5, 'Neném Mattos');
+	
+create table usuario(
+	nome_usuario varchar(15) not null primary key,
+	senha varchar(15) not null
+);
+
+create table permissao_usuario(
+	nome_usuario varchar(15) not null,
+	nome_permissao varchar(15) not null,
+	primary key(nome_usuario, nome_permissao),
+	foreign key(nome_usuario) references usuario(nome_usuario)
+);
+
+insert into usuario values
+	('Jhonatan', '123456'),
+	('Lucas', '123'),
+	('Douglas', '456');
+	
+insert into permissao_usuario values
+	('Jhonatan', 'cadastro'),
+	('Lucas', 'consulta'),
+	('Douglas', 'cadastro'),
+	('Douglas', 'consulta');
