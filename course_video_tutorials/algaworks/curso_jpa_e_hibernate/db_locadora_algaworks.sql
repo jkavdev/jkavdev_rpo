f0f9be2894d40dd131070d9c0f36375636876f27
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-08-01 00:00:30 +0000

    v4.1-criando-o-projeto-do-curso-v1

--- course_video_tutorials/algaworks/curso_jpa_e_hibernate/db_locadora_algaworks.sql
+++ course_video_tutorials/algaworks/curso_jpa_e_hibernate/db_locadora_algaworks.sql
@@ -1,4 +1,4 @@
-create database db_banco_tabajara_algaworks;
-drop database db_banco_tabajara_algaworks;
+create database db_locadora_algaworks;
+drop database db_locadora_algaworks;
 
-use db_banco_tabajara_algaworks;
+use db_locadora_algaworks;
