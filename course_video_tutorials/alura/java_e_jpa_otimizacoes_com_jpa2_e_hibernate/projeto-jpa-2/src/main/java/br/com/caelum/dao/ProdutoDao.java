da589ca26a943e326d9bcee32921d88783c0e730
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-08-04 16:38:47 +0000

    Usando criteria e predicates

--- course_video_tutorials/alura/java_e_jpa_otimizacoes_com_jpa2_e_hibernate/projeto-jpa-2/src/main/java/br/com/caelum/dao/ProdutoDao.java
+++ course_video_tutorials/alura/java_e_jpa_otimizacoes_com_jpa2_e_hibernate/projeto-jpa-2/src/main/java/br/com/caelum/dao/ProdutoDao.java
@@ -1,13 +1,20 @@
 package br.com.caelum.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
 
+import br.com.caelum.model.Loja;
 import br.com.caelum.model.Produto;
 
 @Repository
@@ -27,68 +34,52 @@ public class ProdutoDao {
 
 	public List<Produto> getProdutos(String nome, Integer categoriaId, Integer lojaId) {
 
-		// consulta normal
-		// String jpql = "select p from Produto p "
-		// + "join fetch p.categorias c "
-		// + "where c.id = :pCategoriaId and p.loja.id = :pLojaId and p.nome
-		// like :pProdutoNome";
+		// prover a query a ser criada e metodos adicionais como igual, maior,
+		// menor
+		CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
 
-		// consulta evitando os campos vazios
-		String jpql = "select p from Produto p ";
+		// posso informar qual sera o tipo de retorno da query
+		CriteriaQuery<Produto> query = criteriaBuilder.createQuery(Produto.class);
+
+		// Obtendo acesso ao tipo da classe
+		Root<Produto> root = query.from(Produto.class);
+
+		// obtendo acesso aos atributos classe
+		Path<String> nomePath = root.<String>get("nome");
+
+		// acessando o atributo id no relacionamento com categorias
+		// precisamos relizar um join, há uma associacao entre as entidades
+		Path<Integer> categoriaPath = root.join("categorias").<Integer>get("id");
+
+		// acessando o atributo id de loja
+		Path<Integer> lojaPath = root.<Loja>get("loja").<Integer>get("id");
+
+		// criando lista de predicados
+		// para armazenar os predicas e passar como array no where da query
+		List<Predicate> predicates = new ArrayList<>();
+
+		// criando filtro
+		// verificando variaveis passadas
+		if (!nome.isEmpty()) {
+			Predicate nomeIgual = criteriaBuilder.like(nomePath, nome);
+			predicates.add(nomeIgual);
+		}
 
 		if (categoriaId != null) {
-			jpql += "join fetch p.categorias c where c.id = :pCategoriaId ";
-		} else {
-			jpql += "where ";
+			Predicate categoriaIgual = criteriaBuilder.equal(categoriaPath, categoriaId);
+			predicates.add(categoriaIgual);
 		}
+
 		if (lojaId != null) {
-			jpql += "and p.loja.id = :pLojaId ";
-		}
-		if (!nome.isEmpty()) {
-			jpql += "and p.nome like :pProdutoNome";
+			Predicate lojaIgual = criteriaBuilder.equal(lojaPath, lojaId);
+			predicates.add(lojaIgual);
 		}
 
-		TypedQuery<Produto> query = em.createQuery(jpql, Produto.class);
+		// adiciona filtro a query
+		query.where((Predicate[]) predicates.toArray(new Predicate[0]));
 
-		if (categoriaId != null)
-			query.setParameter("pCategoriaId", categoriaId);
-		if (lojaId != null)
-			query.setParameter("pLojaId", lojaId);
-		if (!nome.isEmpty())
-			query.setParameter("pProdutoNome", nome);
-
-		// CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
-		// CriteriaQuery<Produto> query =
-		// criteriaBuilder.createQuery(Produto.class);
-		// Root<Produto> root = query.from(Produto.class);
-		//
-		// Path<String> nomePath = root.<String> get("nome");
-		// Path<Integer> lojaPath = root.<Loja> get("loja").<Integer> get("id");
-		// Path<Integer> categoriaPath = root.join("categorias").<Integer>
-		// get("id");
-		//
-		// List<Predicate> predicates = new ArrayList<>();
-		//
-		// if (!nome.isEmpty()) {
-		// Predicate nomeIgual = criteriaBuilder.like(nomePath, nome);
-		// predicates.add(nomeIgual);
-		// }
-		// if (categoriaId != null) {
-		// Predicate categoriaIgual = criteriaBuilder.equal(categoriaPath,
-		// categoriaId);
-		// predicates.add(categoriaIgual);
-		// }
-		// if (lojaId != null) {
-		// Predicate lojaIgual = criteriaBuilder.equal(lojaPath, lojaId);
-		// predicates.add(lojaIgual);
-		// }
-		//
-		// query.where((Predicate[]) predicates.toArray(new Predicate[0]));
-		//
-		// TypedQuery<Produto> typedQuery = em.createQuery(query);
-		// return typedQuery.getResultList();
-
-		return query.getResultList();
+		TypedQuery<Produto> typedQuery = em.createQuery(query);
+		return typedQuery.getResultList();
 
 	}
 
