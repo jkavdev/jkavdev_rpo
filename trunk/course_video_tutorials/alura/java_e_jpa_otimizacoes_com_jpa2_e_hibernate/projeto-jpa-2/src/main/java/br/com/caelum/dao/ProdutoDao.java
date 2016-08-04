00966178f59d561db7995c816f375ab7692e6b64
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-08-04 06:00:37 +0000

    Implementando metodo de busca com filtros com concatenacao e veificacao de strings

--- trunk/course_video_tutorials/alura/java_e_jpa_otimizacoes_com_jpa2_e_hibernate/projeto-jpa-2/src/main/java/br/com/caelum/dao/ProdutoDao.java
+++ trunk/course_video_tutorials/alura/java_e_jpa_otimizacoes_com_jpa2_e_hibernate/projeto-jpa-2/src/main/java/br/com/caelum/dao/ProdutoDao.java
@@ -1,20 +1,13 @@
 package br.com.caelum.dao;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Path;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
 
-import br.com.caelum.model.Loja;
 import br.com.caelum.model.Produto;
 
 @Repository
@@ -33,33 +26,69 @@ public class ProdutoDao {
 	}
 
 	public List<Produto> getProdutos(String nome, Integer categoriaId, Integer lojaId) {
-		CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
-		CriteriaQuery<Produto> query = criteriaBuilder.createQuery(Produto.class);
-		Root<Produto> root = query.from(Produto.class);
 
-		Path<String> nomePath = root.<String> get("nome");
-		Path<Integer> lojaPath = root.<Loja> get("loja").<Integer> get("id");
-		Path<Integer> categoriaPath = root.join("categorias").<Integer> get("id");
+		// consulta normal
+		// String jpql = "select p from Produto p "
+		// + "join fetch p.categorias c "
+		// + "where c.id = :pCategoriaId and p.loja.id = :pLojaId and p.nome
+		// like :pProdutoNome";
 
-		List<Predicate> predicates = new ArrayList<>();
+		// consulta evitando os campos vazios
+		String jpql = "select p from Produto p ";
 
-		if (!nome.isEmpty()) {
-			Predicate nomeIgual = criteriaBuilder.like(nomePath, nome);
-			predicates.add(nomeIgual);
-		}
 		if (categoriaId != null) {
-			Predicate categoriaIgual = criteriaBuilder.equal(categoriaPath, categoriaId);
-			predicates.add(categoriaIgual);
+			jpql += "join fetch p.categorias c where c.id = :pCategoriaId ";
+		} else {
+			jpql += "where ";
 		}
 		if (lojaId != null) {
-			Predicate lojaIgual = criteriaBuilder.equal(lojaPath, lojaId);
-			predicates.add(lojaIgual);
+			jpql += "and p.loja.id = :pLojaId ";
+		}
+		if (!nome.isEmpty()) {
+			jpql += "and p.nome like :pProdutoNome";
 		}
 
-		query.where((Predicate[]) predicates.toArray(new Predicate[0]));
+		TypedQuery<Produto> query = em.createQuery(jpql, Produto.class);
 
-		TypedQuery<Produto> typedQuery = em.createQuery(query);
-		return typedQuery.getResultList();
+		if (categoriaId != null)
+			query.setParameter("pCategoriaId", categoriaId);
+		if (lojaId != null)
+			query.setParameter("pLojaId", lojaId);
+		if (!nome.isEmpty())
+			query.setParameter("pProdutoNome", nome);
+
+		// CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+		// CriteriaQuery<Produto> query =
+		// criteriaBuilder.createQuery(Produto.class);
+		// Root<Produto> root = query.from(Produto.class);
+		//
+		// Path<String> nomePath = root.<String> get("nome");
+		// Path<Integer> lojaPath = root.<Loja> get("loja").<Integer> get("id");
+		// Path<Integer> categoriaPath = root.join("categorias").<Integer>
+		// get("id");
+		//
+		// List<Predicate> predicates = new ArrayList<>();
+		//
+		// if (!nome.isEmpty()) {
+		// Predicate nomeIgual = criteriaBuilder.like(nomePath, nome);
+		// predicates.add(nomeIgual);
+		// }
+		// if (categoriaId != null) {
+		// Predicate categoriaIgual = criteriaBuilder.equal(categoriaPath,
+		// categoriaId);
+		// predicates.add(categoriaIgual);
+		// }
+		// if (lojaId != null) {
+		// Predicate lojaIgual = criteriaBuilder.equal(lojaPath, lojaId);
+		// predicates.add(lojaIgual);
+		// }
+		//
+		// query.where((Predicate[]) predicates.toArray(new Predicate[0]));
+		//
+		// TypedQuery<Produto> typedQuery = em.createQuery(query);
+		// return typedQuery.getResultList();
+
+		return query.getResultList();
 
 	}
 
