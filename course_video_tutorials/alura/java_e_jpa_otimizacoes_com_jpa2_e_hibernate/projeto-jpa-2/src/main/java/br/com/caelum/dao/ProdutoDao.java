aaac0251edc99c64bf35a02014f8d06562063de2
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-08-04 20:07:09 +0000

    Usando o criteria do hibernate

--- course_video_tutorials/alura/java_e_jpa_otimizacoes_com_jpa2_e_hibernate/projeto-jpa-2/src/main/java/br/com/caelum/dao/ProdutoDao.java
+++ course_video_tutorials/alura/java_e_jpa_otimizacoes_com_jpa2_e_hibernate/projeto-jpa-2/src/main/java/br/com/caelum/dao/ProdutoDao.java
@@ -1,20 +1,17 @@
 package br.com.caelum.dao;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Path;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
 
+import org.hibernate.Criteria;
+import org.hibernate.FetchMode;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-import br.com.caelum.model.Loja;
 import br.com.caelum.model.Produto;
 
 @Repository
@@ -32,69 +29,29 @@ public class ProdutoDao {
 		return produto;
 	}
 
+	@SuppressWarnings("unchecked")
+	@Transactional
 	public List<Produto> getProdutos(String nome, Integer categoriaId, Integer lojaId) {
 
-		// prover a query a ser criada e metodos adicionais como igual, maior,
-		// menor
-		CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
-
-		// posso informar qual sera o tipo de retorno da query
-		CriteriaQuery<Produto> query = criteriaBuilder.createQuery(Produto.class);
-
-		// Obtendo acesso ao tipo da classe
-		Root<Produto> root = query.from(Produto.class);
-
-		// obtendo acesso aos atributos classe
-		Path<String> nomePath = root.<String>get("nome");
-
-		// acessando o atributo id no relacionamento com categorias
-		// precisamos relizar um join, há uma associacao entre as entidades
-		Path<Integer> categoriaPath = root.join("categorias").<Integer>get("id");
-
-		// acessando o atributo id de loja
-		Path<Integer> lojaPath = root.<Loja>get("loja").<Integer>get("id");
-
-		// criando lista de predicados
-		// para armazenar os predicas e passar como array no where da query
-		List<Predicate> predicates = new ArrayList<>();
-
-		// criando filtro de conjuncao
-		Predicate conjuncao = criteriaBuilder.conjunction();
-
-		// criando filtro
-		// verificando variaveis passadas
-		if (!nome.isEmpty()) {
-			Predicate nomeIgual = criteriaBuilder.like(nomePath, "%" + nome + "%");
-			predicates.add(nomeIgual);
-
-			// adiciona conjuncao
-			conjuncao = criteriaBuilder.and(nomeIgual);
+		Session session = em.unwrap(Session.class);
+		
+		Criteria criteria = session.createCriteria(Produto.class);
+		
+		if(!nome.isEmpty()){
+			criteria.add(Restrictions.like("nome", "%" + nome + "%"));
 		}
-
-		if (categoriaId != null) {
-			Predicate categoriaIgual = criteriaBuilder.equal(categoriaPath, categoriaId);
-			predicates.add(categoriaIgual);
-
-			// como o criteria builder recebe uma varargs de predicates
-			// podemos adicionar varios predicates
-			conjuncao = criteriaBuilder.and(conjuncao, criteriaBuilder.and(categoriaIgual));
+		
+		if(lojaId != null){
+			criteria.add(Restrictions.eq("loja.id", lojaId));
 		}
-
-		if (lojaId != null) {
-			Predicate lojaIgual = criteriaBuilder.equal(lojaPath, lojaId);
-			predicates.add(lojaIgual);
-
-			conjuncao = criteriaBuilder.and(conjuncao, criteriaBuilder.and(lojaIgual));
+		
+		if(categoriaId != null){
+			criteria.setFetchMode("categorias", FetchMode.JOIN)
+				.createAlias("categorias", "c")
+				.add(Restrictions.eq("c.id", categoriaId));
 		}
-
-		// usando filtro de conjuncao
-		// query.where(conjuncao);
-
-		// adiciona filtro a query
-		query.where((Predicate[]) predicates.toArray(new Predicate[0]));
-
-		TypedQuery<Produto> typedQuery = em.createQuery(query);
-		return typedQuery.getResultList();
+		
+		return criteria.list();
 
 	}
 
