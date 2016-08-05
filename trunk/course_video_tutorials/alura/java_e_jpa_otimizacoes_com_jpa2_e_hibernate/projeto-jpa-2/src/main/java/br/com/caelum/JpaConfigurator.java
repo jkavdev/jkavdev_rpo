ae3f4b5e5ff45d292e219d238e41d48a991b22b1
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-08-05 19:24:39 +0000

    Adicionando pool de conexao

--- trunk/course_video_tutorials/alura/java_e_jpa_otimizacoes_com_jpa2_e_hibernate/projeto-jpa-2/src/main/java/br/com/caelum/JpaConfigurator.java
+++ trunk/course_video_tutorials/alura/java_e_jpa_otimizacoes_com_jpa2_e_hibernate/projeto-jpa-2/src/main/java/br/com/caelum/JpaConfigurator.java
@@ -1,5 +1,6 @@
 package br.com.caelum;
 
+import java.beans.PropertyVetoException;
 import java.util.Properties;
 
 import javax.persistence.EntityManagerFactory;
@@ -7,24 +8,32 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 @Configuration
 @EnableTransactionManagement
 public class JpaConfigurator {
 
 	@Bean
-	public DataSource getDataSource() {
-	    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+	public DataSource getDataSource() throws PropertyVetoException {
+		//adicionando pool do c3p0
+	    ComboPooledDataSource dataSource = new ComboPooledDataSource();
 
-	    dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-	    dataSource.setUrl("jdbc:mysql://localhost/db_jpa_avancado_alura");
-	    dataSource.setUsername("jkavdev");
+	    dataSource.setDriverClass("com.mysql.jdbc.Driver");
+	    dataSource.setJdbcUrl("jdbc:mysql://localhost/db_jpa_avancado_alura");
+	    dataSource.setUser("jkavdev");
 	    dataSource.setPassword("123456");
+	    
+	    //adicionando quantidade de conexao a serem abertas
+//	    dataSource.setInitialPoolSize(3);
+	    
+	    dataSource.setMinPoolSize(3);
+	    dataSource.setMaxPoolSize(5);
 
 	    return dataSource;
 	}
