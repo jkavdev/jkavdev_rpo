0e0ac03c98c91f65b2250e77b2496b020f706f9b
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-08-17 16:53:16 +0000

    Vídeo 3 Cadastro e Upload - Integrando Spring data com Controler e visualização de vinho

--- trunk/course_video_tutorials/algaworks/oficina-spring/algaworks-wine/src/main/java/br/com/jkavdev/algaworks/spring/config/WebConfig.java
+++ trunk/course_video_tutorials/algaworks/oficina-spring/algaworks-wine/src/main/java/br/com/jkavdev/algaworks/spring/config/WebConfig.java
@@ -4,6 +4,8 @@ import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomi
 import org.springframework.boot.context.embedded.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.repository.support.DomainClassConverter;
+import org.springframework.format.support.FormattingConversionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -11,10 +13,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebConfig extends WebMvcConfigurerAdapter {
 
 	@Bean
-	public EmbeddedServletContainerCustomizer containerCustomizer(){
+	public EmbeddedServletContainerCustomizer containerCustomizer() {
+
+		return (container -> container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/404")));
+
+	}
+
+	//configurando data jpa com web mvc
+	@Bean
+	public DomainClassConverter<FormattingConversionService> domainClassConverter(
+			FormattingConversionService conversionService) {
 		
-		return (container -> 
-			container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/404")) );
+		return new DomainClassConverter<FormattingConversionService>(conversionService);
 		
 	}
 
