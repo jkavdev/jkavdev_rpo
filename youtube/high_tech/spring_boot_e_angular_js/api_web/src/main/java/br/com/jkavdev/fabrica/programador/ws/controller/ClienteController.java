65fa6d2a6d92353aa2757b556d267493137a0fb2
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-08-24 03:34:24 +0000

    Aula 01 Parte 03 - Spring Boot e Angular JS - Criando listagem de clientes

--- youtube/high_tech/spring_boot_e_angular_js/api_web/src/main/java/br/com/jkavdev/fabrica/programador/ws/controller/ClienteController.java
+++ youtube/high_tech/spring_boot_e_angular_js/api_web/src/main/java/br/com/jkavdev/fabrica/programador/ws/controller/ClienteController.java
@@ -1,5 +1,6 @@
 package br.com.jkavdev.fabrica.programador.ws.controller;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,14 +17,10 @@ import br.com.jkavdev.fabrica.programador.ws.model.Cliente;
 @RestController
 public class ClienteController {
 
-	Map<Integer, Cliente> clientes;
+	Map<Integer, Cliente> clientes = new HashMap<Integer, Cliente>();
 	Integer proximoId = 0;
 
 	private Cliente cadastrar(Cliente cliente) {
-
-		if (clientes == null) {
-			clientes = new HashMap<Integer, Cliente>();
-		}
 
 		cliente.setId(++proximoId);
 
@@ -32,8 +29,12 @@ public class ClienteController {
 		return cliente;
 	}
 
+	private Collection<Cliente> buscarTodos() {
+		return clientes.values();
+	}
+
 	// consumes - indicamos que este metodo possa tambem ler um json
-	@RequestMapping(method = RequestMethod.POST, value = "/clientes", consumes = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(method = RequestMethod.POST, value = "/clientes", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	// requestBody - indicamos que o json que contem os dados sera
 	// prenchido dentro de cliente
 
@@ -43,6 +44,16 @@ public class ClienteController {
 		Cliente clienteCadastrado = cadastrar(cliente);
 
 		return new ResponseEntity<>(clienteCadastrado, HttpStatus.CREATED);
+
+	}
+
+	// produces - entregaremos um json
+	@RequestMapping(method = RequestMethod.GET, value = "/clientes", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Collection<Cliente>> buscarTodosClientes() {
+
+		Collection<Cliente> clientesBuscados = buscarTodos();
+
+		return new ResponseEntity<>(clientesBuscados, HttpStatus.OK);
 
 	}
 
