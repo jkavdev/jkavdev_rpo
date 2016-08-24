bb217297c41a02f0706c8a5cb6c27a500c91e558
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-08-24 03:14:47 +0000

    Aula 01 Parte 03 - Spring Boot e Angular JS - Criando cadastro de cliente

--- youtube/high_tech/spring_boot_e_angular_js/api_web/src/main/java/br/com/jkavdev/fabrica/programador/ws/controller/ClienteController.java
+++ youtube/high_tech/spring_boot_e_angular_js/api_web/src/main/java/br/com/jkavdev/fabrica/programador/ws/controller/ClienteController.java
@@ -1,17 +1,49 @@
 package br.com.jkavdev.fabrica.programador.ws.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.jkavdev.fabrica.programador.ws.model.Cliente;
+
 @RestController
 public class ClienteController {
-	
-	@RequestMapping(method = RequestMethod.GET, value = "/clientes")
-	public void buscar(){
-		
-		System.out.println("Chamou");
-		
+
+	Map<Integer, Cliente> clientes;
+	Integer proximoId = 0;
+
+	private Cliente cadastrar(Cliente cliente) {
+
+		if (clientes == null) {
+			clientes = new HashMap<Integer, Cliente>();
+		}
+
+		cliente.setId(++proximoId);
+
+		clientes.put(cliente.getId(), cliente);
+
+		return cliente;
+	}
+
+	// consumes - indicamos que este metodo possa tambem ler um json
+	@RequestMapping(method = RequestMethod.POST, value = "/clientes", consumes = MediaType.APPLICATION_JSON_VALUE)
+	// requestBody - indicamos que o json que contem os dados sera
+	// prenchido dentro de cliente
+
+	// retornara um ResponseEntity para json na pagina
+	public ResponseEntity<Cliente> cadastrarCliente(@RequestBody Cliente cliente) {
+
+		Cliente clienteCadastrado = cadastrar(cliente);
+
+		return new ResponseEntity<>(clienteCadastrado, HttpStatus.CREATED);
+
 	}
 
 }
