87bc82ad51aa73d81e89c069ec5b6e175fd12a6a
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-08-29 04:00:29 +0000

    Parte 2 - Mutrack Simple - Java - (Criação do Projeto)(Spring Boot + AngularJS) - Criação do projeto mutrack

---
+++ youtube/augusto_branquinho/wpattern-mutrack-simple-project/wpattern-mutrack-simple/src/main/java/br/com/jkavdev/wpattern/mutrack/utils/AppContext.java
@@ -0,0 +1,14 @@
+package br.com.jkavdev.wpattern.mutrack.utils;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import br.com.jkavdev.wpattern.mutrack.AppMain;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan(basePackageClasses = AppMain.class)
+public class AppContext {
+
+}
