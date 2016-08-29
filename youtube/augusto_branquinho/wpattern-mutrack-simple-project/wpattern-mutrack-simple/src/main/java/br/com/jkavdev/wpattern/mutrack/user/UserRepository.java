4609d231179278df978e4e68ff9df988986c8714
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-08-29 14:56:47 +0000

    Parte 5 - Mutrack Simple - Java - (Consultas usando o Spring Data)(Spring Boot + AngularJS) - Realizando consultas adicionais

--- youtube/augusto_branquinho/wpattern-mutrack-simple-project/wpattern-mutrack-simple/src/main/java/br/com/jkavdev/wpattern/mutrack/user/UserRepository.java
+++ youtube/augusto_branquinho/wpattern-mutrack-simple-project/wpattern-mutrack-simple/src/main/java/br/com/jkavdev/wpattern/mutrack/user/UserRepository.java
@@ -6,4 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
+	// realizando busca atraves do nome do metodo
+	// buscando usuario baseado no email
+	public UserEntity findByEmail(String email);
+	
+	//busca usuario por email ou nome
+	public UserEntity findByEmailOrName(String email, String name);
+
 }
