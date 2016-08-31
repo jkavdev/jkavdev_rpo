d32d6c73bfd70e2acbf663f3649f7ed61aaf0084
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-08-31 04:21:03 +0000

    Parte 10 - Mutrack Simple - Java - (Serviço Genérico REST - Pacotes)(Spring Boot + AngularJS) - Atualizacao de data

--- trunk/youtube/augusto_branquinho/wpattern-mutrack-simple-project/wpattern-mutrack-simple/src/main/java/br/com/jkavdev/wpattern/mutrack/permission/PackageeService.java
+++ trunk/youtube/augusto_branquinho/wpattern-mutrack-simple-project/wpattern-mutrack-simple/src/main/java/br/com/jkavdev/wpattern/mutrack/permission/PackageeService.java
@@ -1,5 +1,8 @@
 package br.com.jkavdev.wpattern.mutrack.permission;
 
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +13,14 @@ import br.com.jkavdev.wpattern.mutrack.utils.ServicePath;
 @RestController
 @RequestMapping(path = ServicePath.PACKAGEE_PATH)
 public class PackageeService extends GenericService<PackageeEntity, Long> {
+	
+	@Override
+	public PackageeEntity insert(@RequestBody PackageeEntity entity) {
+		
+		//nova data
+		entity.setRegisterDate(new Date());
+		
+		return super.insert(entity);
+	}
 
 }
