db9c22fb5c3f3b32ddfb998c9bb5988b75f5e220
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-08-31 04:54:33 +0000

    Parte 11 - Mutrack Simple - Java - (Serviço Genérico REST - Permissões)(Spring Boot + AngularJS) - Criacao dos servicos e delete e update

--- youtube/augusto_branquinho/wpattern-mutrack-simple-project/wpattern-mutrack-simple/src/main/java/br/com/jkavdev/wpattern/mutrack/packagee/PackageeService.java
+++ youtube/augusto_branquinho/wpattern-mutrack-simple-project/wpattern-mutrack-simple/src/main/java/br/com/jkavdev/wpattern/mutrack/packagee/PackageeService.java
@@ -1,4 +1,4 @@
-package br.com.jkavdev.wpattern.mutrack.permission;
+package br.com.jkavdev.wpattern.mutrack.packagee;
 
 import java.util.Date;
 
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.jkavdev.wpattern.mutrack.packagee.PackageeEntity;
 import br.com.jkavdev.wpattern.mutrack.utils.GenericService;
 import br.com.jkavdev.wpattern.mutrack.utils.ServicePath;
 
