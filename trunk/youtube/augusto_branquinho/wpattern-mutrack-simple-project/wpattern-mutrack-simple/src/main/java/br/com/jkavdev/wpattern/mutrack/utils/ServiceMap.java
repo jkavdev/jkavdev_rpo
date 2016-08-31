68396aa6caec55f8770a63ad8395a460f67e784a
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2016-08-31 17:08:56 +0000

    Parte 12 - Mutrack Simple - Java - (Testando os Serviços com o AngularJS)(Spring Boot + AngularJS) - Realizando teste com AngularJS com a API

--- trunk/youtube/augusto_branquinho/wpattern-mutrack-simple-project/wpattern-mutrack-simple/src/main/java/br/com/jkavdev/wpattern/mutrack/utils/ServiceMap.java
+++ trunk/youtube/augusto_branquinho/wpattern-mutrack-simple-project/wpattern-mutrack-simple/src/main/java/br/com/jkavdev/wpattern/mutrack/utils/ServiceMap.java
@@ -1,9 +1,10 @@
 package br.com.jkavdev.wpattern.mutrack.utils;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
-
+//todos os tipos de requisicoes terao acesso aos servicos
+@CrossOrigin(origins = "*")
 @RequestMapping(consumes = "application/json", produces = "application/json")
 public interface ServiceMap {
 
